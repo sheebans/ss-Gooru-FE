37994de3bb585cfb5186a3d41c6ee3f486efb33b
Javier Perez <javier.perez@edify.cr>
2015-09-21 16:33:00 -0600

    GG-4, GG-5 Adding i18n, bootstrap-sass and logger

---
+++ app/locales/en/translations.js
@@ -0,0 +1,13 @@
+export default {
+
+    "common": {
+        "logout": "Logout",
+        "create": "Create",
+        "signIn" : "Sign In"
+    },
+
+    "home": {
+        "welcome": "Welcome"
+    }
+
+};
