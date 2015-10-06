b40d758dd6abb7676782b7bbb9b9c1e4a6480be2
Melany Delgado <melany.delgado@edify.cr>
2015-10-05 18:22:19 -0600

    Empower Students section

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -16,7 +16,16 @@ export default {
   },
 
   "index": {
-    "joinUs": "Join Us to <br/> Honor the Human Right to <br/> Education"
+    "joinUs": "Join Us to <br/> Honor the Human Right to <br/> Education",
+    "empowerStudentsTitle":"Empower Students to Learn Their Way",
+    "empowerStudentFind":"Find",
+    "empowerStudentRemix":"Remix",
+    "empowerStudentShare":"Share",
+    "empowerStudentMonitor":"Monitor",
+    "findDescription":"Browse thousands of K-12 collections made by teachers, or search over 16M resources",
+    "remixDescription":"Remix collections and customize content to meet your students' needs.",
+    "shareDescription":"Share collections with students via My Classes. Login is not required to access.",
+    "monitorDescription":"Measure your students' engagement and progress to intervene in real-time."
   },
 
   "login": {
--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -17,7 +17,7 @@ test('Page Layout', function(assert) {
   visit('/');
 
   andThen(function() {
-    assert.expect(3); //making sure all asserts are called
+    assert.expect(21); //making sure all asserts are called
 
     assert.equal(currentURL(), '/');
 
@@ -26,5 +26,32 @@ test('Page Layout', function(assert) {
     T.exists(assert, $hero, "Missing hero section");
     T.exists(assert, $hero.find("h1"), "Missing hero title");
 
+    var $empowerStudents  = find('.empowerStudents');
+    T.exists(assert, $empowerStudents, "Missing empower students section");
+    T.exists(assert, $empowerStudents.find("h2"), "Missing empower students title");
+
+    var $empowerStudentsFind  = find('.find');
+    T.exists(assert, $empowerStudentsFind, "Missing empower students find section");
+    T.exists(assert, $empowerStudentsFind.find("h2"), "Missing empower students find title");
+    T.exists(assert, $empowerStudentsFind.find('.findImg'), "Missing empower students find image");
+    T.exists(assert, $empowerStudentsFind.find("p"), "Missing empower students find description");
+
+    var $empowerStudentsRemix  = find('.remix');
+    T.exists(assert, $empowerStudentsRemix, "Missing empower students remix section");
+    T.exists(assert, $empowerStudentsRemix.find("h2"), "Missing empower students remix title");
+    T.exists(assert, $empowerStudentsRemix.find('.remixImg'), "Missing empower students remix image");
+    T.exists(assert, $empowerStudentsRemix.find("p"), "Missing empower students remix description");
+
+    var $empowerStudentsShare  = find('.share');
+    T.exists(assert, $empowerStudentsShare, "Missing empower students share section");
+    T.exists(assert, $empowerStudentsShare.find("h2"), "Missing empower students share title");
+    T.exists(assert, $empowerStudentsShare.find('.shareImg'), "Missing empower students share image");
+    T.exists(assert, $empowerStudentsShare.find("p"), "Missing empower students share description");
+
+    var $empowerStudentsMonitor  = find('.monitor');
+    T.exists(assert, $empowerStudentsMonitor, "Missing empower students monitor section");
+    T.exists(assert, $empowerStudentsMonitor.find("h2"), "Missing empower students monitor title");
+    T.exists(assert, $empowerStudentsMonitor.find('.monitorImg'), "Missing empower students monitor image");
+    T.exists(assert, $empowerStudentsMonitor.find("p"), "Missing empower students monitor description");
   });
 });
