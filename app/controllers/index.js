5ce76297da9899fbfebfc51eaa398f51ab942abd
Javier Perez <javier.perez@edify.cr>
2015-10-07 14:18:48 -0600

    GG-31 Adding standard dropdown

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -17,13 +17,12 @@ export default Ember.Controller.extend({
     return items;
   }.property(),
 
-  standardItems: function(){
+  standards: function(){
     //@todo: use data retrieved in the route
     const items = Ember.A();
-    items.addObject(DropdownItem.create({ id: 1, label: "Math"}));
-    items.addObject(DropdownItem.create({ id: 2, label: "Science"}));
-    items.addObject(DropdownItem.create({ id: 3, label: "History"}));
-    items.addObject(DropdownItem.create({ id: 4, label: "Language"}));
+    items.addObject(Ember.Object.create({ id: 1, name: "CCSS", title: "Common Core State Standard"}));
+    items.addObject(Ember.Object.create({ id: 2, name: "CA SS", title: "California State Standard"}));
+    items.addObject(Ember.Object.create({ id: 2, name: "NGSS", title: "Next Generation State Standard"}));
     return items;
   }.property(),
 
@@ -31,10 +30,18 @@ export default Ember.Controller.extend({
 
     /**
      * Triggered when a subject selection changes
-     * @param {{ id: string, name: string }} items
+     * @param {DropdownItem[]} items
      */
     onSubjectChange: function(items){
       console.debug(items);
+    },
+
+    /**
+     * Triggered when a standard selection changes
+     * @param {DropdownItem} item
+     */
+    onStandardSelected: function(item){
+      console.debug(item);
     }
   }
 
---
+++ tests/integration/components/standard-dropdown-test.js
@@ -0,0 +1,52 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import T from 'gooru-web/tests/helpers/assert';
+
+
+moduleForComponent('standard-dropdown', 'Integration | Component | standard dropdown', {
+  integration: true
+});
+
+test('Layout and interaction', function (assert) {
+  assert.expect(12);
+
+  const items = Ember.A();
+  items.addObject(Ember.Object.create({ id: 1, name: "CCSS", title: "Common Core State Standard"}));
+  items.addObject(Ember.Object.create({ id: 2, name: "CA SS", title: "California State Standard"}));
+  items.addObject(Ember.Object.create({ id: 2, name: "NGSS", title: "Next Generation State Standard"}));
+
+  this.set('items', items);
+
+  //setting context
+  this.on('myStandardSelectedAction', function(items){
+    assert.equal(items.length, 1, "Only one element should be selected");
+  });
+
+  this.render(hbs`{{standard-dropdown standards=items onStandardSelectedAction='myStandardSelectedAction'}}`);
+
+  const $component = this.$(),
+    $dropdown = $component.find(".standard-dropdown");
+
+  T.exists(assert, $dropdown, "Missing standard-dropdown");
+
+  //button group
+  const $buttonGroup = $dropdown.find(".btn-group");
+  T.exists(assert, $buttonGroup, "Missing btn-group");
+  assert.ok($buttonGroup.hasClass("btn-group-lg"), "Missing default btn group size class");
+  assert.ok($buttonGroup.hasClass("keep-open-no"), "Missing keep-open-no class");
+
+  //Dropdown menu
+  const $dropdownMenu = $dropdown.find(".dropdown-menu");
+  T.exists(assert, $dropdownMenu, "Missing dropdown menu");
+  assert.equal($dropdownMenu.find("li a.item").length, 3, "Missing items");
+  assert.equal($dropdownMenu.find("li a.item strong").length, 3, "Missing item name");
+  assert.equal($dropdownMenu.find("li a.item p").length, 3, "Missing item title");
+  T.notExists(assert, $dropdownMenu.find("li.disabled"), "Disabled items should not exists");
+  T.notExists(assert, $dropdownMenu.find("li.selected"), "Selected items should not exists");
+
+  //clicking at selectable item
+  $dropdownMenu.find("li a.item:eq(1)").click(); //CCSS
+  assert.equal($dropdownMenu.find("li.selected").length, 1, "Missing selected items");
+
+});
