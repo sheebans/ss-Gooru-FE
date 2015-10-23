b8186115234205481bd40fcda60d0cb0d4dbb890
Melany Delgado <melany.delgado@edify.cr>
2015-10-23 16:45:15 -0600

    Standards Popup Component

---
+++ tests/integration/components/standards-popup-test.js
@@ -0,0 +1,32 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import T from 'gooru-web/tests/helpers/assert';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+moduleForComponent('standards-popup', 'Integration | Component | search filter', {
+  integration: true,
+  beforeEach: function () {
+    this.container.lookup('service:i18n').set("locale","en");
+  }
+});
+
+test('it renders', function(assert) {
+  assert.expect(5);
+
+  const standards = Ember.A();
+
+  standards.addObject(Ember.Object.create({ id: 1, title: "CCSS.ELA-Literacy.RI.9-10.6", description: "Determine an author’s point of view or purpose in a text and analyze how an author uses rhetoric to advance that point of view or purpose."}));
+
+  this.set('standards', standards);
+
+  this.render(hbs`{{standards-popup standards=standards}}`);
+  var $component = this.$(); //component dom element
+
+  const $standardsPopupSection = $component.find(".standards");
+  T.exists(assert, $standardsPopupSection, "Missing standards popup");
+  T.exists(assert, $standardsPopupSection.find(".standard-title"), "Missing standard title");
+  assert.equal(T.text($standardsPopupSection.find('span')), "CCSS.ELA-Literacy.RI.9-10.6", "Incorrect standard title");
+
+  T.exists(assert, $standardsPopupSection.find(".standard-description"), "Missing standard description");
+  assert.equal(T.text($standardsPopupSection.find('p')), "Determine an author’s point of view or purpose in a text and analyze how an author uses rhetoric to advance that point of view or purpose.");
+});
