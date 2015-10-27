6cbb7fa73beb56cad44c93a93fda5a3000112b38
Jeffrey Bermúdez <jeffrey.bermudez@edify.cr>
2015-10-26 18:30:46 -0600

    GG-31: Adding some jsDoc and a test case for login with credentials

--- app/models/meta.js
+++ app/models/meta.js
@@ -1,8 +1,17 @@
 import DS from "ember-data";
 
+/**
+ * Model to represent the Meta (metadata) of a User
+ */
 export default DS.Model.extend({
 
+  /**
+   * @property {string} isFeaturedUser
+   */
   isFeaturedUser: DS.attr("string"),
+  /**
+   * @property {TaxonomyPreference} taxonomyPreference
+   */
   taxonomyPreference: DS.belongsTo("taxonomy-preference", { async: true })
 
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,33 +1,98 @@
 import DS from "ember-data";
 
+/**
+ * Model to represent the Users obtained from the end-point
+ */
 export default DS.Model.extend({
 
   // Values used to capture and to read data to/from SignUp end-point
+  /**
+   * @property {string} firstName
+   */
   firstName: DS.attr("string"),
+  /**
+   * @property {string} lastName
+   */
   lastName: DS.attr("string"),
+  /**
+   * @property {string} username
+   */
   username: DS.attr("string"),
+  /**
+   * @property {string} email
+   */
   email: DS.attr("string"),
+  /**
+   * @property {string} organization
+   */
   organization: DS.attr("string"),
 
   // Values only used to read data from SignUp end-point
+  /**
+   * @property {string} gooruId
+   */
   gooruUId: DS.attr("string"),
+  /**
+   * @property {string} usernameDispaly
+   */
   usernameDisplay: DS.attr("string"),
+  /**
+   * @property {string} profileImageUrl
+   */
   profileImageUrl: DS.attr("string"),
+  /**
+   * @property {string} userRoleSetString
+   */
   userRoleSetString: DS.attr("string"),
+  /**
+   * @property {string} accountCreatedType
+   */
   accountCreatedType: DS.attr("string"),
+  /**
+   * @property {number} accountTypeId
+   */
   accountTypeId: DS.attr("number"),
+  /**
+   * @property {number} active
+   */
   active: DS.attr("number"),
+  /**
+   * @property {number} confirmStatus
+   */
   confirmStatus: DS.attr("number"),
+  /**
+   * @property {string} createdOn
+   */
   createdOn: DS.attr("string"),
+  /**
+   * @property {string} partyUid
+   */
   partyUid: DS.attr("string"),
+  /**
+   * @property {number} viewFlag
+   */
   viewFlag: DS.attr("number"),
 
   // Values only used to capture data to be sent to SignUp end-point
+  /**
+   * @property {string} gender
+   */
   gender: DS.attr("string"),
+  /**
+   * @property {string} dateOfBirth
+   */
   dateOfBirth: DS.attr("string"),
+  /**
+   * @property {string} password
+   */
   password: DS.attr("string"),
+  /**
+   * @property {string} role
+   */
   role: DS.attr("string"),
-
+  /**
+   * @property {Meta} metadata
+   */
   metadata: DS.belongsTo("meta", { async: true })
 
 });
