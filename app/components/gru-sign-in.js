944cbf90f23cd35a353f04335486327e44c5fad6
quiaro <quiaro@gmail.com>
2015-11-06 12:29:53 -0600

    GG-62: Open user registration modal from login modal

--- app/components/gru-sign-in.js
+++ app/components/gru-sign-in.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
+import ModalMixin from '../mixins/modal';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(ModalMixin, {
 
   classNames:['gru-sign-in'],
 
