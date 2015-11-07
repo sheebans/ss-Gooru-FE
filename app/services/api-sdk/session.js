a7d761f036fee2882231c25dda442107830a47bd
quiaro <quiaro@gmail.com>
2015-11-06 18:43:50 -0600

    GG-81: Close sign in modal after logging in; remove unnecessary actions from app-header component

--- app/services/api-sdk/session.js
+++ app/services/api-sdk/session.js
@@ -4,15 +4,15 @@ export default Ember.Service.extend({
 
   session: Ember.inject.service("session"),
 
-
   /**
    * Creates a session with the specified user credentials
+   * @param {Ember.Object} credentials - Object with username and password attributes
    * @returns {*|Ember.RSVP.Promise}
    */
   signInWithUser: function(credentials) {
     return this.get("session").authenticate("authenticator:custom", {
-      username: credentials.username,
-      password: credentials.password,
+      username: credentials.get('username'),
+      password: credentials.get('password'),
       isDefaultUser: false
     });
   }
