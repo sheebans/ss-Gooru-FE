69bf93ec29993049ea1bfa03b8e3fc367367179f
Javier Perez <javier.perez@edify.cr>
2015-11-10 11:15:05 -0600

    GG-101 Updating vagrant provision

--- shell-provision.sh
+++ shell-provision.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 echo '-----> Update and Upgrade apt-get'
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
 sudo apt-get upgrade -qq -y
 
-echo '-----> Installing nodejs'
+echo '-----> Installing nodejs 4.x'
 sudo apt-get install nodejs -y -qq
 
 echo '-----> Installing ember-cli'
-npm install -g ember-cli -qq
+npm install -g ember-cli@1.13.8 -qq
 
 echo '-----> Installing bower'
-sudo npm install -g bower -qq
+sudo npm install -g bower@1.6.5 -qq
 
 echo '-----> Installing phantomjs'
-sudo npm install -g phantomjs -qq
+sudo npm install -g phantomjs@1.9.8 -qq
 sudo apt-get install libfontconfig -qq -y
 
 echo '-----> Installing stubby'
-sudo npm install -g stubby -qq -y
+sudo npm install -g stubby@0.2.13 -qq -y
 
 echo '-----> Installing grunt-cli'
-sudo npm install -g grunt-cli -qq -y
+sudo npm install -g grunt-cli@0.1.13 -qq -y
 
 echo '-----> Done'
 
