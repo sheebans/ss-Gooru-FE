ad226debfbad6a202629207cca3304900ce5a5e3
quiaro <quiaro@gmail.com>
2015-11-21 20:55:35 -0600

    GG-114: Add FIB question type to app config

--- app/config/config.js
+++ app/config/config.js
@@ -3,6 +3,7 @@ export const QUESTION_COMPONENT_MAP = {
   'MA'    : 'player.questions.gru-multiple-answer',
   'T/F'   : 'player.questions.gru-true-false',
   'OE'    : 'player.questions.gru-open-ended',
+  'FIB'   : 'player.questions.gru-fib',
   'HS_TXT': 'player.questions.gru-hs-text',
   'HS_IMG': 'player.questions.gru-hs-image',
   'HT_RO' : 'player.questions.gru-reorder',
