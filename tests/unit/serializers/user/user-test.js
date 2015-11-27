bb2f555357306666ed92a2ccb3c970ecd43f596c
Carlos Villalta <carlos.villalta@edify.cr>
2015-11-27 11:09:49 -0600

    Created tests for user serializer

--- tests/unit/serializers/user/user-test.js
+++ tests/unit/serializers/user/user-test.js
@@ -2,32 +2,22 @@ import { moduleForModel, test } from 'ember-qunit';
 import Pretender from 'pretender';
 
 const RESPONSE = {
-    'searchResult': [{
-      'classUid': '67a96ec1-7383-4164-8068-5415621b7a34',
-      'visibility': true,
-      'name': 'Class A2',
-      'classCode': 'JR48FMF',
-      'courseGooruOid': '75366215-f9d5-424c-8a90-2cabdfeb3ffa',
-      'grades': 'K,6,7,8',
-      'user': {
-        'username': 'JeffreyTeacher01',
-        'gooruUId': '88638002-deb6-4f8d-b319-4a7ae18d0efe',
-        'profileImageUrl': 'http://profile-qa.s3.amazonaws.com/88638002-deb6-4f8d-b319-4a7ae18d0efe.png'
-      },
-      'memberCount': 0
-    }, {
-      'classUid': '90d82226-5d0d-4673-a85d-f93aa0cbddf2',
-      'visibility': true,
-      'name': 'Class A1',
-      'classCode': '2WZ8IJA',
-      'grades': 'K',
-      'user': {
-        'username': 'JeffreyTeacher01',
-        'gooruUId': '88638002-deb6-4f8d-b319-4a7ae18d0efe',
-        'profileImageUrl': 'http://profile-qa.s3.amazonaws.com/88638002-deb6-4f8d-b319-4a7ae18d0efe.png'
-      },
-      'memberCount': 1
-    }], 'totalHitCount': 2
+  "accountCreatedType": "normal",
+  "accountTypeId": 3,
+  "active": 1,
+  "confirmStatus": 0,
+  "createdOn": "MonDec0106:25:15UTC2014",
+  "emailId": "sdffsgf@gmail.com",
+  "firstName": "developer",
+  "gooruUId": "006449b7-fd9a-457b-99b0-98e2cacf1536",
+  "lastName": "team",
+  "organizationName": "Gooru",
+  "partyUid": "006449b7-fd9a-457b-99b0-98e2cacf1536",
+  "profileImageUrl": "http://profile-demo.s3.amazonaws.com/profile-prod/006449b7-fd9a-457b-99b0-98e2cacf1536.png",
+  "userRoleSetString": "User",
+  "username": "developer2",
+  "usernameDisplay": "developer2",
+  "viewFlag": 0
 };
 var server;
 
@@ -36,7 +26,7 @@ moduleForModel('user/user', 'Unit | Serializer | user/user', {
 
   beforeEach() {
     server = new Pretender(function() {
-      this.get('/user/users', function(request) {
+      this.get('/user/users', function() {
         return [200, { 'Content-Type': 'application/json' }, JSON.stringify(RESPONSE)];
       });
     });
@@ -49,18 +39,27 @@ moduleForModel('user/user', 'Unit | Serializer | user/user', {
 
 test('it normalizes queryRecord response for user', function(assert) {
   return this.store().queryRecord('user/user', {
-    isStudent: true,
-    limit: 10,
-    offset: 0
-  }).then((classes) => {
-    assert.ok(classes);
-    assert.equal(classes.get('length'), 1);
-    var classItem = classes.objectAt(0);
-    assert.equal(classItem.get('id'), '90d82226-5d0d-4673-a85d-f93aa0cbddf2');
-    assert.equal(classItem.get('name'), 'Class A1');
-    assert.equal(classItem.get('code'), '2WZ8IJA');
-    assert.equal(classItem.get('grades'), 'K');
-    assert.equal(classItem.get('greetings'), 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
-    assert.equal(classItem.get('teachers')[0].username, 'JeffreyTeacher01');
+
+  }).then((users) => {
+    assert.ok(users);
+    //assert.equal(users.get('length'), 1, 'length is not one '+users.get('length'));
+    var userItem = users.objectAt(0);
+    assert.equal(userItem.get('id'), '006449b7-fd9a-457b-99b0-98e2cacf1536', 'Wrong id');
+    assert.equal(userItem.get('accountCreatedType'), 'normal', 'wrong accountCreatedType');
+    assert.equal(userItem.get('accountTypeId'), '3', 'wrong accountTypeId');
+    assert.equal(userItem.get('active'), '1', 'wrong active');
+    assert.equal(userItem.get('confirmStatus'), '0', 'wrong confirmStatus');
+    assert.equal(userItem.get('createdOn'), 'MonDec0106:25:15UTC2014', 'wrong createdOn');
+    assert.equal(userItem.get('email'), 'sdffsgf@gmail.com', 'wrong email');
+    assert.equal(userItem.get('firstName'), 'developer', 'wrong first name');
+    assert.equal(userItem.get('gooruUId'), '006449b7-fd9a-457b-99b0-98e2cacf1536', 'wrong gooruUId');
+    assert.equal(userItem.get('lastName'), 'team', 'wrong lastName');
+    assert.equal(userItem.get('organizationName'), 'Gooru', 'wrong forganizationName');
+    assert.equal(userItem.get('partyUid'), '006449b7-fd9a-457b-99b0-98e2cacf1536', 'wrong partyUid');
+    assert.equal(userItem.get('profileImageUrl'), 'http://profile-demo.s3.amazonaws.com/profile-prod/006449b7-fd9a-457b-99b0-98e2cacf1536.png', 'wrong profileImageUrl');
+    assert.equal(userItem.get('userRoleSetString'), 'User', 'wrong userRoleSetString');
+    assert.equal(userItem.get('username'), 'developer2', 'username');
+    assert.equal(userItem.get('usernameDisplay'), 'developer2', 'wrong usernameDisplay');
+    assert.equal(userItem.get('viewFlag'), '0', 'wrong viewFlag');
   });
 });
