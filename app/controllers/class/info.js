c3edda609e48979f784b67af4d72b94296fa9cad
Javier Perez <javier.perez@edify.cr>
2015-11-27 10:38:56 -0600

    GG-134 Adding alias for class in children controllers

---
+++ app/controllers/class/info.js
@@ -0,0 +1,38 @@
+import Ember from "ember";
+
+/**
+ * Class Information controller
+ *
+ * Controller responsible of the logic for the class information page
+ */
+export default Ember.Controller.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+  classController: Ember.inject.controller('class'),
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+
+  // -------------------------------------------------------------------------
+  // Properties
+  /**
+   * A link to the parent class controller
+   * @see controllers/class.js
+   * @property {Class}
+   */
+  "class": Ember.computed.reads('classController.class')
+
+  // -------------------------------------------------------------------------
+  // Observers
+
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+
+});
