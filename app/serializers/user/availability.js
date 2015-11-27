06d71d2e8275cc775fd694fc61f56929e0e3b042
Carlos Villalta <carlos.villalta@edify.cr>
2015-11-27 14:16:24 -0600

    GG-84 Finished adapter tests and availability tests

--- app/serializers/user/availability.js
+++ app/serializers/user/availability.js
@@ -3,9 +3,10 @@ import DS from "ember-data";
 export default DS.JSONAPISerializer.extend({
 
   normalizeSingleResponse: function(store, primaryModelClass, payload) {
+
     var availabilityModel =  {
       data: {
-        id: payload.gooruId ? payload.gooruId: '_availability_',
+        id: payload.gooruUId ? payload.gooruUId: '_availability_',
         type: "user/availability",
         attributes: {
           confirmStatus: payload.confirmStatus,
--- tests/unit/serializers/user/availability-test.js
+++ tests/unit/serializers/user/availability-test.js
@@ -1,8 +1,16 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from "ember";
 
 moduleFor('serializer:user/availability', 'Unit | Serializer | user/availability');
 
 test('normalizeSingleResponse', function (assert) {
+  assert.expect(2);
+
+  var store = Ember.Object.create({
+    push: function(){
+      assert.ok(true, "This should be called once");
+    }
+  });
   const serializer = this.subject();
 
   const
@@ -12,10 +20,9 @@ test('normalizeSingleResponse', function (assert) {
       "collaboratorCheck": false,
       "availability": true
     },
-    response = serializer.normalizeSingleResponse("any store", "user/availability", payload);
+    response = serializer.normalizeSingleResponse(store, "user/availability", payload);
 
   const expected = {
-
     "data": {
       "id": "1726fb25-2f09-4968-b86b-c170bd58ac4c",
       "type": "user/availability",
@@ -25,9 +32,41 @@ test('normalizeSingleResponse', function (assert) {
         "availability": true
       }
     }
-
   };
 
-  assert.deepEqual(response, expected, "Wrong response");
+  assert.deepEqual(response, expected, "Wrong response: "+ JSON.stringify(response)+ "\n expected: "+JSON.stringify(expected));
+});
 
+
+test('normalizeSingleResponse on negative response', function (assert) {
+  assert.expect(2);
+
+  var store = Ember.Object.create({
+    push: function(){
+      assert.ok(true, "This should be called once");
+    }
+  });
+  const serializer = this.subject();
+
+  const
+    payload = {
+      "confirmStatus": 0,
+      "collaboratorCheck": false,
+      "availability": false
+    },
+    response = serializer.normalizeSingleResponse(store, "user/availability", payload);
+
+  const expected = {
+    "data": {
+      "id": "_availability_",
+      "type": "user/availability",
+      "attributes": {
+        "confirmStatus": 0,
+        "collaboratorCheck": false,
+        "availability": false
+      }
+    }
+  };
+
+  assert.deepEqual(response, expected, "Wrong response: "+ JSON.stringify(response)+ "\n expected: "+JSON.stringify(expected));
 });
