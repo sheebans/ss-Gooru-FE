79dc88f815a297431542024c80e2749236eda8bd
Jeffrey A. Bermudez P <jeffrey.bermudez@edify.cr>
2015-11-27 19:15:46 -0600

    GG-132_adding_backend_support_for_class_info: Adding a stubby endpoint for class info.

--- app/services/api-sdk/class.js
+++ app/services/api-sdk/class.js
@@ -11,12 +11,12 @@ export default Ember.Service.extend(StoreMixin, {
    * @param {Object} options request options, like limit, offset, etc
    * @returns {Promise.<Class[]>}
    */
-  findClassesIJoined: function(options = {}) {
+  findClassesIJoined: function (options = {}) {
     return this.get('store').queryRecord('class/class', {
       isStudent: true,
       limit: (options.limit ? options.limit : 10),
       offset: (options.offset ? options.offset : 0)
-    }).then(function(response) {
+    }).then(function (response) {
       return response;
     }, function (error) {
       Ember.Logger.error('Error querying Classes I Joined: %s', error.message);
@@ -29,12 +29,12 @@ export default Ember.Service.extend(StoreMixin, {
    * @param {Object} options request options, like limit, offset, etc
    * @returns {Promise.<Class[]>}
    */
-  findClassesITeach: function(options = {}) {
+  findClassesITeach: function (options = {}) {
     return this.get('store').queryRecord('class/class', {
       limit: (options.limit ? options.limit : 10),
       offset: (options.offset ? options.offset : 0),
       'flt.exclude.empty.course': (options['flt.exclude.empty.course'] ? options['flt.exclude.empty.course'] : false)
-    }).then(function(response) {
+    }).then(function (response) {
       return response;
     }, function (error) {
       Ember.Logger.error('Error querying Classes I Teach: %s', error.message);
@@ -47,9 +47,9 @@ export default Ember.Service.extend(StoreMixin, {
    * @param {string} id
    * @returns {Promise.<Class>}
    */
-  findById: function(id){
+  findById: function (id) {
     return this.get('store').findRecord('class/class', id)
-      .then(function(response) {
+      .then(function (response) {
         return response;
       }, function (error) {
         Ember.Logger.error('Error finding Class by Id: %s', error.message);
