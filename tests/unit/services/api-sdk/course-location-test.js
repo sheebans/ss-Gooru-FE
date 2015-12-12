5507ef123796ae6dd0d78fab1b05a0869cec42ec
Jeffrey A. Bermudez P <jeffrey.bermudez@edify.cr>
2015-12-11 19:14:17 -0600

    GG-140: Adding temporal options param to CourseLocation SDK and modifying test to support new change.

--- tests/unit/services/api-sdk/course-location-test.js
+++ tests/unit/services/api-sdk/course-location-test.js
@@ -34,12 +34,18 @@ test('findByCourse', function (assert) {
   const service = this.subject();
   var done = assert.async();
   Ember.run(function() {
-    const promise = service.findByCourse('course-with-45-users');
+    const promise = service.findByCourse('the-course-id', {
+      units: [
+        {id: 'unit-1'},
+        {id: 'unit-2'},
+        {id: 'unit-3'}
+      ]
+    });
     promise.then(function(courseLocations) {
       assert.equal(courseLocations.get('length'), 3, 'Missing course locations');
       const courseLocation = courseLocations.get('firstObject');
       assert.equal(courseLocation.get('unit'), 'unit-1', 'Wrong unit');
-      assert.equal(courseLocation.get('locationUsers.length'), 45, 'Missing users');
+      assert.equal(courseLocation.get('locationUsers.length'), 1, 'Missing users');
       const locationUser = courseLocation.get('locationUsers.firstObject');
       assert.equal(locationUser.get('isActive'), true, 'Wrong active status');
       assert.equal(locationUser.get('user.id'), 'id-1', 'Wrong user id');
@@ -56,13 +62,21 @@ test('findByCourseAndUnit', function (assert) {
   const service = this.subject();
   var done = assert.async();
   Ember.run(function() {
-    const promise = service.findByCourseAndUnit('course-with-30-users', 'unit-1');
+    const promise = service.findByCourseAndUnit('the-course-id', 'unit-1', {
+      lessons: [
+        {id: 'lesson-1'},
+        {id: 'lesson-2'},
+        {id: 'lesson-3'},
+        {id: 'lesson-4'},
+        {id: 'lesson-5'}
+      ]
+    });
     promise.then(function(courseLocations) {
-      assert.equal(courseLocations.get('length'), 2, 'Missing course locations');
+      assert.equal(courseLocations.get('length'), 5, 'Missing course locations');
       const courseLocation = courseLocations.get('firstObject');
       assert.equal(courseLocation.get('unit'), 'unit-1', 'Wrong unit');
       assert.equal(courseLocation.get('lesson'), 'lesson-1', 'Wrong lesson');
-      assert.equal(courseLocation.get('locationUsers.length'), 30, 'Missing users');
+      assert.equal(courseLocation.get('locationUsers.length'), 1, 'Missing users');
       const locationUser = courseLocation.get('locationUsers.firstObject');
       assert.equal(locationUser.get('isActive'), true, 'Wrong active status');
       assert.equal(locationUser.get('user.id'), 'id-1', 'Wrong user id');
@@ -70,6 +84,17 @@ test('findByCourseAndUnit', function (assert) {
       assert.equal(locationUser.get('user.lastName'), 'lastname-1', 'Wrong user lastName');
       assert.equal(locationUser.get('user.email'), 'user_1@test.com', 'Wrong user email');
       assert.equal(locationUser.get('user.username'), 'username-1', 'Wrong user username');
+      const courseLocationLast = courseLocations.get('lastObject');
+      assert.equal(courseLocationLast.get('unit'), 'unit-1', 'Wrong unit');
+      assert.equal(courseLocationLast.get('lesson'), 'lesson-5', 'Wrong lesson');
+      assert.equal(courseLocationLast.get('locationUsers.length'), 45, 'Missing users');
+      const locationUserLast = courseLocationLast.get('locationUsers.lastObject');
+      assert.equal(locationUserLast.get('isActive'), true, 'Wrong active status');
+      assert.equal(locationUserLast.get('user.id'), 'id-45', 'Wrong user id');
+      assert.equal(locationUserLast.get('user.firstName'), 'firstname-45', 'Wrong user firstName');
+      assert.equal(locationUserLast.get('user.lastName'), 'lastname-45', 'Wrong user lastName');
+      assert.equal(locationUserLast.get('user.email'), 'user_45@test.com', 'Wrong user email');
+      assert.equal(locationUserLast.get('user.username'), 'username-45', 'Wrong user username');
       done();
     });
   });
@@ -79,14 +104,21 @@ test('findByCourseAndUnitAndLesson', function (assert) {
   const service = this.subject();
   var done = assert.async();
   Ember.run(function() {
-    const promise = service.findByCourseAndUnitAndLesson('course-with-3-users', 'unit-1', 'lesson-1');
+    const promise = service.findByCourseAndUnitAndLesson('the-course-id', 'unit-1', 'lesson-1', {
+      collections: [
+        {id: 'collection-1'},
+        {id: 'collection-2'},
+        {id: 'collection-3'},
+        {id: 'collection-4'}
+      ]
+    });
     promise.then(function(courseLocations) {
-      assert.equal(courseLocations.get('length'), 3, 'Missing course locations');
+      assert.equal(courseLocations.get('length'), 4, 'Missing course locations');
       const courseLocation = courseLocations.get('firstObject');
       assert.equal(courseLocation.get('unit'), 'unit-1', 'Wrong unit');
       assert.equal(courseLocation.get('lesson'), 'lesson-1', 'Wrong lesson');
       assert.equal(courseLocation.get('collection'), 'collection-1', 'Wrong collection');
-      assert.equal(courseLocation.get('locationUsers.length'), 3, 'Missing users');
+      assert.equal(courseLocation.get('locationUsers.length'), 1, 'Missing users');
       const locationUser = courseLocation.get('locationUsers.firstObject');
       assert.equal(locationUser.get('isActive'), true, 'Wrong active status');
       assert.equal(locationUser.get('user.id'), 'id-1', 'Wrong user id');
@@ -94,6 +126,18 @@ test('findByCourseAndUnitAndLesson', function (assert) {
       assert.equal(locationUser.get('user.lastName'), 'lastname-1', 'Wrong user lastName');
       assert.equal(locationUser.get('user.email'), 'user_1@test.com', 'Wrong user email');
       assert.equal(locationUser.get('user.username'), 'username-1', 'Wrong user username');
+      const courseLocationLast = courseLocations.get('lastObject');
+      assert.equal(courseLocationLast.get('unit'), 'unit-1', 'Wrong unit');
+      assert.equal(courseLocationLast.get('lesson'), 'lesson-1', 'Wrong lesson');
+      assert.equal(courseLocationLast.get('collection'), 'collection-4', 'Wrong collection');
+      assert.equal(courseLocationLast.get('locationUsers.length'), 30, 'Missing users');
+      const locationUserLast = courseLocationLast.get('locationUsers.lastObject');
+      assert.equal(locationUserLast.get('isActive'), true, 'Wrong active status');
+      assert.equal(locationUserLast.get('user.id'), 'id-30', 'Wrong user id');
+      assert.equal(locationUserLast.get('user.firstName'), 'firstname-30', 'Wrong user firstName');
+      assert.equal(locationUserLast.get('user.lastName'), 'lastname-30', 'Wrong user lastName');
+      assert.equal(locationUserLast.get('user.email'), 'user_30@test.com', 'Wrong user email');
+      assert.equal(locationUserLast.get('user.username'), 'username-30', 'Wrong user username');
       done();
     });
   });
