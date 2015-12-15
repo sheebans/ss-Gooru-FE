a9378052b5895740c3569df2c1e871b59db5261d
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2015-12-15 15:30:16 -0600

    GG-185: Class Teacher Analytics - Performance scale indicator

---
+++ app/components/class/analytics/performance/gru-scale-indicator.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+/**
+ * Class actions bar
+ *
+ * Component responsible for showing the action bar in my class analytics page.
+ *
+ * @module
+ * @see controllers/class.js
+ * @augments ember/Component
+ */
+export default Ember.Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  classNames: ['gru-scale-indicator'],
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  // -------------------------------------------------------------------------
+
+  // Methods
+
+});
