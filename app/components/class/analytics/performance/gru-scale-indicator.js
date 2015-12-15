26a68b2534b12d17e46814aaff08bffa4c17777a
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2015-12-15 15:54:31 -0600

    GG-185: Class Teacher Analytics - Performance scale indicator (PR feedback)

--- app/components/class/analytics/performance/gru-scale-indicator.js
+++ app/components/class/analytics/performance/gru-scale-indicator.js
@@ -6,7 +6,6 @@ import Ember from 'ember';
  * Component responsible for showing the Performance Scale Indicator in the teacher page.
  *
  * @module
- * @see controllers/teacher.js
  * @augments ember/Component
  */
 export default Ember.Component.extend({
