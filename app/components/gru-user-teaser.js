30910b5ddd9014c0303e3ac14ea040044182110e
Melany Delgado <melany.delgado@edify.cr>
2015-12-29 11:26:25 -0600

    Application Cards - Users link component

--- app/components/gru-user-teaser.js
+++ app/components/gru-user-teaser.js
@@ -25,31 +25,7 @@ export default Ember.Component.extend({
    * List of users to show
    * @property {Array}
    */
-  "users":  Ember.A([Ember.Object.create({
-    'email': 'user_1@test.com',
-    'firstName': 'firstname-1',
-    'fullName': 'lastname-1 firstname-1',
-    'id': 'id-1',
-    'lastName': 'lastname-1',
-    'profileImageUrl': '/assets/gooru/profile.png',
-    'username': 'username-1'
-  }),Ember.Object.create({
-    'email': 'user_2@test.com',
-    'firstName': 'firstname-2',
-    'fullName': 'lastname-2 firstname-2',
-    'id': 'id-2',
-    'lastName': 'lastname-2',
-    'profileImageUrl': '/assets/gooru/profile.png',
-    'username': 'username-2'
-  }),Ember.Object.create({
-    'email': 'user_1@test.com',
-    'firstName': 'firstname-3',
-    'fullName': 'lastname-3 firstname-3',
-    'id': 'id-1',
-    'lastName': 'lastname-3',
-    'profileImageUrl': '/assets/gooru/profile.png',
-    'username': 'username-3'
-  })]),
+  "users": null,
 
   /**
    * Get the first user from the users list
