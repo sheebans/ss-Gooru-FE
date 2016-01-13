2cca95d9869b07cbff453e81d3d579a29632c58d
Melany Delgado <melany.delgado@edify.cr>
2016-01-13 11:07:42 -0600

    Course card layout changes

--- tests/integration/components/cards/gru-course-card-test.js
+++ tests/integration/components/cards/gru-course-card-test.js
@@ -14,8 +14,9 @@ test('Course Card Layout', function(assert) {
   var course = Ember.Object.create({
     'title': 'Water cycle',
     'totalUnits': 8,
-    'subject': 'Science',
+    'subjects': ['Science'],
     'imageUrl': '/assets/gooru/profile.png',
+    'isPublic':true,
     'remixedBy':  Ember.A([Ember.Object.create({
       'email': 'user_1@test.com',
       'firstName': 'firstname-1',
@@ -44,7 +45,7 @@ test('Course Card Layout', function(assert) {
   });
 
   this.set('course', course);
-  assert.expect(7);
+  assert.expect(8);
   this.render(hbs`{{cards/gru-course-card course=course}}`);
 
   var $component = this.$(); //component dom element
@@ -53,16 +54,61 @@ test('Course Card Layout', function(assert) {
   T.exists(assert, $courseCard.find("h3"), "Missing course card title");
   T.exists(assert, $courseCard.find(".total-units"), "Missing total units");
   T.exists(assert, $courseCard.find(".subject"), "Missing subject");
+  T.exists(assert, $courseCard.find(".icon"), "Missing icon");
   T.exists(assert, $courseCard.find(".remixed"), "Missing Remixed By");
   T.exists(assert, $courseCard.find(".users-teaser"), "Missing users teaser");
   T.exists(assert, $courseCard.find(".remix-button  button"), "Missing remix button");
+
+});
+test('Course Card Private', function(assert) {
+  var course = Ember.Object.create({
+    'title': 'Water cycle',
+    'totalUnits': 8,
+    'subjects': ['Science'],
+    'imageUrl': '/assets/gooru/profile.png',
+    'isPublic':false,
+    'remixedBy':  Ember.A([Ember.Object.create({
+      'email': 'user_1@test.com',
+      'firstName': 'firstname-1',
+      'fullName': 'lastname-1 firstname-1',
+      'id': 'id-1',
+      'lastName': 'lastname-1',
+      'avatarUrl': '/assets/gooru/profile.png',
+      'username': 'username-1'
+    }),Ember.Object.create({
+      'email': 'user_2@test.com',
+      'firstName': 'firstname-2',
+      'fullName': 'lastname-2 firstname-2',
+      'id': 'id-2',
+      'lastName': 'lastname-2',
+      'avatarUrl': '/assets/gooru/profile.png',
+      'username': 'username-2'
+    }),Ember.Object.create({
+      'email': 'user_1@test.com',
+      'firstName': 'firstname-3',
+      'fullName': 'lastname-3 firstname-3',
+      'id': 'id-1',
+      'lastName': 'lastname-3',
+      'avatarUrl': '/assets/gooru/profile.png',
+      'username': 'username-3'
+    })])
+  });
+
+  this.set('course', course);
+  assert.expect(1);
+  this.render(hbs`{{cards/gru-course-card course=course}}`);
+
+  var $component = this.$(); //component dom element
+  const $courseCard = $component.find(".gru-course-card");
+  T.notExists(assert, $courseCard.find(".icon"), "Icon should not appear");
+
 
 });
 test('Click Title and Image', function(assert) {
   var course = Ember.Object.create({
     'title': 'Water cycle',
     'totalUnits': 8,
-    'subject': 'Science',
+    'subjects': ['Science'],
     'imageUrl': '/assets/gooru/profile.png',
     'remixedBy':  Ember.A([Ember.Object.create({
       'email': 'user_1@test.com',
@@ -109,8 +155,9 @@ test('Click Remix', function(assert) {
   var course = Ember.Object.create({
     'title': 'Water cycle',
     'totalUnits': 8,
-    'subject': 'Science',
+    'subjects': ['Science'],
     'imageUrl': '/assets/gooru/profile.png',
+    'isPublic':false,
     'remixedBy':  Ember.A([Ember.Object.create({
       'email': 'user_1@test.com',
       'firstName': 'firstname-1',
