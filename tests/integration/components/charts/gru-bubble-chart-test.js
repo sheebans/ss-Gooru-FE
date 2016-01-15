3f9a8378125a33bdf83ca5239c56e52d593e8ab4
Fabian <fabian.perez@edify.cr>
2016-01-15 16:22:04 -0600

    Fixing issues

--- tests/integration/components/charts/gru-bubble-chart-test.js
+++ tests/integration/components/charts/gru-bubble-chart-test.js
@@ -3,29 +3,28 @@ import hbs from 'htmlbars-inline-precompile';
 import T from 'gooru-web/tests/helpers/assert';
 
 moduleForComponent('charts/gru-bubble-chart', 'Integration | Component | charts/gru bubble chart', {
-  integration: true,
-  beforeEach: function () {
-    this.container.lookup('service:i18n').set("locale","en");
-  }
+  integration: true
 });
 
 test('Bubble Chart Layout', function(assert) {
-  assert.expect(2);
 
-  const scorePercentage = '80%';
-  this.set('scorePercentage', scorePercentage);
+  const content = '80%';
+  this.set('content', content);
 
-  const backgroundColor = '#00e100';
-  this.set('backgroundColor', backgroundColor);
+  const color = '#00e100';
+  this.set('color', color);
 
-  this.render(hbs`{{charts.gru-bubble-chart scorePercentage=scorePercentage backgroundColor=backgroundColor}}`);
+  this.render(hbs`{{charts.gru-bubble-chart content=content color=color}}`);
 
   const $component = this.$(); //component dom element
   const $completionBubbleChart = $component.find(".gru-bubble-chart");
-  T.exists(assert, $completionBubbleChart, 'Missing bubble chart component');
+  T.exists(assert, $completionBubbleChart, 'Bubble chart component exits');
 
   const $completionCircle = $component.find(".bubble-circle");
-  T.exists(assert, $completionCircle, 'Missing circle');
+  T.exists(assert, $completionCircle, 'Circle exits');
+
+  const $contentTest = $component.find(".bubble-circle span");
+  assert.equal($contentTest.text().trim(), "80%", "Correct content");
 
 });
 
