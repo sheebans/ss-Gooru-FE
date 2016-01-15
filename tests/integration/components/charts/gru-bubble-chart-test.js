f7a389cb4fc2a087301cd26230a556414402d54f
Fabian <fabian.perez@edify.cr>
2016-01-15 15:00:50 -0600

    Fixing test

--- tests/integration/components/charts/gru-bubble-chart-test.js
+++ tests/integration/components/charts/gru-bubble-chart-test.js
@@ -22,7 +22,7 @@ test('Bubble Chart Layout', function(assert) {
 
   const $component = this.$(); //component dom element
   const $completionBubbleChart = $component.find(".gru-bubble-chart");
-  T.exists(assert, $completionInformationChart, 'Missing bubble chart component');
+  T.exists(assert, $completionBubbleChart, 'Missing bubble chart component');
 
   const $completionCircle = $component.find(".bubble-circle");
   T.exists(assert, $completionCircle, 'Missing circle');
