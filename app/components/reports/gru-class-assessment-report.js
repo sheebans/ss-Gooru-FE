32ad7d8c88cddceeea95a9f87f76ae7b2b20ca47
quiaro <quiaro@gmail.com>
2016-01-15 08:50:27 -0600

    GG-255: Define models and mock data for the class assessment report

--- app/components/reports/gru-class-assessment-report.js
+++ app/components/reports/gru-class-assessment-report.js
@@ -1,3 +1,33 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({});
+export default Ember.Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  classNames: ['reports', 'gru-class-assessment-report'],
+
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  /**
+   * Model for the assessment taken by a group of students
+   * @prop { Collection }
+   */
+  collection: null,
+
+  /**
+   * Group of students taking an assessment
+   * @prop { User[] }
+   */
+  users: null,
+
+  /**
+   * List of results for every question in the assessment for each student in the class
+   * @prop { User[] }
+   */
+  results: null
+
+
+});
