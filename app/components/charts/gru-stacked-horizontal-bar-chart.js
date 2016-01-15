dcf257c57228b8a1b8679b5c7a87f35f8b508337
Melany Delgado <melany.delgado@edify.cr>
2016-01-15 16:19:32 -0600

    Stacked horizontal bar chart component

---
+++ app/components/charts/gru-stacked-horizontal-bar-chart.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+/**
+ * Stacked Horizontal Bar Chart
+ *
+ * Component responsible for showing the stacked horizontal bar chart.
+ * This component takes the dimensions of height and width from the parent element.
+ *
+ * @module
+ * @augments ember/Component
+ */
+export default Ember.Component.extend({
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  classNames: ['charts','gru-stacked-horizontal-bar-chart'],
+
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  stackedBarData:null,
+
+  // -------------------------------------------------------------------------
+  // Events
+
+
+  didInsertElement: function(){
+    if(!this.validValues()){
+      Ember.Logger.warn('Graph values sum more than 100');
+    }
+  },
+
+  /*
+  * Check if the values are up 100%
+  */
+  validValues:function(){
+  var sum = this.get("stackedBarData").reduce(function(previousValue, value){
+    return previousValue + parseInt(value.percentage);
+  }, 0);
+  return (sum <= 100);
+}
+
+});
---
+++ tests/integration/components/charts/gru-stacked-horizontal-bar-chart-test.js
@@ -0,0 +1,32 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import T from 'gooru-web/tests/helpers/assert';
+import Ember from 'ember';
+
+moduleForComponent('charts/gru-stacked-horizontal-bar-chart', 'Integration | Component | charts/gru stacked horizontal bar chart', {
+  integration: true,
+  beforeEach: function () {
+    this.container.lookup('service:i18n').set("locale","en");
+  }
+});
+
+test('Stacked horizontal bar chart layout', function(assert) {
+  //assert.expect(5);
+
+  var data= Ember.A([Ember.Object.create({
+    color: "#00e100",
+    percentage: "20",
+  }),Ember.Object.create({
+    color: "#ff5a5a",
+    percentage: "35"
+  })]);
+
+  this.set('data', data);
+  this.render(hbs`{{charts/gru-stacked-horizontal-bar-chart stackedBarData=data}}`);
+  const $component = this.$(); //component dom element
+  const $horizontalBarChart = $component.find(".gru-stacked-horizontal-bar-chart");
+
+  T.exists(assert, $horizontalBarChart, 'Missing stacked horizontal bar chart component');
+  assert.equal($horizontalBarChart.find('.segment:nth-child(1)').attr("style"),"background-color: "+data[0].color+";width: "+data[0].percentage+"%;", "Incorrect width and color for the first segment");
+  assert.equal($horizontalBarChart.find('.segment:nth-child(2)').attr("style"),"background-color: "+data[1].color+";width: "+data[1].percentage+"%;", "Incorrect width and color for the second segment");
+});
