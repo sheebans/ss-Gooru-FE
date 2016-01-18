d044adf9e67048cab0914dda6f56cbc504d0d3fd
Melany Delgado <melany.delgado@edify.cr>
2016-01-18 13:56:42 -0600

    Fractional Helper

--- app/helpers/fractional.js
+++ app/helpers/fractional.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
-
+/**
+ * Show fractions values like "3/4" more friendly.
+ *
+ * @example
+ *
+ * <span class="fractional fraction">{{{fractional numerator=1 denominator=2}}}</span>
+ * or
+ * <span class="fractional fraction">{{{fractional expression="1/3"}}}</span>
+ * @see /app/templates/components
+ *
+ * @param numerator {Number}
+ * @param denominator {Number}
+ * @param expression {String} example: "1/3" Have priority to get the numerator and denominator.
+ * @returns {String} - fractional value is presented friendly
+ */
 export function fractional(params,{ numerator, denominator,expression }) {
   var num = numerator;
   var den = denominator;
@@ -11,7 +25,7 @@ export function fractional(params,{ numerator, denominator,expression }) {
       den=split[1];
     }
   }
-  return ('<span class="top">'+num+'</span><span class="bottom">'+den+'</span>');
+  return Ember.String.htmlSafe('<span class="top">'+num+'</span><span class="bottom">'+den+'</span>');
 }
 
 export default Ember.Helper.helper(fractional);
