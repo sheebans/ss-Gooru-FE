3ba7bdf07349469a1a504eaec5e0ba796746d206
Melany Delgado <melany.delgado@edify.cr>
2016-01-19 13:55:28 -0600

    Assessment Report - Question Details - Multiple Choice

--- app/components/reports/assessment/questions/gru-multiple-choice.js
+++ app/components/reports/assessment/questions/gru-multiple-choice.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import {MultipleChoiceUtil} from 'gooru-web/utils/questions';
 
 export default Ember.Component.extend({
   // -------------------------------------------------------------------------
