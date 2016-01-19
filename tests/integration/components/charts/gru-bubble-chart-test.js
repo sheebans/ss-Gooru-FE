bc74f683d7eae69be6059bc6026eafa2c01eff7d
Fabian <fabian.perez@edify.cr>
2016-01-19 09:51:28 -0600

    Adding necessary changes

--- tests/integration/components/charts/gru-bubble-chart-test.js
+++ tests/integration/components/charts/gru-bubble-chart-test.js
@@ -19,12 +19,14 @@ test('Bubble Chart Layout', function(assert) {
   const $component = this.$(); //component dom element
   const $completionBubbleChart = $component.find(".gru-bubble-chart");
   T.exists(assert, $completionBubbleChart, 'Missing Bubble chart component');
+  assert.equal($completionBubbleChart.find(':nth-child(1)').attr("style"),"background-color:"+color,"Incorrect color for the circle");
 
   const $completionCircle = $component.find(".bubble-circle");
   T.exists(assert, $completionCircle, 'Missing circle');
 
   const $contentTest = $component.find(".bubble-circle span");
   assert.equal($contentTest.text().trim(), "80%", "Content incorrect");
+
 
 });
 
