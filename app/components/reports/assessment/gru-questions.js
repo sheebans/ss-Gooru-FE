d643459a815c554b640d0ca8f0844d0d1977c5d0
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2016-02-12 14:26:54 -0600

    GG-489: Branding - Student Assessment Report (WIth Questions)

--- app/components/reports/assessment/gru-questions.js
+++ app/components/reports/assessment/gru-questions.js
@@ -42,20 +42,5 @@ export default Ember.Component.extend({
    *
    * @property {Boolean}
    */
-  showPerformance: true,
-  /**
-   * List of layouts to be displayed by the switch component
-   *
-   * @constant {Array}
-   */
-  switchOptions: Ember.computed(function(){
-    var component =this;
-    return Ember.A([Ember.Object.create({
-      label: component.get('i18n').t('common.show-correct-answer'),
-      value: "some-value"
-    }),Ember.Object.create({
-      label: component.get('i18n').t('common.performance'),
-      value: "some-value"
-    })]);
-  })
+  showPerformance: true
 });
--- tests/integration/components/reports/assessment/gru-questions-test.js
+++ tests/integration/components/reports/assessment/gru-questions-test.js
@@ -60,53 +60,53 @@ test('Questions Layout', function (assert) {
   assert.equal($question.find('table tbody tr').length,2, "Incorrect number of rows");
 });
 
-test('Switch Options', function (assert) {
-  assert.expect(15);
-
-  const questions = Ember.A([Ember.Object.create({
-    question: Ember.Object.create({
-      text:"This is a question 1",
-      questionType: 'OE',
-      order: 1
-    }),
-    correct: true,
-    timeSpent: 10, //seconds
-    reaction: 5,
-    answer: "answer"
-  }), Ember.Object.create({
-    question: Ember.Object.create({
-      text:"This is a question 2",
-      order: 2,
-      questionType: 'OE'
-    }),
-    correct: false,
-    timeSpent: 25, //seconds
-    reaction: 2,
-    answer: "answer"
-  })]);
-
-  this.set('questions', questions);
-  this.render(hbs`{{reports/assessment/gru-questions results=questions}}`);
-  const $component = this.$(); //component dom element
-  const $question = $component.find(".gru-questions");
-  const $switch = $question.find('.switch-section a');
-  $switch.click();//Show correct answer
-  T.exists(assert, $question.find('table thead th.header.score.hide'), 'Score header should be hide');
-  T.exists(assert, $question.find('table thead th.header.time-spent.hide'), 'Time spent header should be hide');
-  T.exists(assert, $question.find('table thead th.header.reaction.hide'), 'Reaction header should be hide');
-  T.exists(assert, $question.find('table thead th.header.correct-answer.visible'), 'Correct answer header should be visible');
-  T.exists(assert, $question.find('table tbody td.question-score.hide'), 'Score column should be hide');
-  T.exists(assert, $question.find('table tbody td.question-time.hide'), 'Time spent column should be hide');
-  T.exists(assert, $question.find('table tbody td.question-reaction.hide'), 'Reaction column should be hide');
-  T.exists(assert, $question.find('table tbody td.correct-answer.visible'), 'Correct answer column should be visible');
-  T.exists(assert, $question.find('table tbody td.correct-answer.visible:eq(0) .gru-open-ended'), 'Correct answer column should be visible');
-
-  $switch.click();//Show performance
-  T.exists(assert, $question.find('table th.header.score.visible'), 'Score header should be visible');
-  T.exists(assert, $question.find('table th.header.time-spent.visible'), 'Time spent header should be visible');
-  T.exists(assert, $question.find('table thead th.header.reaction.visible'), 'Reaction header should be visible');
-  T.exists(assert, $question.find('table tbody td.question-score.visible'), 'Score column should be visible');
-  T.exists(assert, $question.find('table tbody td.question-time.visible'), 'Time spent column should be visible');
-  T.exists(assert, $question.find('table tbody td.question-reaction.visible'), 'Reaction column should be visible');
-});
+//test('Switch Options', function (assert) {
+//  assert.expect(15);
+//
+//  const questions = Ember.A([Ember.Object.create({
+//    question: Ember.Object.create({
+//      text:"This is a question 1",
+//      questionType: 'OE',
+//      order: 1
+//    }),
+//    correct: true,
+//    timeSpent: 10, //seconds
+//    reaction: 5,
+//    answer: "answer"
+//  }), Ember.Object.create({
+//    question: Ember.Object.create({
+//      text:"This is a question 2",
+//      order: 2,
+//      questionType: 'OE'
+//    }),
+//    correct: false,
+//    timeSpent: 25, //seconds
+//    reaction: 2,
+//    answer: "answer"
+//  })]);
+//
+//  this.set('questions', questions);
+//  this.render(hbs`{{reports/assessment/gru-questions results=questions}}`);
+//  const $component = this.$(); //component dom element
+//  const $question = $component.find(".gru-questions");
+//  const $switch = $question.find('.switch-section a');
+//  $switch.click();//Show correct answer
+//  T.exists(assert, $question.find('table thead th.header.score.hide'), 'Score header should be hide');
+//  T.exists(assert, $question.find('table thead th.header.time-spent.hide'), 'Time spent header should be hide');
+//  T.exists(assert, $question.find('table thead th.header.reaction.hide'), 'Reaction header should be hide');
+//  T.exists(assert, $question.find('table thead th.header.correct-answer.visible'), 'Correct answer header should be visible');
+//  T.exists(assert, $question.find('table tbody td.question-score.hide'), 'Score column should be hide');
+//  T.exists(assert, $question.find('table tbody td.question-time.hide'), 'Time spent column should be hide');
+//  T.exists(assert, $question.find('table tbody td.question-reaction.hide'), 'Reaction column should be hide');
+//  T.exists(assert, $question.find('table tbody td.correct-answer.visible'), 'Correct answer column should be visible');
+//  T.exists(assert, $question.find('table tbody td.correct-answer.visible:eq(0) .gru-open-ended'), 'Correct answer column should be visible');
+//
+//  $switch.click();//Show performance
+//  T.exists(assert, $question.find('table th.header.score.visible'), 'Score header should be visible');
+//  T.exists(assert, $question.find('table th.header.time-spent.visible'), 'Time spent header should be visible');
+//  T.exists(assert, $question.find('table thead th.header.reaction.visible'), 'Reaction header should be visible');
+//  T.exists(assert, $question.find('table tbody td.question-score.visible'), 'Score column should be visible');
+//  T.exists(assert, $question.find('table tbody td.question-time.visible'), 'Time spent column should be visible');
+//  T.exists(assert, $question.find('table tbody td.question-reaction.visible'), 'Reaction column should be visible');
+//});
 
