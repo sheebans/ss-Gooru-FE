73ea36a44b251324df664a81862d2bb6cc63cc9b
quiaro <quiaro@gmail.com>
2016-02-12 13:02:51 +0530

    GG-481: Copy class assessment report to its new location

--- app/controllers/class/analytics/collection/index.js
+++ app/controllers/class/analytics/collection/index.js
@@ -12,11 +12,16 @@ export default Ember.Controller.extend({
   // -------------------------------------------------------------------------
   // Dependencies
 
-  classController: Ember.inject.controller('class')
+  classController: Ember.inject.controller('class'),
 
   // -------------------------------------------------------------------------
   // Actions
 
+  actions: {
+    setAnonymous: function () {
+      this.set("anonymous", !this.get("anonymous"));
+    }
+  },
 
   // -------------------------------------------------------------------------
   // Events
@@ -24,6 +29,26 @@ export default Ember.Controller.extend({
 
   // -------------------------------------------------------------------------
   // Properties
+
+  /**
+   * @property {Collection} assessment
+   */
+  assessment: null,
+
+  /**
+   * @property {User[]} students
+   */
+  students: Ember.A([]),
+
+  /**
+   * @prop { UserQuestionsResult[] } userResults - Content feed to update the report data
+   */
+  userResults: null,
+
+  /**
+   * @property {boolean}
+   */
+  anonymous: false
 
 
   // -------------------------------------------------------------------------
