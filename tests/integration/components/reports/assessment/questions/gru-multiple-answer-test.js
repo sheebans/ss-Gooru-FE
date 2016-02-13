130b60a145c1d0dfdc1a8b3ae6a4cbe141d2ed29
Javier Perez <javier.perez@edify.cr>
2016-02-13 10:37:04 -0600

    Adding multiple answer anonymous and styles

--- tests/integration/components/reports/assessment/questions/gru-multiple-answer-test.js
+++ tests/integration/components/reports/assessment/questions/gru-multiple-answer-test.js
@@ -93,3 +93,32 @@ test('Multiple Answer Your Answer Correct', function(assert) {
   T.exists(assert, $multipleAnswer.find('li:eq(3) span.column.correct'), 'The third answer should be correct');
   T.notExists(assert, $multipleAnswer.find('li span.column.incorrect'), 'Should not be incorrect answers at all');
 });
+
+test('Multiple Answer anonymous', function(assert) {
+
+  var question = Ember.Object.create({
+    questionType: 'MA',
+    text: 'Sample Question MA',
+    hints: [],
+    explanation: 'Sample explanation text',
+    answers:  Ember.A([
+      Ember.Object.create({ id: "1", isCorrect: false, text:"Answer 1" }),
+      Ember.Object.create({ id: "2", isCorrect: true, text:"Answer 2" }),
+      Ember.Object.create({ id: "3", isCorrect: true, text:"Answer 3" })
+    ]),
+    order: 2
+  });
+
+  var userAnswer = Ember.A([{id: "1", selection: false}, {id: "2", selection: true}, {id: "3", selection: true}]);
+
+  this.set('question', question);
+  this.set('userAnswer', userAnswer);
+
+  this.render(hbs`{{reports/assessment/questions/gru-multiple-answer question=question userAnswer=userAnswer anonymous=true}}`);
+  const $component = this.$(); //component dom element
+  const $multipleAnswer = $component.find(".reports.assessment.questions.gru-multiple-answer");
+
+  T.exists(assert, $multipleAnswer.find('li:eq(1) span.column.anonymous'), 'The first answer should be anonymous');
+  T.exists(assert, $multipleAnswer.find('li:eq(2) span.column.anonymous'), 'The second answer should be anonymous');
+  T.exists(assert, $multipleAnswer.find('li:eq(3) span.column.anonymous'), 'The third answer should be anonymous');
+});
--- tests/integration/components/reports/assessment/questions/gru-true-false-test.js
+++ tests/integration/components/reports/assessment/questions/gru-true-false-test.js
@@ -61,3 +61,21 @@ test('True False Your Answer Correct', function (assert) {
   T.notExists(assert, $component.find('li:nth-child(1) span i'), 'The first answer should dont have selected icon');
   T.exists(assert, $component.find('li:nth-child(2) span.correct'), 'The second answer should have the incorrect icon');
 });
+
+test('True False anonymous', function (assert) {
+
+  var question = Ember.Object.create({
+    answers: Ember.A([
+      Ember.Object.create({id: "1", isCorrect: false, text: "True"}),
+      Ember.Object.create({id: "2", isCorrect: true, text: "False"})
+    ])
+  });
+  var userAnswer = "2";
+  this.set('question', question);
+  this.set('userAnswer', userAnswer);
+
+  this.render(hbs`{{reports/assessment/questions/gru-true-false question=question userAnswer=userAnswer}}`);
+  const $component = this.$(); //component dom element
+  T.notExists(assert, $component.find('li:nth-child(1) span i'), 'The first answer should dont have selected icon');
+  T.exists(assert, $component.find('li:nth-child(2) span.anonymous'), 'The second answer should be anonymous');
+});
