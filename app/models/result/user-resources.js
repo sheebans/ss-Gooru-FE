e6539e2801f0bb8855c36384e66f30f08047c76f
quiaro <quiaro@gmail.com>
2016-02-18 13:44:45 +0530

    GG-481: add properties to inform of new attempt and attempt completion

--- app/models/result/user-resources.js
+++ app/models/result/user-resources.js
@@ -12,26 +12,37 @@ export default Ember.Object.extend({
 
   // -------------------------------------------------------------------------
   // Properties
+
   /**
-   * @property {ResourceResult[]} questionResults
+   * @property {boolean} isAttemptStarted - is the user starting a new attempt?
    */
-  resourceResults: [],
+  isAttemptStarted: false,
+
+  /**
+   * @property {boolean} isAttemptFinished - is the user finishing the current attempt?
+   */
+  isAttemptFinished: false,
 
   /**
    * @property {QuestionResult[]} questionResults
    */
-  questionResults: Ember.computed("resourceResults.[]", function() {
+  questionResults: Ember.computed("resourceResults.[]", function () {
     return this.get("resourceResults").filter(function (resourceResult) {
       return resourceResult instanceof QuestionResult;
     });
   }),
 
   /**
+   * @property {ResourceResult[]} questionResults
+   */
+  resourceResults: [],
+
+  /**
    * @property {string} user
    */
   user: null,
 
-  
+
   // -------------------------------------------------------------------------
   // Methods
   /**
