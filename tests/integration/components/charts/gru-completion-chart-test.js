08db1c24ba9a420089f68451d8a35751e8a2cdd8
quiaro <quiaro@gmail.com>
2016-02-25 15:50:34 -0600

    GG-471: create gru-completion-chart

---
+++ tests/integration/components/charts/gru-completion-chart-test.js
@@ -0,0 +1,37 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+import { ANONYMOUS_COLOR } from 'gooru-web/config/config';
+
+moduleForComponent('charts/gru-completion-chart', 'Integration | Component | charts/gru completion chart', {
+  integration: true
+});
+
+test('it renders', function (assert) {
+  this.set('completed', 4);
+  this.set('total', 10);
+  this.set('color', '#DD0000');
+
+  this.render(hbs`{{charts/gru-completion-chart color=color completed=completed total=total}}`);
+
+  const $component = this.$('.charts.gru-completion-chart');
+  assert.ok($component.length, 'Component does not have the component classes');
+
+  const $bar = $component.find('.gru-x-bar-chart');
+  assert.equal($bar.find('.segment').length, 1, "Bar chart");
+  assert.equal($bar.find('.segment:nth-child(1)').attr("style"), "background-color: #DD0000; width: 40%;", "Segment");
+  assert.equal($component.find('span').text(), '4/10', 'Text label');
+});
+
+test('it renders correctly when the total value is zero', function (assert) {
+  this.set('completed', 0);
+  this.set('total', 0);
+
+  this.render(hbs`{{charts/gru-completion-chart completed=completed total=total}}`);
+
+  const $component = this.$('.charts.gru-completion-chart');
+  const $bar = $component.find('.gru-x-bar-chart');
+  assert.equal($bar.find('.segment').length, 1, "Bar chart");
+  assert.equal($bar.find('.segment:nth-child(1)').attr("style"), "background-color: " + ANONYMOUS_COLOR + "; width: 0%;", "Segment");
+  assert.equal($component.find('span').text(), '0/0', 'Text label');
+});
