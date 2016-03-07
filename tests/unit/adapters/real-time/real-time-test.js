5da5dd32a54002accd558c37793f0d5df6c64541
Jeffrey A. Bermudez P <jeffrey.bermudez@edify.cr>
2016-03-07 10:35:17 -0600

    GG-538_fixing_unit_tests: Fixing some pending Adapters tests to cover a complete scenario.

--- tests/unit/adapters/real-time/real-time-test.js
+++ tests/unit/adapters/real-time/real-time-test.js
@@ -4,8 +4,11 @@ import Pretender from 'pretender';
 moduleFor('adapter:real-time/real-time', 'Unit | Adapter | real-time/real-time', {
   // Specify the other units that are required for this test.
   // needs: ['serializer:foo']
-  beforeEach: function() {
+  beforeEach: function(assert) {
     this.pretender = new Pretender();
+    this.pretender.unhandledRequest = function(verb, path) {
+      assert.ok(false, `Wrong request [${verb}] url: ${path}`);
+    };
   },
   afterEach: function() {
     this.pretender.shutdown();
@@ -22,16 +25,12 @@ test('postData', function(assert) {
     },
     body: {}
   };
-  const routes = function() {
+
+  this.pretender.map(function() {
     this.post('/nucleus/realtime/class/the-class-id/collection/the-collection-id/user/the-user-id/event', function() {
       return [200, {'Content-Type': 'text/plain'}, ""];
     }, false);
-  };
-
-  this.pretender.map(routes);
-  this.pretender.unhandledRequest = function(verb, path) {
-    assert.ok(false, `Wrong request [${verb}] url: ${path}`);
-  };
+  });
 
   adapter.postData(data)
     .then(function(response) {
@@ -39,48 +38,118 @@ test('postData', function(assert) {
     });
 });
 
-test('urlForGetEvents', function (assert) {
+test('getData', function (assert) {
   const adapter = this.subject();
   const query = {
     classId: 'the-class-id',
     collectionId: 'the-collection-id'
   };
-  const url = adapter.urlForGetEvents(query);
 
-  assert.equal(url, '/nucleus/realtime/class/the-class-id/collection/the-collection-id/events', 'Wrong url');
+  this.pretender.map(function() {
+    this.get('/nucleus/realtime/class/the-class-id/collection/the-collection-id/events', function() {
+      return [200, {'Content-Type': 'application/json'}, JSON.stringify({})];
+    }, false);
+  });
+
+  adapter.getData(query)
+    .then(function(response) {
+      assert.deepEqual({}, response, 'Wrong response');
+    });
 });
 
-test('urlForPostAttempt', function (assert) {
+test('postAttempt', function (assert) {
   const adapter = this.subject();
   const query = {
     classId: 'the-class-id',
     collectionId: 'the-collection-id',
     userId: 'the-user-id'
   };
-  const url = adapter.urlForPostAttempt(query);
 
-  assert.equal(url, '/nucleus/realtime/class/the-class-id/collection/the-collection-id/user/the-user-id/complete', 'Wrong url');
+  this.pretender.map(function() {
+    this.post('/nucleus/realtime/class/the-class-id/collection/the-collection-id/user/the-user-id/complete', function() {
+      return [200, {'Content-Type': 'text/plain'}, ""];
+    }, false);
+  });
+
+  adapter.postAttempt(query)
+    .then(function(response) {
+      assert.equal("", response, 'Wrong response');
+    });
 });
 
-test('urlForDeleteAttempt', function (assert) {
+test('deleteAttempt', function (assert) {
   const adapter = this.subject();
   const query = {
     classId: 'the-class-id',
     collectionId: 'the-collection-id',
     userId: 'the-user-id'
   };
-  const url = adapter.urlForDeleteAttempt(query);
 
-  assert.equal(url, '/nucleus/realtime/class/the-class-id/collection/the-collection-id/user/the-user-id/reset', 'Wrong url');
+  this.pretender.map(function() {
+    this.delete('/nucleus/realtime/class/the-class-id/collection/the-collection-id/user/the-user-id/reset', function() {
+      return [200, {'Content-Type': 'text/plain'}, ""];
+    }, false);
+  });
+
+  adapter.deleteAttempt(query)
+    .then(function(response) {
+    assert.equal("", response, 'Wrong response');
+  });
 });
 
-test('urlForOnAir', function (assert) {
+test('postOnAir', function (assert) {
   const adapter = this.subject();
   const query = {
     classId: 'the-class-id',
     collectionId: 'the-collection-id'
   };
-  const url = adapter.urlForOnAir(query);
 
-  assert.equal(url, '/nucleus/realtime/class/the-class-id/collection/the-collection-id/onair', 'Wrong url');
+  this.pretender.map(function() {
+    this.post('/nucleus/realtime/class/the-class-id/collection/the-collection-id/onair', function() {
+      return [200, {'Content-Type': 'text/plain'}, ""];
+    }, false);
+  });
+
+  adapter.postOnAir(query)
+    .then(function(response) {
+      assert.equal("", response, 'Wrong response');
+    });
+});
+
+test('deleteOnAir', function (assert) {
+  const adapter = this.subject();
+  const query = {
+    classId: 'the-class-id',
+    collectionId: 'the-collection-id'
+  };
+
+  this.pretender.map(function() {
+    this.delete('/nucleus/realtime/class/the-class-id/collection/the-collection-id/onair', function() {
+      return [200, {'Content-Type': 'text/plain'}, ""];
+    }, false);
+  });
+
+  adapter.deleteOnAir(query)
+    .then(function(response) {
+      assert.equal("", response, 'Wrong response');
+    });
+});
+
+test('isOnAir', function (assert) {
+  const adapter = this.subject();
+  const query = {
+    classId: 'the-class-id',
+    collectionId: 'the-collection-id'
+  };
+
+  this.pretender.map(function() {
+    this.get('/nucleus/realtime/class/the-class-id/collection/the-collection-id/onair', function() {
+      return [200, {'Content-Type': 'text/plain'}, ""];
+    }, false);
+  });
+
+  adapter.isOnAir(query)
+    .then(function(response) {
+      assert.equal("", response, 'Wrong response');
+    });
 });
