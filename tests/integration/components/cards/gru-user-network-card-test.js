e48be9cb509857c2206e511c52f4c7e0e45428f1
Melany Delgado <melany.delgado@edify.cr>
2016-03-08 16:57:03 -0600

    Genarete User Network Card Component

---
+++ tests/integration/components/cards/gru-user-network-card-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('cards/gru-user-network-card', 'Integration | Component | cards/gru user network card', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });"
+
+  this.render(hbs`{{cards/gru-user-network-card}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:"
+  this.render(hbs`
+    {{#cards/gru-user-network-card}}
+      template block text
+    {{/cards/gru-user-network-card}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
