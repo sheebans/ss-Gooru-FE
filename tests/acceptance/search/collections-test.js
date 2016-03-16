7806c1172eed9b20b0a01f4cc39a46a69f847ccc
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2016-03-16 13:49:27 -0600

    GG-568: Search - Resource results (some tests and css changes)

--- tests/acceptance/search/collections-test.js
+++ tests/acceptance/search/collections-test.js
@@ -20,7 +20,7 @@ test('Layout', function(assert) {
   visit('/search/collections?term=any');
   andThen(function() {
     assert.equal(currentURL(), '/search/collections?term=any');
-    T.exists(assert, find(".search-filter"), "Missing search filters");
+    T.exists(assert, find(".content-navigation"), "Missing content-navigation filters");
     T.exists(assert, find(".collection-results .results"), "Missing collection results");
   });
 });
@@ -34,20 +34,6 @@ test('onOpenContentPlayer: When opening a collection', function(assert) {
     click($firstCollectionLink); //clicking first collection title
     andThen(function() {
       assert.equal(currentURL(), '/player/76cb53df-1f6a-41f2-a31d-c75876c6bcf9?resourceId=f86f874c-efc9-4100-9cf7-55eb86ec95ae');
-    });
-  });
-});
-
-test('filterType: When filtering by assessments', function(assert) {
-  assert.expect(3);
-  visit('/search/collections?term=any');
-  andThen(function() {
-    const $assessmentButton = find(".search-filter-options .assessments");
-    T.exists(assert, $assessmentButton, "Missing assessment filter button");
-    click($assessmentButton); //clicking first collection title
-    andThen(function() {
-      assert.equal(currentURL(), '/search/collections?term=any');
-      assert.equal(find(".results div.gru-collection-card").length, 9, "Search should return only 9 assessments");
     });
   });
 });
@@ -76,9 +62,9 @@ test('No results found', function(assert) {
   assert.expect(5);
   visit('/search/collections?term=noResultFound');
   andThen(function() {
-    const $assessmentButton = find(".search-filter-options .assessments");
-    T.exists(assert, $assessmentButton, "Missing assessment filter button");
-    click($assessmentButton); //clicking first collection title
+    const $collectionsButton = find(".content-navigation .category-options .collections");
+    T.exists(assert, $collectionsButton, "Missing assessment filter button");
+    click($collectionsButton); //clicking first collection title
     andThen(function() {
       assert.equal(currentURL(), '/search/collections?term=noResultFound');
       const $noResultFound = find(".results div.no-results-found");
