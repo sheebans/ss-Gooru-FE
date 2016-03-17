52fcb6a4324290ccf0b82e64020afbbf2e4578b6
Melany Delgado <melany.delgado@edify.cr>
2016-03-17 17:37:38 -0600

    Edit Information Content Area

--- app/controllers/content/courses/edit.js
+++ app/controllers/content/courses/edit.js
@@ -8,15 +8,47 @@ export default Ember.Controller.extend(BuilderMixin, {
   // -------------------------------------------------------------------------
   // Actions
   actions:{
+    /*
+    * Send request to publish a course
+    * */
     sendRequest:function(){
       this.set('wasRequestSent',true);
-    }
+    },
+    /*
+     * Edit Course
+     * */
+    editCourse:function(){
+      this.set('isEdit',true);
+    },
+    /*
+     * Cancel Edit
+     * */
+    cancelEditCourse:function(){
+      this.set('isEdit',false);
+    },
   },
   // -------------------------------------------------------------------------
   // Events
 
   // -------------------------------------------------------------------------
   // Properties
+  /**
+   * ONLY FOR TEST
+   * @property {Course}
+   */
+  course: Ember.Object.create({
+    'title': "Course Title",
+    'categories':Ember.A([Ember.Object.create({
+      'name': "K-12",
+      'isActive': true
+    }),Ember.Object.create({
+      'name': "Higher Education",
+      'isActive': false
+    }),Ember.Object.create({
+      'name': "Professional Development",
+      'isActive': false
+    })])
+  }),
   /**
    * Indicate if a request to be publish is approved
    * @property {Boolean}
