936080c380c4b7b7cb7e989e8cb4da0622e9094d
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2016-03-17 15:15:42 -0600

    GG-568: Search - Resource results (css changes)

--- app/routes/search/resources.js
+++ app/routes/search/resources.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  /**
+   * @property {Ember.Service} Service to do the search
+   */
+  searchService: Ember.inject.service('api-sdk/search'),
+
+  /**
+   * Set all controller properties used in the template
+   * @param controller
+   * @param model
+   */
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    var term = controller.get('searchController').term;
+    var searchParams = {
+      "term": term,
+      "collectionType": 'collections'
+    };
+
+    var resourceResults = this.get('searchService').searchCollections(searchParams);
+
+    controller.set('resourceResults', resourceResults);
+  }
+
+  // -------------------------------------------------------------------------
+  // Actions
+
 });
