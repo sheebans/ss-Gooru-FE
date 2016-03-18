f7e4107073f60ce051cf1d53aeadc2b620613e95
CarlosV89 <cevillalta07@gmail.com>
2016-03-18 15:50:24 -0600

    GG-632 added sub-menu, need integration to go on further.

--- app/routes/profile/content/course.js
+++ app/routes/profile/content/course.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  
+  setupController: function (controller /*, model */) {
+    controller.get('parentController').selectMenuItem('content');
+  }
+
 });
