d5ec310051f89ee3300e1cc519dc79f37bdf7f42
Melany Delgado <melany.delgado@edify.cr>
2016-03-23 11:53:51 -0600

    Content Builder - Edit Collection - Settings

--- app/controllers/content/collections/edit.js
+++ app/controllers/content/collections/edit.js
@@ -1,4 +1,47 @@
 import Ember from 'ember';
 import BuilderMixin from 'gooru-web/mixins/content/builder';
 
-export default Ember.Controller.extend(BuilderMixin, {});
+export default Ember.Controller.extend(BuilderMixin, {
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  // -------------------------------------------------------------------------
+  // Actions
+  actions:{
+    /**
+     * Send request to publish a collection
+     */
+    sendRequest: function () {
+      this.set('wasRequestSent',true);
+    }
+  },
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  /**
+   * Is a request pending approval?
+   * @property {Boolean}
+   */
+  isRequestApproved: false,
+
+  /**
+   * Has a request to make the course searchable been sent?
+   * @property {Boolean}
+   */
+  wasRequestSent: false,
+
+  /**
+   * Toggle Options
+   * @property {Ember.Array}
+   */
+  switchOptions:Ember.A([Ember.Object.create({
+    'label': "On",
+    'value': true
+  }),Ember.Object.create({
+    'label': "Off",
+    'value': false
+  })]),
+});
--- tests/acceptance/content/collections/edit-test.js
+++ tests/acceptance/content/collections/edit-test.js
@@ -40,3 +40,25 @@ test('Layout', function (assert) {
     assert.ok($container.find('> section#settings').length, "Settings section");
   });
 });
+test('Settings Layout', function (assert) {
+  visit('/content/collections/edit/123');
+
+  andThen(function () {
+    assert.equal(currentURL(), '/content/collections/edit/123');
+
+    var $container = find(".controller.content.collections.edit");
+    assert.ok($container.find('#settings .header h2'), "Missing Settings Title");
+    assert.ok($container.find('.panel h3'), "Missing Content Publishing Subtitle");
+    assert.ok($container.find('.panel-body .setting-content:nth-child(0) .icon i.visibility'), "Missing Visibility Icon");
+    assert.ok($container.find('.panel-body .setting-content:nth-child(0) .description.publish-to'), "Missing Publish to message");
+    assert.ok($container.find('.panel-body .toggle'), "Missing Toggle");
+    assert.ok($container.find('.panel-body .setting-content:nth-child(1) .icon i.public'), "Missing Public Icon");
+    assert.ok($container.find('.panel-body .setting-content:nth-child(1) .description.request-to'), "Missing Request to message");
+    const $sendRequest = $container.find('.panel-body .request');
+    assert.equal($sendRequest.text(), "Send Request", "The button should be say Send Request");
+    $sendRequest.click();
+    andThen(function () {
+      assert.equal($container.find('.panel-body .request').text(), "Pending", "The button should be say Send Request");
+    });
+  });
+});
