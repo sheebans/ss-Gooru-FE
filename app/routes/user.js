dddfd4ea48b8e1cc9e3cffaa281e5b3691e637c3
CarlosV89 <cevillalta07@gmail.com>
2016-03-30 11:21:42 -0600

    GG-799 User route and template

---
+++ app/routes/user.js
@@ -0,0 +1,46 @@
+import Ember from 'ember';
+import SessionMixin from '../mixins/session';
+
+/**
+ * Classes route
+ *
+ * @module
+ * @augments Ember.Route
+ */
+export default Ember.Route.extend(SessionMixin, {
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  /**
+   * @type {Ember.Service} Service to retrieve class information
+   */
+  classService: Ember.inject.service("api-sdk/class"),
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+  beforeModel: function() {
+    // TODO: authenticate session with ember-simple-auth, if not send to log in
+  },
+
+  /**
+   * Get model for the controller
+   */
+  model: function() {
+
+  },
+
+  /**
+   * Set all controller properties from the model
+   * @param controller
+   * @param model
+   */
+  setupController: function(controller, model) {
+    controller.set("myClasses", model.myClasses);
+  }
+
+});
