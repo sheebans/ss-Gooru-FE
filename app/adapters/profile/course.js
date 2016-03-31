6e3efca9fd5a9fb7ccdb08c3855af2ea02796f4e
Jeffrey A. Bermudez P <jeffrey.bermudez@edify.cr>
2016-03-31 16:38:03 -0600

    GG-632: Working on API 3.0 Integration...

--- app/adapters/profile/course.js
+++ app/adapters/profile/course.js
@@ -1,26 +1,41 @@
 import Ember from 'ember';
 
 /**
- * Adapter to support the Course CRUD operations in the API 3.0
+ * Adapter to get the list of courses created by a Profile
  *
- * @typedef {Object} ProfileAdapter
+ * @typedef {Object} ProfileCourseAdapter
  */
 export default Ember.Object.extend({
 
   session: Ember.inject.service('session'),
 
-  namespace: '/api/nucleus-auth/v1/',
+  namespace: '/api/nucleus/v1/profiles',
 
-  getCourseListForUser:function(){
+  getCourses: function(profileId, subject) {
     const adapter = this;
     const namespace = adapter.get('namespace');
-    const userId = this.get('session.userId');
-    const url = `${namespace}profiles/${userId}/courses`;
+    const url = `${namespace}/${profileId}/courses`;
     const options = {
       type: 'GET',
       contentType: 'application/json; charset=utf-8',
-      headers: adapter.defineHeaders()
+      headers: adapter.defineHeaders(),
+      data: {
+        limit: 20,
+        offset: 0
+      }
     };
+
+    if (subject) {
+      options.data.subject = subject;
+    }
+
     return Ember.$.ajax(url, options);
+  },
+
+  defineHeaders: function() {
+    return {
+      'Authorization': 'Token ' + this.get('session.token-api3')
+    };
   }
+
 });
