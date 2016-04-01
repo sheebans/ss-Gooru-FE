6f66dfd8c390617e1b02a90ca4417417e906e39a
Melany Delgado <melany.delgado@edify.cr>
2016-04-01 10:59:43 -0600

    New Assessment Component and Integration Test

--- tests/integration/components/content/modals/gru-assessment-new-test.js
+++ tests/integration/components/content/modals/gru-assessment-new-test.js
@@ -2,34 +2,34 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import wait from 'ember-test-helpers/wait';
 
-moduleForComponent('gru-collection-new', 'Integration | Component | gru collection new', {
+moduleForComponent('gru-assessment-new', 'Integration | Component | gru assessment new', {
   integration: true,
   beforeEach: function () {
     this.inject.service('i18n');
   }
 });
 
-test('New Collection Layout', function(assert) {
+test('New Assessment Layout', function(assert) {
 
-  this.render(hbs`{{content/modals/gru-collection-new}}`);
+  this.render(hbs`{{content/modals/gru-assessment-new}}`);
 
-  const $component = this.$(".gru-collection-new");
+  const $component = this.$(".gru-assessment-new");
   assert.ok($component, 'Missing Component');
   assert.ok($component.find('h4.modal-title'), 'Missing Title');
-  assert.equal($component.find('h4.modal-title').text(), this.get('i18n').t('common.add-collection').string, 'Incorrect Title');
-  assert.ok($component.find('label span.required'), 'Missing Collection Title label');
-  assert.equal($component.find('label span.required').text(), this.get('i18n').t('common.collection-title').string, 'Incorrect Collection Title Label');
-  assert.ok($component.find('label input'), 'Missing Collection Title Input');
+  assert.equal($component.find('h4.modal-title').text(), this.get('i18n').t('common.add-assessment').string, 'Incorrect Title');
+  assert.ok($component.find('label span.required'), 'Missing Assessment Title label');
+  assert.equal($component.find('label span.required').text(), this.get('i18n').t('common.assessment-title').string, 'Incorrect Assessment Title Label');
+  assert.ok($component.find('label input'), 'Missing Assessment Title Input');
   assert.ok($component.find('actions .cancel'), 'Missing Cancel Button');
   assert.ok($component.find('actions .add'), 'Missing Add Button');
 
 });
-test('Validate if the collection title field is left blank', function (assert) {
+test('Validate if the assessment title field is left blank', function (assert) {
   assert.expect(3);
 
-  this.render(hbs`{{content/modals/gru-collection-new}}`);
+  this.render(hbs`{{content/modals/gru-assessment-new}}`);
 
-  const $component = this.$('.gru-collection-new');
+  const $component = this.$('.gru-assessment-new');
   const $titleField = $component.find(".gru-input.title");
 
   assert.ok(!$titleField.find(".error-messages .error").length, 'Title error message not visible');
@@ -41,7 +41,7 @@ test('Validate if the collection title field is left blank', function (assert) {
 
     assert.ok($titleField.find(".error-messages .error").length, 'Title error should be visible');
     // Fill in the input field
-    $titleField.find("input").val('Collection Name');
+    $titleField.find("input").val('assessment Name');
     $titleField.find("input").blur();
 
     return wait().then(function () {
@@ -52,9 +52,9 @@ test('Validate if the collection title field is left blank', function (assert) {
 test('Validate the character limit in the course title field', function (assert) {
   assert.expect(1);
 
-  this.render(hbs`{{content/modals/gru-collection-new}}`);
+  this.render(hbs`{{content/modals/gru-assessment-new}}`);
 
-  const $component = this.$('.gru-collection-new');
+  const $component = this.$('.gru-assessment-new');
   const $titleField = $component.find(".gru-input.title");
 
   $titleField.find("input").val('123456790123456790123456790123456790123456790extra');
