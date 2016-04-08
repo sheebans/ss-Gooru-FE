8d8d35367e5d66fe65f9163edaad9be257bc4028
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2016-04-08 15:11:00 -0600

    GG-773: Sign Up - Step 1 User Information Validations(some tests)

--- tests/acceptance/sign-up-test.js
+++ tests/acceptance/sign-up-test.js
@@ -7,7 +7,7 @@ moduleForAcceptance('Acceptance | sign-up', {
   beforeEach: function() {
     authenticateSession(this.application, {
       isAnonymous: true,
-      token: 'sign-in-token',
+      token: 'sign-up-token',
       user: {
         gooruUId: 'session-id'
       }
@@ -87,6 +87,111 @@ test('it shows error messages if the all fields are left blank', function (asser
       assert.ok($emailField.find(".error-messages .error").length, 'Email error message visible');
       assert.ok($passwordField.find(".error-messages .error").length, 'Password error message visible');
       assert.ok($rePasswordField.find(".error-messages .error").length, 'Repassword error message visible');
+
+    });
+  });
+});
+
+test('it shows an error message if the first name field is left blank', function (assert) {
+  visit('/sign-up');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign-up');
+
+
+    const $signUpContainer = find(".sign-up");
+    const $firstNameField = $signUpContainer.find(".gru-input.firstName");
+
+    // Invalid
+    $firstNameField.find("input").val('f');
+    $firstNameField.find("input").blur();
+    return wait().then(function () {
+
+      assert.ok($firstNameField.find(".error-messages .error").length, 'firstName error message visible');
+      // Valid
+      $firstNameField.find("input").val('first name');
+      $firstNameField.find("input").blur();
+      return wait().then(function () {
+        assert.ok(!$firstNameField.find(".error-messages .error").length, 'firstName error message was hidden');
+      });
+    });
+  });
+});
+
+test('it shows an error message if the last name field has less than 2 characters', function (assert) {
+  visit('/sign-up');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign-up');
+
+
+    const $signUpContainer = find(".sign-up");
+    const $lastNameField = $signUpContainer.find(".gru-input.lastName");
+
+    // Invalid
+    $lastNameField.find("input").val('l');
+    $lastNameField.find("input").blur();
+    return wait().then(function () {
+
+      assert.ok($lastNameField.find(".error-messages .error").length, 'lastName error message visible');
+      // Valid
+      $lastNameField.find("input").val('last name');
+      $lastNameField.find("input").blur();
+      return wait().then(function () {
+        assert.ok(!$lastNameField.find(".error-messages .error").length, 'lastName error message was hidden');
+      });
+    });
+  });
+});
+
+test('it shows an error message if the email has incorrect format', function (assert) {
+  visit('/sign-up');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign-up');
+
+
+    const $signUpContainer = find(".sign-up");
+    const $emailField = $signUpContainer.find(".gru-input.email");
+
+    // Invalid format
+    $emailField.find("input").val('notAnEmail.com');
+    $emailField.find("input").blur();
+
+    return wait().then(function () {
+      assert.ok($emailField.find(".error-messages .error").length, 'email error message visible');
+      // Valid format
+      $emailField.find("input").val('jennifer@gooru.org');
+      $emailField.find("input").blur();
+      return wait().then(function () {
+        assert.ok(!$emailField.find(".error-messages .error").length, 'email error message was hidden');
+      });
+     });
+  });
+});
+
+test('it shows an error message if the password and rePassword fields do not match', function (assert) {
+  visit('/sign-up');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/sign-up');
+
+
+    const $signUpContainer = find(".sign-up");
+    const $passwordField = $signUpContainer.find(".gru-input.password");
+    const $rePasswordField = $signUpContainer.find(".gru-input.rePassword");
+
+    assert.ok(!$rePasswordField.find(".error-messages .error").length, 'rePassword error message not visible');
+
+    // Invalid format
+    $passwordField.find("input").val('password');
+    $rePasswordField.find("input").val('notTheSame');
+
+    // Try submitting without filling in data
+    $signUpContainer.find("button.submit-sign-up").click();
+
+    return wait().then(function () {
+      assert.ok($rePasswordField.find(".error-messages .error").length, 'passwords do not match');
 
     });
   });
