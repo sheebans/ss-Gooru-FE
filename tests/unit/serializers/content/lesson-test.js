13c6bb8c0d0e099e20ef47de1bcded07c7745132
quiaro <quiaro@gmail.com>
2016-04-13 22:06:59 -0600

    GG-709: Add serializer unit test for normalizeLesson

--- tests/unit/serializers/content/lesson-test.js
+++ tests/unit/serializers/content/lesson-test.js
@@ -1,5 +1,7 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 import Lesson from 'gooru-web/models/content/lesson';
+import LessonItem from 'gooru-web/models/content/lessonItem';
 import { CREATOR_SYSTEM } from 'gooru-web/config/config';
 
 moduleFor('serializer:content/lesson', 'Unit | Serializer | content/lesson');
@@ -22,4 +24,76 @@ test('serializeCreateLesson', function (assert) {
 
   const modelObject = serializer.serializeCreateLesson(modelInstance);
   assert.deepEqual(modelObject, expected, 'Serializer response');
+});
+
+test('normalizeLesson', function (assert) {
+  const serializer = this.subject();
+
+  const payload = {
+    "lesson_id": "lesson-id-789",
+    "unit_id": "unit-id-456",
+    "course_id": "course-id-123",
+    "title": "Lesson title",
+    "created_at": "2016-03-10T08:19:17Z",
+    "updated_at": "2016-03-10T08:19:17Z",
+    "creator_id": "owner-id",
+    "modifier_id": "owner-id",
+    "owner_id": "owner-id",
+    "original_creator_id": null,
+    "original_lesson_id": null,
+    "metadata": null,
+    "taxonomy": [
+      "K12.MA-MA2",
+      "K12.MA-MAK-CC"
+    ],
+    "sequence_id": 4,
+    "is_deleted": false,
+    "creator_system": "gooru",
+    "collection_summary": [
+      {
+        "id": "collection-id-123",
+        "title": "Collection title",
+        "format": "collection",
+        "sequence_id": 1,
+        "resource_count": 5,
+        "question_count": 5
+      },
+      {
+        "id": "assessment-id-456",
+        "title": "Assessment title",
+        "format": "assessment",
+        "sequence_id": 2,
+        "resource_count": null,
+        "question_count": 10
+      }
+    ]
+  };
+
+  var expected = Lesson.create(Ember.getOwner(this).ownerInjection(), {
+    children: Ember.A([
+      LessonItem.create({
+        id: payload.collection_summary[0].id,
+        format: payload.collection_summary[0].format,
+        questionCount: payload.collection_summary[0].question_count,
+        resourceCount: payload.collection_summary[0].resource_count,
+        sequence: payload.collection_summary[0].sequence_id,
+        title: payload.collection_summary[0].title
+      }),
+      LessonItem.create({
+        id: payload.collection_summary[1].id,
+        format: payload.collection_summary[1].format,
+        questionCount: payload.collection_summary[1].question_count,
+        resourceCount: 0,
+        sequence: payload.collection_summary[1].sequence_id,
+        title: payload.collection_summary[1].title
+      })
+    ]),
+    id: payload.lesson_id,
+    sequence: payload.sequence_id,
+    title: payload.title,
+    taxonomy: payload.taxonomy.slice(0)
+  });
+
+  const result = serializer.normalizeLesson(payload);
+  assert.deepEqual(result, expected, 'Serialized response');
 });
