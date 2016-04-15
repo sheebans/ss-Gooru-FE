b61ab0e0d766df2874f28578f8c14707471c0df2
quiaro <quiaro@gmail.com>
2016-04-15 17:52:32 -0600

    GG-699: Import ember

--- tests/integration/components/content/gru-audience-test.js
+++ tests/integration/components/content/gru-audience-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('content/gru-audience', 'Integration | Component | content/gru audience', {
   integration: true,
