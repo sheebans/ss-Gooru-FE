cc9325cfb09f922257db18b1eddfa682e5e88206
quiaro <quiaro@gmail.com>
2016-04-18 15:49:20 -0600

    GG-745: Create gru-assessment-edit component; remove controller for edit assessment

--- app/routes/content/assessments/edit.js
+++ app/routes/content/assessments/edit.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Assessment from 'gooru-web/models/content/assessment';
 
 export default Ember.Route.extend({
 
@@ -9,12 +10,25 @@ export default Ember.Route.extend({
    */
   session: Ember.inject.service("session"),
 
+  assessmentService: Ember.inject.service('api-sdk/assessment'),
 
   // -------------------------------------------------------------------------
   // Methods
 
   beforeModel: function () {
     // TODO: authenticate session with ember-simple-auth, if not send to log in
+  },
+  setupController(controller /*, model */) {
+
+    // TODO: Fetch data from model
+    var assessment = Assessment.create(Ember.getOwner(this).ownerInjection(), {
+      title: "Assessment Title",
+      category: 1,
+      audience: [2, 4],
+      learningObjectives: "Learning Objectives"
+    });
+
+    controller.set('collection', assessment);
   }
 
 });
