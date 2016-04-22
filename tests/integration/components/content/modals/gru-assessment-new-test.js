4c9f657216e738d2904957eb5315a46b1e0c9ef2
Martín Méndez <martn.mndez@edify.cr>
2016-04-22 15:48:14 -0600

    GG-996. GG-1001. Added integration tests.

--- tests/integration/components/content/modals/gru-assessment-new-test.js
+++ tests/integration/components/content/modals/gru-assessment-new-test.js
@@ -1,11 +1,88 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import wait from 'ember-test-helpers/wait';
+
+const classServiceMock = Ember.Service.extend({
+  associateCourseToClass: function(courseId, classId) {
+    if (courseId && classId) {
+      return Ember.RSVP.resolve('');
+    } else {
+      return Ember.RSVP.reject('Course association to class failed');
+    }
+  }
+});
+
+const courseServiceMock = Ember.Service.extend({
+  createCourse: function(courseModel) {
+    if (courseModel) {
+      return Ember.RSVP.resolve(Ember.Object.create({
+        id: 'course-id'
+      }));
+    } else {
+      return Ember.RSVP.reject('Course creation failed');
+    }
+  }
+});
+
+const unitServiceMock = Ember.Service.extend({
+  createUnit: function(courseId, unitModel) {
+    if(unitModel && courseId) {
+      return Ember.RSVP.resolve(Ember.Object.create({
+        id: 'unit-id'
+      }));
+    } else {
+      return Ember.RSVP.reject('Unit creation failed');
+    }
+  }
+});
+
+const lessonServiceMock = Ember.Service.extend({
+  createLesson: function(courseId, unitId, lessonModel) {
+    if(courseId && unitId && lessonModel) {
+      return Ember.RSVP.resolve(Ember.Object.create({
+        id: 'lesson-id'
+      }));
+    } else {
+      return Ember.RSVP.reject('Lesson creation failed');
+    }
+  },
+  associateAssessmentOrCollectionToLesson: function (courseId, unitId, lessonId, collectionId) {
+    if(courseId && unitId && lessonId && collectionId) {
+      return Ember.RSVP.resolve('');
+    }
+    return Ember.RSVP.reject('Collection/assessment association to lesson failed');
+  }
+});
+
+const assessmentServiceMock = Ember.Service.extend({
+  createAssessment: function(assessmentModel) {
+    if (assessmentModel) {
+      return Ember.RSVP.resolve(Ember.Object.create({
+        id: 'assessment-id'
+      }));
+    } else {
+      return Ember.RSVP.reject('Assessment creation failed');
+    }
+  }
+});
 
 moduleForComponent('gru-assessment-new', 'Integration | Component | gru assessment new', {
   integration: true,
   beforeEach: function () {
     this.inject.service('i18n');
+
+    this.register('service:api-sdk/class', classServiceMock);
+    this.register('service:api-sdk/course', courseServiceMock);
+    this.register('service:api-sdk/unit', unitServiceMock);
+    this.register('service:api-sdk/lesson', lessonServiceMock);
+    this.register('service:api-sdk/assessment', assessmentServiceMock);
+
+    this.inject.service('api-sdk/class');
+    this.inject.service('api-sdk/course');
+    this.inject.service('api-sdk/unit');
+    this.inject.service('api-sdk/lesson');
+    this.inject.service('api-sdk/assessment');
   }
 });
 
@@ -86,4 +163,100 @@ test('Validate the character limit in the assessment title field', function (ass
   $titleField.find("input").blur();
 
   assert.equal($titleField.find("input").val().length,50, "Incorrect number of incorrect characters");
+});
+
+test('Validate quickstart', function (assert) {
+  assert.expect(3);
+
+  var transition;
+
+  // Mock the transitionTo method in the router
+  this.set('router', {
+    transitionTo(route, assessmentId) {
+      transition = {
+        route: route,
+        assessment: assessmentId
+      };
+    }
+  });
+
+  this.set('class', {
+    isQuickstart: true,
+    class: Ember.Object.create({
+      id: 'class-id',
+      title: 'class-title'
+    })
+  });
+
+  this.on('closeModal', function () {
+    assert.ok(true, 'closeModal action triggered');
+  });
+
+  this.render(hbs`{{content/modals/gru-assessment-new model=class router=router}}`);
+
+  const $component = this.$('.gru-assessment-new');
+  const $titleField = $component.find(".gru-input.title");
+
+  // Fill in the input field
+  $titleField.find("input").val('assessment-title');
+  $titleField.find("input").blur();
+
+  return wait().then(function () {
+
+    // Try submitting without filling in data
+    $component.find(".actions button[type='submit']").click();
+
+    return wait().then(function () {
+      assert.equal(transition.route, 'content.assessments.edit', 'Transition to correct route');
+      assert.equal(transition.assessment, 'assessment-id', 'Correct assessment ID');
+    });
+  });
+});
+
+
+test('Validate associating lesson', function (assert) {
+  assert.expect(3);
+
+  var transition;
+
+  // Mock the transitionTo method in the router
+  this.set('router', {
+    transitionTo(route, assessmentId) {
+      transition = {
+        route: route,
+        assessment: assessmentId
+      };
+    }
+  });
+
+  this.set('class', {
+    associateLesson: true,
+    lessonId: 'lesson-id',
+    unitId: 'unit-id',
+    courseId: 'course-id'
+  });
+
+  this.on('closeModal', function () {
+    assert.ok(true, 'closeModal action triggered');
+  });
+
+  this.render(hbs`{{content/modals/gru-assessment-new model=class router=router}}`);
+
+  const $component = this.$('.gru-assessment-new');
+  const $titleField = $component.find(".gru-input.title");
+
+  // Fill in the input field
+  $titleField.find("input").val('assessment-title');
+  $titleField.find("input").blur();
+
+  return wait().then(function () {
+
+    // Try submitting without filling in data
+    $component.find(".actions button[type='submit']").click();
+
+    return wait().then(function () {
+      assert.equal(transition.route, 'content.assessments.edit', 'Transition to correct route');
+      assert.equal(transition.assessment, 'assessment-id', 'Correct assessment ID');
+    });
+  });
 });
