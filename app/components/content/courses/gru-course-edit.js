264c42890ba032e90ea7b6ea30f7a2fa29c58bda
Javier Perez <javier.perez@edify.cr>
2016-05-06 18:01:37 -0600

    GG-1096 Fixing peer review feedback

--- app/components/content/courses/gru-course-edit.js
+++ app/components/content/courses/gru-course-edit.js
@@ -61,8 +61,7 @@ export default Ember.Component.extend(ContentEditMixin, {
             component.get('courseService').updateCourse(editedCourse)
 
               .then(function () {
-                course.merge(editedCourse, ['title', 'isVisibleOnProfile', 'image']);
-                component.set('course', editedCourse);
+                course.merge(editedCourse, ['title', 'isVisibleOnProfile', 'image', 'description']);
                 component.set('isEditing', false);
               })
 
