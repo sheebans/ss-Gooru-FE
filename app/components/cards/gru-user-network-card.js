50f7770d25e086682968e2b221a7cd6a213acba1
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2016-05-11 15:53:05 -0600

    GG-685: Profile - Network - Unfollow User

--- app/components/cards/gru-user-network-card.js
+++ app/components/cards/gru-user-network-card.js
@@ -56,7 +56,7 @@ export default Ember.Component.extend({
       });
 
     this.$(".btn-unfollow")
-      .mouseout(function() {
+      .mouseleave(function() {
         component.set('showUnFollowButton', false);
       });
   },
