60876e913ba32876bade7a4527d6b5b35c6d6068
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2016-05-16 11:07:58 -0600

    GG-760: Sign In - Forgot Password Submit and Validations

--- app/controllers/forgot-password.js
+++ app/controllers/forgot-password.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
-//import User from 'gooru-web/models/forgot-password';
-//import Env from 'gooru-web/config/environment';
+import User from 'gooru-web/models/forgot-password';
 
 export default Ember.Controller.extend({
 
@@ -28,17 +27,55 @@ export default Ember.Controller.extend({
 
   actions: {
 
+    resetPassword: function() {
+      const controller = this;
+      const user = controller.get('user');
 
+      if(controller.get('didValidate') === false) {
+        var email = Ember.$('.gru-input-mixed-validation.email input').val();
+        user.set('email',email);
+      }
+
+      user.validate().then(function ({ model, validations }) {
+        if (validations.get('isValid')) {
+          //controller.get("sessionService")
+          //  .signInWithUser(user, true)
+          //  .then(function() {
+          //    controller.set('didValidate', true);
+          //    // Trigger action in parent
+          //    controller.send('signIn');
+          //  })
+        } else {
+          controller.set('submitFlag', true);
+        }
+      });
+    }
   },
 
   init(){
-
-  }
+    this._super(...arguments);
+    var user = User.create(Ember.getOwner(this).ownerInjection(), {email: null});
+    this.set('user', user);
+  },
 
 
   // -------------------------------------------------------------------------
   // Properties
 
+  /**
+   * @type {User} user
+   */
+  user: null,
 
+  /**
+   * Submit has been performed
+   * @property {Boolean}
+   */
+  submitFlag: true,
+
+  /**
+   * @param {Boolean } didValidate - value used to check if input has been validated or not
+   */
+  didValidate: false
 
 });
