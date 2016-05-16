7a58e9617e70f6032c8e3046ea1ce23b26b1f43b
quiaro <quiaro@gmail.com>
2016-05-16 12:33:07 -0600

    GG-1044: Test layout of information panel

--- tests/integration/components/content/courses/gru-course-play-test.js
+++ tests/integration/components/content/courses/gru-course-play-test.js
@@ -60,3 +60,78 @@ test('it has correct header buttons when user is not course owner', function (as
   assert.ok($actions.find('> button.remix').length, "Remix button");
   assert.ok($actions.find('> button.play').length, "Play button");
 });
+
+test('it renders the course information in the side panel', function (assert) {
+
+  var course = Course.create(Ember.getOwner(this).ownerInjection(), {
+    owner_details: [
+      {
+        id: 'owner-1',
+        firstName: 'Russell',
+        fullName: 'Russell Owner1',
+        avatarUrl: '/assets/gooru/profile.png'
+      },
+      {
+        id: 'owner-2',
+        firstName: 'Frank',
+        fullName: 'Frank Owner2',
+        avatarUrl: '/assets/gooru/profile.png'
+      }
+    ],
+    collaborator_details: [
+      {
+        id: 'collaborator-1',
+        firstName: 'Shawn',
+        fullName: 'Shawn Collaborator1',
+        avatarUrl: '/assets/gooru/profile.png'
+      }
+    ],
+    license: 'License text',
+    useCase: 'Use case text'
+  });
+
+  this.set('course', course);
+  this.render(hbs`{{content/courses/gru-course-play course=course}}`);
+
+  var $container = this.$("article.content.courses.gru-course-play");
+
+  const $sidePanel = $container.find('#information .panel.aside > .panel-body');
+  assert.ok($sidePanel.length, "Side panel");
+
+  const $rows = $sidePanel.find('> .row');
+  assert.equal($rows.length, 2, 'Number of rows');
+
+  var $row = $rows.eq(0);
+  assert.equal($row.find('> div').length, 3, 'Number of columns -first row');
+
+  var $column = $row.find('> div:eq(0)');
+  assert.ok($column.hasClass('created-by'), 'Class 1 -column 1');
+  assert.ok($column.hasClass('gru-user-icons'), 'Class 2 -column 1');
+  assert.ok($column.find('> strong').length, 'Title -column 1');
+  assert.ok($column.find('li.item.user').length, 2, 'Authors');
+
+  var $user = $column.find('li.item.user:eq(0)');
+  assert.equal($user.find('span').text(), 'Russell', 'First author name');
+
+  $column = $row.find('> div:eq(1)');
+  assert.ok($column.hasClass('remixed-by'), 'Class 1 -column 2');
+  assert.ok($column.hasClass('gru-user-icons'), 'Class 2 -column 2');
+  assert.ok($column.find('> strong').length, 'Title -column 2');
+  assert.ok($column.find('li.item.user').length, 1, 'Collaborators');
+
+  $user = $column.find('li.item.user:eq(0)');
+  assert.equal($user.find('span').text(), 'Shawn', 'First collaborator name');
+
+  $column = $row.find('> div:eq(2)');
+  assert.ok($column.hasClass('license'), 'Class -column 3');
+  assert.ok($column.find('> strong').length, 'Title -column 3');
+  assert.ok($column.find('p').text(), 'License text', 'License text');
+
+  $row = $rows.eq(1);
+  assert.equal($row.find('> div').length, 1, 'Number of columns -second row');
+
+  $column = $row.find('> div:eq(0)');
+  assert.ok($column.hasClass('use-case'), 'Class -column 1, row 2');
+  assert.ok($column.find('> strong').length, 'Title -column 1, row 2');
+  assert.ok($column.find('p').text(), 'Use case text', 'Use case text');
+});
