499420e184f854570613e4f6db2e077b0a8deff7
Carlos Villalta <cevillalta07@gmail.com>
2016-05-19 14:16:05 -0600

    progress

--- app/components/gru-featured-courses.js
+++ app/components/gru-featured-courses.js
@@ -23,7 +23,55 @@ export default Ember.Component.extend({
   // -------------------------------------------------------------------------
   // Properties
 
+  courses:null,
+  testCourses: Ember.A([
+  Ember.Object.create({
+    'subject':'K12.First'
+  }),
+  Ember.Object.create({
+    'subject':'K12.Second'
+  }),
+  Ember.Object.create({
+    'subject':'K12.First'
+  }),
+  Ember.Object.create({
+    'subject':'K12.Third'
+  }),
+  Ember.Object.create({
+    'subject':'K12.Fourth'
+  }),
+  Ember.Object.create({
+    'subject':'K12.Second'
+  })]),
 
+  subjects: Ember.computed('testCourses', function() {
+    return this.get('testCourses').map(function(course){
+      return course.subject;
+    }).filter(function(elem, pos, list) {
+      return list.indexOf(elem) === pos;
+    });
+  }),
+
+  orderedCourses: Ember.computed('formattedSubjects', 'testCourses', function(){
+    const component = this;
+    return component.get('formattedSubjects').map(function(subject){
+      console.log(subject.category+'.'+subject.subject);
+
+     return component.get('testCourses').filter(function(course){
+       console.log(course.subject);
+       return course.subject===subject.category+'.'+subject.subject;
+     });
+   });
+  }),
+
+  formattedSubjects: Ember.computed('subjects', function(){
+    return this.get('subjects').map(function(subject){
+      return Ember.Object.create({
+        'category': subject.slice(0,subject.indexOf('.')),
+        'subject':  subject.slice(subject.indexOf('.')+1, subject.length)
+      });
+    });
+  })
   // -------------------------------------------------------------------------
   // Methods
 
--- app/routes/featured.js
+++ app/routes/featured.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
+
+  // -------------------------------------------------------------------------
+  // Dependencies
+  /**
+   * @type {ProfileService} Profile service object
+   */
+
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+  model: function() {
+
+  },
+
+  setupController: function (controller, model) {
+
+  }
+
 });
