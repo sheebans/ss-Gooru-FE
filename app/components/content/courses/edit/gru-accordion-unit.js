b29552f7a47c89236a99533ced5f5cabcdffe1ee
Melany Delgado <melany.delgado@edify.cr>
2016-05-19 12:12:11 -0600

    Delete lesson and acceptance test

--- app/components/content/courses/edit/gru-accordion-unit.js
+++ app/components/content/courses/edit/gru-accordion-unit.js
@@ -121,6 +121,12 @@ export default PlayerAccordionUnit.extend(ModalMixin,{
         'content.modals.gru-delete-content',
         model, null, null, null, false);
     },
+    /**
+     * Remove Lesson from a list of lessons
+     */
+    removeLesson: function (builderItem) {
+      this.get('items').removeObject(builderItem);
+    }
 
   },
 
