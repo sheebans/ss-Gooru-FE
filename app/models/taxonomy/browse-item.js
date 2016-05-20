6d0f74c3dc7a5362fba3c5130f9801075d04130c
quiaro <quiaro@gmail.com>
2016-05-20 08:53:41 -0600

    GG-1154: Make BrowseItem extend from TaxonomyItem

--- app/models/taxonomy/browse-item.js
+++ app/models/taxonomy/browse-item.js
@@ -1,40 +1,15 @@
 import Ember from 'ember';
+import TaxonomyItem from 'gooru-web/models/taxonomy/taxonomy-item';
 
 /**
  * Browse Item
  *
  * @typedef {Object} BrowseItem
  */
-export default Ember.Object.extend({
+export default TaxonomyItem.extend({
 
   // -------------------------------------------------------------------------
   // Properties
-
-  /**
-   * @property {boolean} checked - If this item checked or not.
-   */
-  isChecked: false,
-
-  /**
-   * @property {browseItem[]} children - List of item's children
-   */
-  children: null,
-
-  /**
-   * @property {boolean} level - Level where this item is found in the
-   * browse selector (@see gru-browse-selector). Level values start with 1.
-   */
-  level: null,
-
-  /**
-   * @property {TaxonomyItem} item
-   */
-  item: null,
-
-  /**
-   * @property {string} parent - Parent item or null if this is node is at the root of the tree.
-   */
-  parent: null,
 
   /**
    * @property {boolean} selected - Does this item have children checked or not?
@@ -43,8 +18,6 @@ export default Ember.Object.extend({
 
   /**
    * @property {string} totalChildrenChecked - Number of children that are currently selected.
-   * This counter will work only for items that are *not* in the last level of the
-   * browse selector (@see gru-browse-selector)
    */
   totalChildrenChecked: 0,
 
@@ -52,8 +25,8 @@ export default Ember.Object.extend({
   // -------------------------------------------------------------------------
   // Observers
 
-  onCheckUpdate: Ember.observer('isChecked', function() {
-    if (this.get('isChecked')) {
+  onCheckUpdate: Ember.observer('isSelected', function() {
+    if (this.get('isSelected')) {
       this.increaseCheckedCounters();
     } else {
       this.decreaseCheckedCounters();
@@ -63,25 +36,6 @@ export default Ember.Object.extend({
 
   // -------------------------------------------------------------------------
   // Methods
-
-  /**
-   * @function Calculate an array with the path to this item
-   */
-  calculateItemPath: function() {
-    var path = new Array(this.get('level'));
-    this.calculatePath(path);
-    return path;
-  },
-
-  calculatePath: function(resultArray) {
-    var level = this.get('level');
-    var parent = this.get('parent');
-
-    resultArray[level - 1] = this.get('item.id');
-    if (parent) {
-      parent.calculatePath(resultArray);
-    }
-  },
 
   /**
    * @function Decrease the counter 'totalChildrenChecked' in all of this node's ancestors
--- app/models/taxonomy/taxonomy-item.js
+++ app/models/taxonomy/taxonomy-item.js
@@ -8,14 +8,59 @@ import Ember from 'ember';
 export default Ember.Object.extend({
 
   /**
+   * @property {browseItem[]} children - List of item's children
+   */
+  children: [],
+
+  /**
    * @property {string} id - Item ID
    */
   id: null,
 
   /**
+   * @property {boolean} isSelected - If this item has been selected or not.
+   */
+  isSelected: false,
+
+  /**
    * @property {string} label - Text label for this item
    */
-  label: null
+  label: null,
 
+  /**
+   * @property {Number} level - Depth of the item in the tree. Level values start at 1.
+   */
+  level: 1,
+
+  /**
+   * @property {string} parent - Parent item or null if this is node is at the root of the tree.
+   */
+  parent: null,
+
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+  /**
+   * @function Calculate an array with the path to this item
+   * @example
+   * ["grandparent-id", "parent-id", "item-id"]
+   * @return {String[]}
+   */
+  getPath: function() {
+    var path = new Array(this.get('level'));
+    this.constructPath(path);
+    return path;
+  },
+
+  constructPath: function(resultArray) {
+    var level = this.get('level');
+    var parent = this.get('parent');
+
+    resultArray[level - 1] = this.get('item.id');
+    if (parent) {
+      parent.constructPath(resultArray);
+    }
+  }
 
 });
