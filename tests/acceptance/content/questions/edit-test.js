c4f75a16ab58654b4b8a65b08d5f75605c5ff5c8
Melany Delgado <melany.delgado@edify.cr>
2016-05-25 17:23:17 -0600

    Content Builder - Edit Question - Top Header Delete

--- tests/acceptance/content/questions/edit-test.js
+++ tests/acceptance/content/questions/edit-test.js
@@ -34,3 +34,37 @@ test('Click share button and check clipboard functionality', function (assert) {
   });
 });
 */
+test('Delete Question', function (assert) {
+  visit('/content/assessments/edit/50484e74-ad95-44d5-981a-c18411260233');
+  andThen(function () {
+    assert.equal(currentURL(), '/content/assessments/edit/50484e74-ad95-44d5-981a-c18411260233');
+    var $deleteButton = find("header .actions .delete");
+    click($deleteButton);
+    andThen(function () {
+      var $deleteContentModal = find(".gru-modal .gru-delete-content");
+      var $check1 = $deleteContentModal.find("ul li:eq(0) input");
+      click($check1);
+      andThen(function () {
+        var $check2 = $deleteContentModal.find("ul li:eq(1) input");
+        click($check2);
+        andThen(function () {
+          var $check3 = $deleteContentModal.find("ul li:eq(2) input");
+          click($check3);
+          andThen(function () {
+            var $input = $deleteContentModal.find(".delete-input");
+            $input.val('delete');
+            $input.blur();
+            keyEvent($input, 'keyup', KEY_CODES.ENTER);
+            andThen(function () {
+              var $deleteButton = $deleteContentModal.find("button.delete");
+              click($deleteButton);
+              andThen(function () {
+                assert.equal(currentURL(), '/pochita/content/courses');
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
