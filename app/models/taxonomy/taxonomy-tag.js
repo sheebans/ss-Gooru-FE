90df342a887ef27c18717d371633f222bde47710
quiaro <quiaro@gmail.com>
2016-05-31 17:14:22 -0600

    GG-1159: Create taxonomy tag data model

--- app/models/taxonomy/taxonomy-tag.js
+++ app/models/taxonomy/taxonomy-tag.js
@@ -1,39 +1,13 @@
 import Ember from 'ember';
-import { TAXONOMY_LEVELS } from 'gooru-web/config/config';
 
 /**
  * Taxonomy Tag
  *
- * Model representation of a taxonomy item as a tag
+ * Model of a visual representation of a taxonomy tag
  *
  * @typedef {Object} TaxonomyTag
  */
 export default Ember.Object.extend({
-
-  /**
-   * @property {String} title
-   */
-  title: '',
-
-  /**
-   * @property {String} code
-   */
-  code: '',
-
-  /**
-   * @property {String} description
-   */
-  description: '',
-
-  /**
-   * @property {String} framework code
-   */
-  frameworkCode: '',
-
-  /**
-   * @property {String} parentTitle - Text label
-   */
-  parentTitle: '',
 
   /**
    * @property {boolean} isActive - Is the state of the tag active or not?
@@ -52,38 +26,8 @@ export default Ember.Object.extend({
   isRemovable: false,
 
   /**
-   * @property {String} label - Text label
+   * @property {TaxonomyTagData} data - Data for the taxonomy tag
    */
-  label: Ember.computed('taxonomyLevel', function() {
-    return (this.get('taxonomyLevel') === TAXONOMY_LEVELS.STANDARD) ?
-      this.get('code') : this.get('title');
-  }),
-
-  /**
-   * @property {String} caption - Additional detail for the label
-   */
-  caption: Ember.computed('taxonomyLevel', function() {
-    var taxonomyLevel = this.get('taxonomyLevel');
-    var value;
-
-    switch(taxonomyLevel) {
-      case TAXONOMY_LEVELS.STANDARD:
-      case TAXONOMY_LEVELS.COURSE:
-        value = this.get('frameworkCode') + this.get('parentTitle');
-        break;
-      case TAXONOMY_LEVELS.DOMAIN:
-        value = this.get('parentTitle');
-        break;
-      default:
-        // Micro standards
-        value = this.get('code');
-    }
-    return value;
-  }),
-
-  /**
-   * @property {String} taxonomyLevel
-   */
-  taxonomyLevel: TAXONOMY_LEVELS.STANDARD
+  data: null
 
 });
