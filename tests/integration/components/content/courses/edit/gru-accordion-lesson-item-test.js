80580001c7af5a47dc9659cc69486d18714dd9fb
Carlos Villalta <cevillalta07@gmail.com>
2016-06-06 11:06:19 -0600

    Fix build

--- tests/integration/components/content/courses/edit/gru-accordion-lesson-item-test.js
+++ tests/integration/components/content/courses/edit/gru-accordion-lesson-item-test.js
@@ -63,10 +63,10 @@ test('it renders a lesson item correctly -collection', function (assert) {
     '5 ' + this.get('i18n').t('common.question-pl', { count: 5}).string, 'Detail text');
 
   assert.equal($heading.find('.actions button').length, 4, 'Header action buttons');
-  assert.ok($heading.find('.actions button:eq(0)').hasClass('delete-item'), 'First button is for editing');
-  assert.ok($heading.find('.actions button:eq(1)').hasClass('move-item'), 'Second button is for copying');
-  assert.ok($heading.find('.actions button:eq(2)').hasClass('copy-item'), 'Third button is for moving');
-  assert.ok($heading.find('.actions button:eq(3)').hasClass('edit-item'), 'Fourth button is for deleting');
+  assert.ok($heading.find('.actions button:eq(0)').hasClass('delete-item'), 'First button is for deleting');
+  assert.ok($heading.find('.actions button:eq(1)').hasClass('move-item'), 'Second button is for moving');
+  assert.ok($heading.find('.actions button:eq(2)').hasClass('copy-item'), 'Third button is for copying');
+  assert.ok($heading.find('.actions button:eq(3)').hasClass('edit-item'), 'Fourth button is for editing');
 
   this.set('isSorting', true);
   assert.ok($heading.find('.drag-icon .drag_handle').length, 'Drag icon should be appear');
