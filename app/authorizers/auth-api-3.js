4ac02a032de617e2cfad1b16ca6e5cb2798c8f1f
Martin <gmmendezp@gmail.com>
2016-06-10 18:08:06 -0600

    GG-1256. Improved authorizer TODO comment.

--- app/authorizers/auth-api-3.js
+++ app/authorizers/auth-api-3.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import BaseAuthorizer from 'ember-simple-auth/authorizers/base';
 
 
-// TODO use this instead of defineHeaders in all the adapters
+// TODO use this instead of defineHeaders in all the adapters that need this Authorization header
 export default BaseAuthorizer.extend({
 
   authenticationService: Ember.inject.service('api-sdk/authentication'),
@@ -19,8 +19,8 @@ export default BaseAuthorizer.extend({
           let newToken = authorizer.get('session.token-api3');
           block(null, 'Authorization', `Token ${newToken}`);
         }
-      );
-    }).catch(block);
+      ).catch(block);
+    });
 
   }
 });
