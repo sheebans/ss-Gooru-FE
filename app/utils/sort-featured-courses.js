519e5d18329e52064139b6617573d07837b50429
quiaro <quiaro@gmail.com>
2016-06-16 11:14:34 -0600

    GG-000: Fix test in quick course search

--- app/utils/sort-featured-courses.js
+++ app/utils/sort-featured-courses.js
@@ -34,14 +34,47 @@ export function getSubjects(courses) {
 }
 
 /**
- * Ordered courses by sequence, and subject sequence for an array of Courses
  * @param {Course[]} featured courses
- * @prop {Course[]} ordered featured course subjects
+ * @return {Course[]} featured courses ordered by subjects
  */
 export function sortFeaturedCourses(courses) {
   let result = getSubjects(courses).map(
     subjectBucket => courses.filter(
       course => course.subjectName === subjectBucket.taxonomySubject
+    )
+  );
+  return result.map(
+    arrayOfCourses => arrayOfCourses.sort(
+      (a,b) => a.sequence-b.sequence));
+}
+
+/**
+ * @param {Course[]} list of courses
+ * @return {Course[]} list of courses ordered by subjects
+ */
+export function sortCoursesBySubject(courses) {
+  /**
+   * Ordered and unique subjects for an array of Courses
+   * @param {Course[]} featured courses
+   * @prop {Course[]} ordered featured course subjects
+   */
+  function getSubjectBuckets(courses) {
+    let result = courses.map(
+      course => Ember.Object.create({
+        subject: course.subject,
+        subjectSequence: course.subjectSequence
+      })
+    ).filter(
+      (elem, pos, list) => list.reduce(
+        (result, e, i) => result < 0 && e.subject === elem.subject ? i : result , -1
+      ) === pos
+    );
+    return result.sort((a,b) => a.subjectSequence-b.subjectSequence);
+  }
+
+  let result = getSubjectBuckets(courses).map(
+    subjectBucket => courses.filter(
+      course => course.subject===subjectBucket.subject
     )
   );
   return result.map(
