3bd1f13c26c091aebe341beb1ca0cf27778f49c2
Martin <gmmendezp@gmail.com>
2016-06-23 14:40:13 -0600

    GG-1421. Small fixes.

--- app/components/validation/gru-input.js
+++ app/components/validation/gru-input.js
@@ -59,7 +59,6 @@ export default Ember.Component.extend({
     var valuePath = this.get('valuePath');
     defineProperty(this, 'attributeValidation', computed.oneWay(`model.validations.attrs.${valuePath}`));
     var value = this.removeTags(this.get(`model.${valuePath}`));
-    console.log(value);
     this.set('rawInputValue', value);
     defineProperty(this, 'value', computed.alias(`model.${valuePath}`));
   },
