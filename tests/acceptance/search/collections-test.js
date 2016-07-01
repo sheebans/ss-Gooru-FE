df06bb17bf3180c733eb8cb195322a93ab1cfcc5
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2016-07-01 15:37:43 -0600

    GG-1420: Search - Add/Remove standard filters (some tests)

--- tests/acceptance/search/collections-test.js
+++ tests/acceptance/search/collections-test.js
@@ -81,7 +81,7 @@ test('Apply taxonomy filter', function(assert) {
   visit('/search/collections?taxonomies=["TEKS.K12.SC-K-SIR-01","TEKS.K12.SC-K-SIR-02"]&term=any');
 
   andThen(function() {
-    assert.equal(currentURL(), '/search/resources?taxonomies=%5B%22TEKS.K12.SC-K-SIR-01%22%2C%22TEKS.K12.SC-K-SIR-02%22%5D&term=any');
+    assert.equal(currentURL(), '/search/collections?taxonomies=%5B%22TEKS.K12.SC-K-SIR-01%22%2C%22TEKS.K12.SC-K-SIR-02%22%5D&term=any');
 
     assert.equal(find(".gru-taxonomy-tag-list .gru-taxonomy-tag").length, 2, "Number of tags rendered");
   });
@@ -91,7 +91,7 @@ test('Apply taxonomy filter - Removing taxonomy tag', function(assert) {
   visit('/search/collections?taxonomies=["TEKS.K12.SC-K-SIR-01","TEKS.K12.SC-K-SIR-02"]&term=any');
 
   andThen(function() {
-    assert.equal(currentURL(), '/search/resources?taxonomies=%5B%22TEKS.K12.SC-K-SIR-01%22%2C%22TEKS.K12.SC-K-SIR-02%22%5D&term=any');
+    assert.equal(currentURL(), '/search/collections?taxonomies=%5B%22TEKS.K12.SC-K-SIR-01%22%2C%22TEKS.K12.SC-K-SIR-02%22%5D&term=any');
 
     const $taxonomyTags = find(".gru-taxonomy-tag-list .gru-taxonomy-tag");
 
--- tests/acceptance/search/questions-test.js
+++ tests/acceptance/search/questions-test.js
@@ -67,7 +67,7 @@ test('Apply taxonomy filter', function(assert) {
   visit('/search/questions?taxonomies=["TEKS.K12.SC-K-SIR-01","TEKS.K12.SC-K-SIR-02"]&term=any');
 
   andThen(function() {
-    assert.equal(currentURL(), '/search/resources?taxonomies=%5B%22TEKS.K12.SC-K-SIR-01%22%2C%22TEKS.K12.SC-K-SIR-02%22%5D&term=any');
+    assert.equal(currentURL(), '/search/questions?taxonomies=%5B%22TEKS.K12.SC-K-SIR-01%22%2C%22TEKS.K12.SC-K-SIR-02%22%5D&term=any');
 
     assert.equal(find(".gru-taxonomy-tag-list .gru-taxonomy-tag").length, 2, "Number of tags rendered");
   });
@@ -77,7 +77,7 @@ test('Apply taxonomy filter - Removing taxonomy tag', function(assert) {
   visit('/search/questions?taxonomies=["TEKS.K12.SC-K-SIR-01","TEKS.K12.SC-K-SIR-02"]&term=any');
 
   andThen(function() {
-    assert.equal(currentURL(), '/search/resources?taxonomies=%5B%22TEKS.K12.SC-K-SIR-01%22%2C%22TEKS.K12.SC-K-SIR-02%22%5D&term=any');
+    assert.equal(currentURL(), '/search/questions?taxonomies=%5B%22TEKS.K12.SC-K-SIR-01%22%2C%22TEKS.K12.SC-K-SIR-02%22%5D&term=any');
 
     const $taxonomyTags = find(".gru-taxonomy-tag-list .gru-taxonomy-tag");
 
