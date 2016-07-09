4c46b1cdb4d0551cc64d1f23cf6885f801d50451
Martin <gmmendezp@gmail.com>
2016-07-08 19:00:37 -0600

    GG-1500. Fixed tests for presence html validator.

--- tests/unit/validators/presence-html-test.js
+++ tests/unit/validators/presence-html-test.js
@@ -1,16 +1,14 @@
 import { moduleFor, test } from 'ember-qunit';
 import Ember from 'ember';
-import BaseValidator from 'ember-cp-validations/validators/base';
 
 moduleFor('validator:presence-html', 'Unit | Validator | presence-html', {
   needs: ['validator:messages']
 });
 
-test('Text is empty', function(assert) {
+test('Text is invalid', function(assert) {
   assert.expect(1);
 
   let validator =  this.subject();
-  let done = assert.async();
   let message = 'error-message';
 
   validator.set('i18n', Ember.Object.create({
@@ -19,60 +17,15 @@ test('Text is empty', function(assert) {
     }
   }));
 
-  console.log(validator);
-  console.log(BaseValidator);
-  validator.validate('', { messageKey: 'error'}).then((msg) => {
-    assert.equal(msg, message, "Incorrect message");
-    done();
-  });
+  assert.equal(validator.validate('', { messageKey: 'error'}), assert.equal(msg, , "Incorrect message for empty text"));
+  assert.equal(validator.validate('<br><div></div>&nbsp;', { messageKey: 'error'}), assert.equal(msg, , "Incorrect message for text with tags"));
+  assert.equal(validator.validate('    ', { messageKey: 'error'}), assert.equal(msg, , "Incorrect message for whitespace only"));
 });
 
-test('Text with html tags', function(assert) {
+
+test('Text is valid', function(assert) {
   assert.expect(1);
 
   let validator =  this.subject();
-  let done = assert.async();
-  let message = 'error-message';
-
-  validator.set('i18n', Ember.Object.create({
-    t: function () {
-      return { string: message };
-    }
-  }));
-
-  validator.validate('<br><div></div>&nbsp;', { messageKey: 'error'}).then((msg) => {
-    assert.equal(msg, message, "Incorrect message");
-    done();
-  });
-});
-
-test('Text with only spaces', function(assert) {
-  assert.expect(1);
-
-  let validator =  this.subject();
-  let done = assert.async();
-  let message = 'error-message';
-
-  validator.set('i18n', Ember.Object.create({
-    t: function () {
-      return { string: message };
-    }
-  }));
-
-  validator.validate('    ', { messageKey: 'error'}).then((msg) => {
-    assert.equal(msg, message, "Incorrect message");
-    done();
-  });
-});
-
-test('Valid text', function(assert) {
-  assert.expect(1);
-
-  let validator =  this.subject();
-  let done = assert.async();
-
-  validator.validate('test-value', { messageKey: 'error'}).then((msg) => {
-    assert.ok(msg, "Incorrect message");
-    done();
-  });
+  validator.validate('test-value', { messageKey: 'error'}), "Incorrect value when text is valid");
 });
