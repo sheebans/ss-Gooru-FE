92657abce8c52ea8bfcc18581881af8d7cb8ace8
Javier Perez <javier.perez@edify.cr>
2016-07-22 14:08:18 -0600

    GG-1609 Adding support to legacy FIB questions

---
+++ tests/unit/models/content/question-test.js
@@ -0,0 +1,93 @@
+import Ember from "ember";
+import Question from 'gooru-web/models/content/question';
+import Answer from 'gooru-web/models/content/answer';
+import {QUESTION_TYPES} from 'gooru-web/config/question';
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('model:content/question', 'Unit | Model | content/question', {
+  unit: true
+});
+
+test('fibText having correct answer', function (assert) {
+  var model = this.subject({
+    text: "The ball is [red] and [white]"
+  });
+  assert.equal(model.get("fibText"), "The ball is _______ and _______", "Wrong FIB text");
+});
+
+test('fibText having no correct answers', function (assert) {
+  var model = this.subject({
+    text: "The ball is red and white"
+  });
+  assert.equal(model.get("fibText"), "The ball is red and white", "Wrong FIB text");
+});
+
+test('isLegacyFIB', function (assert) {
+
+  var model = this.subject({
+    questionType: QUESTION_TYPES.MA,
+    text: "The car is red"
+  });
+
+  assert.ok(!model.get("isLegacyFIB"), "Should not be Legacy FIB, incorrect type");
+
+  model.set("questionType", QUESTION_TYPES.fib);
+  assert.ok(!model.get("isLegacyFIB"), "Should not be Legacy FIB, correct type but has no ______");
+
+  model.set("text", "The car is [red]"); // new format
+  assert.ok(!model.get("isLegacyFIB"), "Should not be Legacy FIB, correct type but has no ______");
+
+  model.set("text", "The car is _______"); // legacy format
+  assert.ok(model.get("isLegacyFIB"), "Should be Legacy FIB, correct type and has  ______");
+
+});
+
+
+test('updateLegacyFIBText with no text', function (assert) {
+  var model = this.subject({
+    questionType: QUESTION_TYPES.fib
+  });
+
+  model.updateLegacyFIBText();
+
+  assert.equal(model.get("text"), null, "Should have no text still");
+});
+
+test('updateLegacyFIBText with text not following the legacy format', function (assert) {
+  var model = this.subject({
+    questionType: QUESTION_TYPES.fib,
+    text: "The car is [red] and [blue]"
+  });
+
+  model.updateLegacyFIBText();
+
+  assert.equal(model.get("text"), "The car is [red] and [blue]", "Wrong text");
+});
+
+
+test('updateLegacyFIBText with text following the legacy format, but no answer', function (assert) {
+  var model = this.subject({
+    questionType: QUESTION_TYPES.fib,
+    text: "The car is _______ and _______ and _______"
+  });
+
+  model.updateLegacyFIBText();
+
+  assert.equal(model.get("text"), "The car is _______ and _______ and _______", "Wrong text");
+});
+
+test('updateLegacyFIBText with text following the legacy format and answers', function (assert) {
+  var model = this.subject({
+    questionType: QUESTION_TYPES.fib,
+    text: "The car is _______ and _______ and _______",
+    answers: [
+      Answer.create({ text: "red", sequence: 1 }),
+      Answer.create({ text: "yellow", sequence: 2 }),
+      Answer.create({ text: "white", sequence: 3 })
+    ]
+  }); // we are assuming the answers are always sorted
+
+  model.updateLegacyFIBText();
+
+  assert.equal(model.get("text"), "The car is [red] and [yellow] and [white]", "Wrong text");
+});
