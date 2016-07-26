83ea93cc44e28727ed394f2d857156e60156bd7e
Carlos Villalta <cevillalta07@gmail.com>
2016-07-26 16:58:31 -0600

    GG-1628

---
+++ tests/unit/components/gru-tour-test.js
@@ -0,0 +1,27 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import Question from 'gooru-web/models/content/question';
+
+moduleForComponent('gru-tour', 'Unit | Component | gru-tour', {
+  integration: false
+});
+
+
+test('generateTemplate', function(assert) {
+  const component = this.subject();
+  const title = 'some-title';
+  const description = 'some-description';
+  const image = 'some-image-class';
+  const containerClass = 'test-class';
+  const result = component.constructModal(title, description, image, containerClass);
+
+  let template =
+  `<div class="tour-header-test-class">
+        <h2>some-title</h2>
+        <i onclick="introJs().exit()" class="material-icons">clear</i>
+      </div>
+      <div class="tour-description-test-class"><img class=some-image-class><p>some-description</p>
+      </div>`;
+
+  console.log(result.trim());
+  assert.equal(result.trim(), template.trim(), 'wrong template');
+});
