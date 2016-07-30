b77169e77768aea5526736d56dd329531da32b9e
Javier Perez <javier@gooru.org>
2016-07-30 07:36:53 -0600

    GG-1690 Tracking errors

---
+++ app/adapters/error/error.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+/**
+ * Adapter to support the errors API 3.0 integration
+ *
+ * @typedef {Object} ErrorAdapter
+ */
+export default Ember.Object.extend({
+
+  session: Ember.inject.service('session'),
+
+  namespace: '/api/nucleus-utils/v1/user-error',
+
+  createError: function(data) {
+    const adapter = this;
+    const namespace = adapter.get('namespace');
+    const path = `${namespace}`;
+    const options = {
+      type: 'POST',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'text',
+      processData: false,
+      headers: adapter.defineHeaders(),
+      data: JSON.stringify(data.body)
+    };
+    return Ember.$.ajax(path, options);
+  },
+
+  /**
+   *
+   * @returns {{Authorization: string}}
+   */
+  defineHeaders: function() {
+    return {
+      'Authorization': 'Token ' + this.get('session.token-api3')
+    };
+  }
+
+});
+
---
+++ app/services/api-sdk/error.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+import ErrorSerializer from 'gooru-web/serializers/error/error';
+import ErrorAdapter from 'gooru-web/adapters/error/error';
+
+
+export default Ember.Service.extend({
+
+  errorSerializer: null,
+
+  errorAdapter: null,
+
+
+  init: function () {
+    this._super(...arguments);
+    this.set('errorSerializer', ErrorSerializer.create(Ember.getOwner(this).ownerInjection()));
+    this.set('errorAdapter', ErrorAdapter.create(Ember.getOwner(this).ownerInjection()));
+  },
+
+
+  /**
+   *
+   * @param {Error} error
+   * @returns {Test.Promise|*|RSVP.Promise}
+   */
+  createError: function (error) {
+    var service = this;
+    Ember.Logger.error(error);
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      const errorContent = service.get('errorSerializer').serializeError(error);
+      service.get('errorAdapter').createError({body: errorContent})
+        .then(resolve, reject);
+    });
+  }
+
+})
