3df39d435a7f916a28be58da89581ad4eb82b81b
Javier Perez <javier@gooru.org>
2016-07-30 09:01:10 -0600

    GG-1690 Adding stubby endpoint

--- app/routes/application.js
+++ app/routes/application.js
@@ -158,6 +158,10 @@ export default Ember.Route.extend(PublicRouteMixin, {
      */
   trackEndPointError : function(event, jqXHR, settings){
     const route = this;
+
+    // do not track errors at the user-error api, this to prevent a loop
+    if (settings.url.indexOf('api/nucleus-utils/v1/user-error') >=0 ) return;
+
     const targetElement = event.currentTarget && event.currentTarget.activeElement ?
       event.currentTarget.activeElement : false;
     const model = Error.create({
