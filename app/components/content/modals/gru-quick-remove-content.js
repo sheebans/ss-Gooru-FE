c1d067784583290c99fc3533161611e3a5f83725
Carlos Villalta <cevillalta07@gmail.com>
2016-08-11 16:21:19 -0600

    GG-1698

---
+++ app/components/content/modals/gru-quick-remove-content.js
@@ -0,0 +1,85 @@
+import Ember from 'ember';
+import { CONTENT_TYPES } from 'gooru-web/config/config';
+/**
+ * Remove content component
+ *
+ * Component responsible for remove a content from content builder
+ *
+ * @module
+ * @augments ember/Component
+ */
+export default Ember.Component.extend({
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  /**
+   * @requires service:i18n
+   */
+  i18n: Ember.inject.service(),
+
+  /**
+   * @requires service:notifications
+   */
+  notifications: Ember.inject.service(),
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  classNames: ['content', 'modals', 'gru-quick-remove-content'],
+  // -------------------------------------------------------------------------
+  // Events
+
+  // -------------------------------------------------------------------------
+  // Actions
+
+  actions: {
+
+    /**
+     * Remove Content
+     */
+    removeContent: function (model) {
+      let component = this;
+
+      component.set('isLoading', true);
+
+      // This removeMethod will be a wrapper around the actual remove method that is particular to
+      // each content type.
+      model.removeMethod()
+        .then(function () {
+          if (model.callback) {
+            model.callback.success();
+          }
+          component.set('isLoading', false);
+          component.triggerAction({ action: 'closeModal' });
+          if (model.redirect) {
+            component.get('router').transitionTo(model.redirect.route, model.redirect.params.id);
+          }
+        })
+        .catch(function (error) {
+          var message = component.get('i18n').t('content.modals.remove-content.remove-error',
+            { type: component.get('i18n').t(`common.${model.type}`).string.toLowerCase() }).string;
+          component.get('notifications').error(message);
+          Ember.Logger.error(error);
+        });
+    }
+  },
+
+  // -------------------------------------------------------------------------
+  // Properties
+
+  /**
+   * This is the model used to delete.
+   * @property {model}
+   */
+  model: null,
+
+  /**
+   * Content types.
+   */
+  types: CONTENT_TYPES,
+
+  /**
+   * Indicate if it's waiting for removeMethod callback
+   */
+  isLoading: false
+});
