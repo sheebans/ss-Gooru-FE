a8c9a197565e88c829410f47a1f24471e93d8e8f
Javier Perez <javier@gooru.org>
2016-08-12 10:53:30 -0600

    GG-1606 Adding math expression to FIB

--- app/components/gru-math-text.js
+++ app/components/gru-math-text.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+
+  tagName: "span",
+
   // -------------------------------------------------------------------------
   // Events
 
