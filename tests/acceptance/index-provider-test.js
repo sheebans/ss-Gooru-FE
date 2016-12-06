7cdf7558212221af3b27f7357a2116d1b60acf1e
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2016-12-05 18:08:23 -0600

    NILE-159: Provider Landing Page login flow

---
+++ tests/acceptance/index-provider-test.js
@@ -0,0 +1,37 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'gooru-web/tests/helpers/module-for-acceptance';
+import T from 'gooru-web/tests/helpers/assert';
+import { authenticateSession } from 'gooru-web/tests/helpers/ember-simple-auth';
+
+moduleForAcceptance('Acceptance | index-provider', {
+  beforeEach: function() {
+    authenticateSession(this.application, {
+      isAnonymous: false,
+      'token-api3': 'user-token',
+      user: {
+        gooruUId: 'other-123'
+      }
+    });
+  }
+});
+
+test('logged in as a provider and home-link button navigation', function (assert) {
+
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/other-123/content/courses');
+    const $contentNav = find(".content-navigation");
+    click($contentNav.find(".collections a"));
+
+    andThen(function() {
+      assert.equal(currentURL(), '/other-123/content/collections');
+      const $navHeader = find(".gru-header .navbar-header");
+      click($navHeader.find(".home-link"));
+
+      andThen(function() {
+        assert.equal(currentURL(), '/other-123/content/courses');
+      });
+    });
+  });
+});
---
+++ tests/acceptance/index-teacher-test.js
@@ -0,0 +1,37 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'gooru-web/tests/helpers/module-for-acceptance';
+import T from 'gooru-web/tests/helpers/assert';
+import { authenticateSession } from 'gooru-web/tests/helpers/ember-simple-auth';
+
+moduleForAcceptance('Acceptance | index-teacher', {
+  beforeEach: function() {
+    authenticateSession(this.application, {
+      isAnonymous: false,
+      'token-api3': 'user-token',
+      user: {
+        gooruUId: 'id-for-pochita'
+      }
+    });
+  }
+});
+
+test('logged in as a teacher and home-link button navigation', function (assert) {
+
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/teacher');
+    const $navMenu = find(".gru-header .menu-navbar");
+    click($navMenu.find(".profile-link a.profile"));
+
+    andThen(function () {
+      assert.equal(currentURL(), '/id-for-pochita/content/courses');
+      const $navHeader = find(".gru-header .navbar-header");
+      click($navHeader.find(".home-link"));
+
+      andThen(function () {
+        assert.equal(currentURL(), '/teacher');
+      });
+    });
+  });
+});
