f0e947581001a9cb2dd10e92b4874130a9084931
Eduardo Oviedo <eduardo.oviedo@gmail.com>
2017-01-03 13:03:21 -0600

    Nile-183, removed condition in properties

--- app/controllers/teacher.js
+++ app/controllers/teacher.js
@@ -40,13 +40,9 @@ export default Ember.Controller.extend({
   /**
    * @property {Number} Total of teaching classes
    */
-  totalTeachingClasses: Ember.computed('myClasses', function() {
-    return this.getActiveClasses(this.get('myClasses.ownerList')) + this.getActiveClasses(this.get('myClasses.collaboratorList'));
-  }),
+  totalTeachingClasses: Ember.computed.alias("activeClasses.length"),
 
-  hasClasses:Ember.computed('totalTeachingClasses',function(){
-    return this.get('totalTeachingClasses') > 0;
-  }),
+  hasClasses: Ember.computed.bool("totalTeachingClasses"),
 
   /**
    * Toolkit site url
@@ -54,24 +50,9 @@ export default Ember.Controller.extend({
    */
   toolkitSiteUrl: Ember.computed(function(){
     return Env.toolkitSiteUrl;
-  }),
+  })
 
 // -------------------------------------------------------------------------
 // Methods
 
-  /**
-   * Return the number of active classes on specific list
-   */
-  getActiveClasses:function(list){
-    var component = this;
-    var totalActiveClasses = Ember.A();
-    list.forEach(function(item){
-      let activeItem = component.get('activeClasses').findBy('id',item);
-      if(activeItem){
-        totalActiveClasses.addObject(activeItem);
-      }
-    });
-
-    return totalActiveClasses.length;
-  }
 });
