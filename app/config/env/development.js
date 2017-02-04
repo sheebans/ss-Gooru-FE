48b1b7c6e3241fcd0f8ff9c7ca7de04254a8d2be
javier@gooru.org <javier@gooru.org>
2017-02-03 18:26:39 -0600

    Nile-394 reverting local changes

--- app/config/env/development.js
+++ app/config/env/development.js
@@ -4,8 +4,8 @@
 export default {
   "appRootPath": "/", //default is root
   "endpoint" : {
-    "url": "",
-    "secureUrl": ""
+    "url": "http://nile-dev.gooru.org",
+    "secureUrl": "https://nile-dev.gooru.org"
   },
 
   "realTime": {
