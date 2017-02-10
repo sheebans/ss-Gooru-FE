565ad3c445f63baf2a45125237e303e6c517cfc7
Julio Arias <jarias@users.noreply.github.com>
2017-02-10 10:10:56 -0600

    Build embedded version of the app

--- .ci/build.sh
+++ .ci/build.sh
@@ -63,3 +63,14 @@ silent tar xvzf welcome.tar.gz -C gooru-web/
 info "Creating artifact with version ${VERSION}..."
 tar czf gooru-web-${VERSION}.tar.gz gooru-web/ appspec.yml .deploy/
 
+info "Building embedded version..."
+
+rm -rf gooru-web
+GOORU_EMBEDDED=true grunt build:prod-bamboo
+
+echo $VERSION > gooru-web/version.html
+
+info "Creating embedded artifact with version ${VERSION}..."
+
+tar czf gooru-embedded-web-${VERSION}.tar.gz gooru-web/
+
