ac90647ec1bb749e6d48f8ed1261ac240574acd0
javier@gooru.org <javier@gooru.org>
2017-02-17 11:30:52 -0600

    Nile-506 suggest api for resource and collection

--- app/adapters/suggest/suggest.js
+++ app/adapters/suggest/suggest.js
@@ -9,6 +9,8 @@ export default ApplicationAdapter.extend({
    */
   namespace: '/gooru-search/rest/suggest/v2',
 
+  namespaceV3: '/gooru-search/rest/v3/suggest',
+
   suggestResources: function(term, params = {}) {
     const adapter = this;
     const namespace = this.get('namespace');
@@ -34,10 +36,80 @@ export default ApplicationAdapter.extend({
     return Ember.$.ajax(url, options);
   },
 
+  /**
+   * Gets resource suggestion for an specific resource in a context
+   * @param {String} resourceId
+   * @param {SuggestContext} context
+   * @param {number} limit
+   * @returns {*}
+     */
+  suggestResourcesForResource: function(resourceId, context, limit = 10) {
+    const adapter = this;
+    const namespace = this.get('namespaceV3');
+    const url = `${namespace}/resource?limit=${limit}`;
+
+    const options = {
+      type: 'GET',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      headers: adapter.defineAuthorizationHeaders(),
+      data: {
+        "contextType": "resource-study-suggest",
+        "resourceId": resourceId,
+        "userId": context.get("userId"),
+        "containerId": context.get('containerId') ? context.get('containerId') : undefined,
+        "courseId": context.get("courseId") ? context.get("courseId") : undefined,
+        "unitId": context.get("unitId") ? context.get("unitId") : undefined,
+        "lessonId": context.get("lessonId") ? context.get("lessonId") : undefined
+      }
+    };
+    return Ember.$.ajax(url, options);
+  },
+
+  /**
+   * Gets resource suggestion for an specific collection/assessment in a context
+   * @param {SuggestContext} context
+   * @param {number} limit
+   * @returns {*}
+     */
+  suggestResourcesForCollection: function(context, limit = 10) {
+    const adapter = this;
+    const namespace = this.get('namespaceV3');
+    const url = `${namespace}/resource?limit=${limit}`;
+
+    const options = {
+      type: 'POST',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      headers: adapter.defineAuthorizationHeaders(),
+      data: JSON.stringify({
+        "context":{
+          "contextType": "collection-study",
+          "containerId": context.get('containerId'),
+          "userId": context.get("userId"),
+          "courseId": context.get("courseId") ? context.get("courseId") : undefined,
+          "unitId": context.get("unitId") ? context.get("unitId") : undefined,
+          "lessonId": context.get("lessonId") ? context.get("lessonId") : undefined
+        },
+        "metrics":{
+          "score": context.get("score") >= 0 ? context.get("score") : undefined,
+          "timespent": context.get("timeSpent") >= 0 ? context.get("timeSpent") : undefined
+        }
+      })
+    };
+    return Ember.$.ajax(url, options);
+  },
+
   defineHeaders: function() {
     return {
       //'Authorization': 'Token ' + this.get('session.token-api3')
       'Gooru-Session-Token': this.get('session.token-api3')
+    };
+  },
+
+  defineAuthorizationHeaders: function() {
+    return {
+      'Authorization': 'Token ' + this.get('session.token-api3')
     };
   }
 });
---
+++ app/serializers/suggest/suggest.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+import SearchSerializer from 'gooru-web/serializers/search/search';
+
+/**
+ * Serializer to support Suggest functionality
+ *
+ * @typedef {Object} SuggestSerializer
+ */
+export default SearchSerializer.extend({
+
+
+  /**
+   * Normalize the suggest resources response
+   *
+   * @param payload is the endpoint response in JSON format
+   * @returns {Resource[]}
+   */
+  normalizeSuggestResources: function(payload) {
+    const serializer = this;
+    if (Ember.isArray(payload.suggestResults)) {
+      return payload.suggestResults.map(function(result) {
+        return serializer.normalizeResource(result);
+      });
+    }
+  },
+
+});
