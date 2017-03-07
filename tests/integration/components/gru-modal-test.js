c621c9eb3c068273d43681c15afb71c0a5c750b7
gmmendezp <gmmendezp@gmail.com>
2017-03-07 14:47:12 -0600

    NILE-519. Update player acceptance tests.

--- tests/integration/components/gru-modal-test.js
+++ tests/integration/components/gru-modal-test.js
@@ -1,20 +1,20 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('gru-modal', 'Integration | Component | gru modal', {
+moduleForComponent('qz-modal', 'Integration | Component | qz modal', {
   integration: true,
   beforeEach: function () {
-    this.container.lookup('service:i18n').set("locale","en");
+    this.container.lookup('service:i18n').set('locale','en');
   }
 });
 
 test('it renders', function(assert) {
 
-  this.render(hbs`{{gru-modal}}`);
+  this.render(hbs`{{qz-modal}}`);
 
   var $component = this.$(); //component dom element
-  var $rootElement = $component.find(".gru-modal");
-  assert.ok($rootElement, "Root element not found");
-  assert.ok($rootElement.hasClass('modal'), "Component should have class to identify it as a modal");
+  var $rootElement = $component.find('.qz-modal');
+  assert.ok($rootElement, 'Root element not found');
+  assert.ok($rootElement.hasClass('modal'), 'Component should have class to identify it as a modal');
 
 });
