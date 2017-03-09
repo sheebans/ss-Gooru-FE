ca82858bd29fd83c607bd4545df455c9e4c95244
gmmendezp <gmmendezp@gmail.com>
2017-03-09 11:08:39 -0600

    NILE-519. Fix modal test.

--- tests/integration/components/gru-modal-test.js
+++ tests/integration/components/gru-modal-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('qz-modal', 'Integration | Component | qz modal', {
+moduleForComponent('gru-modal', 'Integration | Component | gru modal', {
   integration: true,
   beforeEach: function () {
     this.container.lookup('service:i18n').set('locale','en');
@@ -10,10 +10,10 @@ moduleForComponent('qz-modal', 'Integration | Component | qz modal', {
 
 test('it renders', function(assert) {
 
-  this.render(hbs`{{qz-modal}}`);
+  this.render(hbs`{{gru-modal}}`);
 
   var $component = this.$(); //component dom element
-  var $rootElement = $component.find('.qz-modal');
+  var $rootElement = $component.find('.gru-modal');
   assert.ok($rootElement, 'Root element not found');
   assert.ok($rootElement.hasClass('modal'), 'Component should have class to identify it as a modal');
 
