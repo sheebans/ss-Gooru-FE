697ad535820d571964a8432954b5a72e0740a738
Martin Mendez <gmmendezp@gmail.com>
2017-03-08 18:17:09 -0600

    Create context when initializing player.

--- app/routes/context-player.js
+++ app/routes/context-player.js
@@ -1,5 +1,6 @@
 import PlayerRoute from 'gooru-web/routes/player';
-import PrivateRouteMixin from "gooru-web/mixins/private-route-mixin";
+import PrivateRouteMixin from 'gooru-web/mixins/private-route-mixin';
+import QuizzesContext from 'quizzes-addon/models/context/context';
 
 /**
  * Context Player Route
@@ -11,5 +12,27 @@ import PrivateRouteMixin from "gooru-web/mixins/private-route-mixin";
  * @extends PlayerRoute
  */
 export default PlayerRoute.extend(PrivateRouteMixin, {
-  templateName: 'player'
+  templateName: 'player',
+
+  // -------------------------------------------------------------------------
+  // Methods
+
+  /**
+   * @param {All route params} params
+   * @param {Collection} collection
+   */
+  createContext(params, collection) {
+    return this.get('quizzesContextService').createContext(QuizzesContext.create({
+      classId: params.classId,
+      collectionId: collection.get('id'),
+      title: collection.get('title'),
+      isCollection: collection.get('isCollection'),
+      contextMapping: {
+        courseId: params.courseId,
+        unitId: params.unitId,
+        lessonId: params.lessonId
+      }
+    }));
+  }
+
 });
