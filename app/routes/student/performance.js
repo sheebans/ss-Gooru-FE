94cc0591ed253e186a2b8e5147a2718c30395e4e
Jennifer Ajoy <jennifer.ajoy@edify.cr>
2017-03-15 20:41:20 -0600

    NILE-635: Left navigation for My Performance

--- app/routes/student/performance.js
+++ app/routes/student/performance.js
@@ -35,32 +35,33 @@ export default Ember.Route.extend({
   // -------------------------------------------------------------------------
   // Methods
 
-  model: function(params) {
-
-    const route = this;
-
-    const userId = route.get('session.userId');
-    const criteria = {
-      courseId: params.courseId,
-      unitId: params.unitId,
-      lessonId: params.lessonId,
-      collectionType: params.collectionType
-    };
-
-    //Remove this after the integration works
-    Ember.log.info(userId);
-    Ember.log.info(criteria);
-
+  model: function() {
     return Ember.RSVP.hash({
-
-      //Meanwhile the integration works
-      //assessments: route.get('assessmentService').findAssessments(userId, criteria),
-      //collectionPerformanceSummaryItems: route.get('performanceService').searchStudentCollectionPerformanceSummary(userId, criteria)
       assessments: [],
       collectionPerformanceSummaryItems: []
-
     });
   },
+
+  //Meanwhile the integration works
+
+  //model: function(params) {
+  //
+  //  const route = this;
+  //
+  //  const userId = route.get('session.userId');
+  //  const criteria = {
+  //    courseId: params.courseId,
+  //    unitId: params.unitId,
+  //    lessonId: params.lessonId,
+  //    collectionType: params.collectionType
+  //  };
+  //
+  //
+  //  return Ember.RSVP.hash({
+  //    assessments: route.get('assessmentService').findAssessments(userId, criteria),
+  //    collectionPerformanceSummaryItems: route.get('performanceService').searchStudentCollectionPerformanceSummary(userId, criteria)
+  //  });
+  //},
 
   /**
    * Set all controller properties from the model
