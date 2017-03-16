aa4b6a5acb5c3aa5e0d355a97f855284f8e2cd1c
Melany Delgado <melany.delgado@edify.cr>
2017-03-16 10:08:27 -0600

    Fix scoring levels

--- tests/integration/components/content/rubric/gru-scoring-levels-test.js
+++ tests/integration/components/content/rubric/gru-scoring-levels-test.js
@@ -8,7 +8,18 @@ moduleForComponent('content/rubric/gru-scoring-levels', 'Integration | Component
 });
 
 test('Layout', function(assert) {
-  this.render(hbs`{{content/rubric/gru-scoring-levels}}`);
+  let levels =  Ember.A([
+    {
+      name:'',
+      score:null
+    },
+    {
+      name:'',
+      score:null
+    }]);
+  this.set('scoringLevels',levels);
+
+  this.render(hbs`{{content/rubric/gru-scoring-levels scoringLevels=scoringLevels}}`);
   var $component = this.$();
   assert.ok($component.find('.content.rubric.gru-scoring-levels').length,'Missing scoring levels component');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .level span').length,'Missing levels title');
@@ -20,28 +31,36 @@ test('Layout', function(assert) {
   assert.ok($component.find('.content.rubric.gru-scoring-levels .level .levels .scale .worst').length,'Missing worst label');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .level .levels .scale .worst').length,'Missing worst label');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .level .levels .level-list').length,'Missing level list');
-  assert.equal($component.find('.content.rubric.gru-scoring-levels .level .levels .level-list .gru-input').length,4,'Should have 4 levels');
+  assert.equal($component.find('.content.rubric.gru-scoring-levels .level .levels .level-list .gru-input').length,2,'Should have 2 levels');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .level .btn-new-level').length,'Missing add new level button');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .points').length,'Missing points section');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .points span').length,'Missing points title');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .points .gru-switch').length,'Missing points switch');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .points .point-list').length,'Missing points list');
-  assert.equal($component.find('.content.rubric.gru-scoring-levels .points .point-list .gru-input').length,4,'Should have 4 point inputs');
+  assert.equal($component.find('.content.rubric.gru-scoring-levels .points .point-list .gru-input').length,2,'Should have 2 point inputs');
 });
 
 test('Delete Scoring Level', function(assert) {
-  this.render(hbs`{{content/rubric/gru-scoring-levels}}`);
+  let levels =  Ember.A([
+    {
+      name:'',
+      score:null
+    },
+    {
+      name:'',
+      score:null
+    }]);
+  this.set('scoringLevels',levels);
+  this.render(hbs`{{content/rubric/gru-scoring-levels scoringLevels=scoringLevels}}`);
   var $component = this.$();
   assert.ok($component.find('.content.rubric.gru-scoring-levels').length,'Missing scoring levels component');
-  assert.equal($component.find('.content.rubric.gru-scoring-levels .delete-levels .btn.delete').length,4,'Should have 4 delete buttons');
+  assert.equal($component.find('.content.rubric.gru-scoring-levels .points .point-list .gru-input').length,2,'Should have 2 delete buttons');
   var $firstLevelDeleteBtn = $component.find('.content.rubric.gru-scoring-levels .delete-levels .btn.delete:eq(0)');
-  var done = assert.async();
-  Ember.run.later(function(){
+  Ember.run(function () {
     $firstLevelDeleteBtn.click();
-    done();
-  }, 300);
+  });
   return wait().then(function () {
-    assert.equal($component.find('.content.rubric.gru-scoring-levels .points .point-list .gru-input').length,3,'Should have 3 levels');
+    assert.equal($component.find('.content.rubric.gru-scoring-levels .points .point-list .gru-input').length,1,'Should have 1 levels');
   });
 });
 
@@ -51,11 +70,9 @@ test('Disabled scoring', function(assert) {
   assert.ok($component.find('.content.rubric.gru-scoring-levels').length,'Missing scoring levels component');
   var $scoringSwitch = $component.find('.content.rubric.gru-scoring-levels .points .gru-switch a input');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .points .point-list').length,'Scoring should appear');
-  var done = assert.async();
-  Ember.run.later(function(){
+  Ember.run(function () {
     $scoringSwitch.click();
-    done();
-  }, 300);
+  });
   return wait().then(function () {
     assert.notOk($component.find('.content.rubric.gru-scoring-levels .points .point-list').length,'Scoring should not appear');
   });
@@ -67,11 +84,9 @@ test('Disabled level', function(assert) {
   assert.ok($component.find('.content.rubric.gru-scoring-levels').length,'Missing scoring levels component');
   var $levelSwitch = $component.find('.content.rubric.gru-scoring-levels .level .gru-switch a input');
   assert.ok($component.find('.content.rubric.gru-scoring-levels .level .levels').length,'Levels should appear');
-  var done = assert.async();
-  Ember.run.later(function(){
+  Ember.run(function () {
     $levelSwitch.click();
-    done();
-  }, 300);
+  });
   return wait().then(function () {
     assert.notOk($component.find('.content.rubric.gru-scoring-levels .level .levels').length,'Levels should not appear');
   });
