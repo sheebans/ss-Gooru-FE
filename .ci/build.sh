65afbc9a2e85dceb8d030f66952933cc9622c6ad
Julio Arias <jarias@users.noreply.github.com>
2017-03-22 13:42:41 -0600

    Improve build times by using docker prebuilt img

--- .ci/build.sh
+++ .ci/build.sh
@@ -19,34 +19,16 @@ export VERSION=${GIT_BRANCH}-${BUILD_NUMBER}
 export QUIZZES_VERSION=${QUIZZES_VERSION}
 
 if [ $UID -eq 0 ]; then
-  info "Running as root creating builder user and dropping privileges"
-  groupadd -r -g 501 builder && useradd -m -r -g builder -u 500 builder
-  curl -sL https://github.com/ncopa/su-exec/archive/v0.2.tar.gz | tar xz -C /tmp
-  CURDIR=$PWD
-  cd /tmp/su-exec-0.2
-  make
-  cd $CURDIR
-  /tmp/su-exec-0.2/su-exec builder $0
+  info "Running as root dropping privileges"
+  /usr/local/bin/su-exec builder $0
   exit $?
 fi
-
-info "Installing global npm dependencies..."
-npm config set prefix '/tmp/node_modules'
-PATH=/tmp/node_modules/bin:$PATH
-silent npm -q install -g npm@latest
-PHANTOMJS_CDNURL=http://cnpmjs.org/downloads
-silent npm -q install -g \
-  ember-cli@2.9.0 \
-  bower@1.7.7 \
-  phantomjs-prebuilt@2.1.3 \
-  stubby@0.2.13 \
-  grunt-cli@0.1.13
 
 info "Installing quizzes addon..."
 silent npm install quizzes-addon-${QUIZZES_VERSION}.tgz
 
 info "Installing npm dependencies..."
-silent npm -q install
+silent yarn install
 
 info "Installing bower dependencies..."
 silent bower install
