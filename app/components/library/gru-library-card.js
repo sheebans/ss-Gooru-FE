ca90c7abb349401bb82dad050cc2a1290fbd7d52
Melany Delgado <melany.delgado@edify.cr>
2017-03-23 16:52:41 -0600

    Create library card

---
+++ app/components/library/gru-library-card.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  classNames: ['library', 'gru-library-card'],
+
+  // -------------------------------------------------------------------------
+  // Properties
+  /**
+   * Object to show in the library card
+   */
+  model:null
+});
---
+++ tests/integration/components/library/gru-library-card-test.js
@@ -0,0 +1,25 @@
+//import { moduleForComponent, test } from 'ember-qunit';
+//import hbs from 'htmlbars-inline-precompile';
+//
+//moduleForComponent('library/gru-library-card', 'Integration | Component | library/gru library card', {
+//  integration: true
+//});
+//
+//test('it renders', function(assert) {
+//
+//  // Set any properties with this.set('myProperty', 'value');
+//  // Handle any actions with this.on('myAction', function(val) { ... });
+//
+//  this.render(hbs`{{library/gru-library-card}}`);
+//
+//  assert.equal(this.$().text().trim(), '');
+//
+//  // Template block usage:
+//  this.render(hbs`
+//    {{#library/gru-library-card}}
+//      template block text
+//    {{/library/gru-library-card}}
+//  `);
+//
+//  assert.equal(this.$().text().trim(), 'template block text');
+//});
