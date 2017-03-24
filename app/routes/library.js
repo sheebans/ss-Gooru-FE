d0969bbc4ef5dd025569fef2eb7664548e9806f1
Melany Delgado <melany.delgado@edify.cr>
2017-03-23 21:02:44 -0600

    Styles for library card and integration with featured courses

--- app/routes/library.js
+++ app/routes/library.js
@@ -9,5 +9,25 @@ import PrivateRouteMixin from 'gooru-web/mixins/private-route-mixin';
  */
 
 export default Ember.Route.extend(PrivateRouteMixin, {
+  // -------------------------------------------------------------------------
+  // Dependencies
+  /**
+   * @type {ProfileService} Search service object
+   */
+  searchService: Ember.inject.service('api-sdk/search'),
 
+  // -------------------------------------------------------------------------
+  // Methods
+
+  model: function() {
+    let route = this;
+    return route.get('searchService').searchFeaturedCourses("*").then(function (result) {
+      return Ember.RSVP.hash({
+        courses: result
+      });
+    });
+  },
+  setupController: function (controller, model) {
+    controller.set('courses', model.courses);
+  }
 });
