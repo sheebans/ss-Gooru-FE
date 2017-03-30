2bb3ee1a779239741b5517464a004ba9bc329332
Melany Delgado <melany.delgado@edify.cr>
2017-03-30 10:24:44 -0600

    Search component for Courses in Performance Dashboard

--- tests/integration/components/student/gru-performance-courses-test.js
+++ tests/integration/components/student/gru-performance-courses-test.js
@@ -1,25 +1,67 @@
-//import { moduleForComponent, test } from 'ember-qunit';
-//import hbs from 'htmlbars-inline-precompile';
-//
-//moduleForComponent('student/gru-performance-courses', 'Integration | Component | student/gru performance courses', {
-//  integration: true
-//});
-//
-//test('it renders', function(assert) {
-//
-//  // Set any properties with this.set('myProperty', 'value');
-//  // Handle any actions with this.on('myAction', function(val) { ... });
-//
-//  this.render(hbs`{{student/gru-performance-courses}}`);
-//
-//  assert.equal(this.$().text().trim(), '');
-//
-//  // Template block usage:
-//  this.render(hbs`
-//    {{#student/gru-performance-courses}}
-//      template block text
-//    {{/student/gru-performance-courses}}
-//  `);
-//
-//  assert.equal(this.$().text().trim(), 'template block text');
-//});
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+import wait from 'ember-test-helpers/wait';
+
+moduleForComponent('student/gru-performance-courses', 'Integration | Component | student/gru performance courses', {
+  integration: true
+});
+
+test('Layout', function(assert) {
+  const courses = [
+    Ember.Object.create({
+      id: '0101',
+      title: 'Course 1'
+    }),
+    Ember.Object.create({
+      id: '0201',
+      title: 'Course 2'
+    }),
+    Ember.Object.create({
+      id: '0301',
+      title: 'Course 3'
+    })
+  ];
+  this.set('courses',courses);
+
+  this.render(hbs`{{student/gru-performance-courses courses=courses}}`);
+
+  const $component = this.$();
+
+  assert.ok($component.find('.student.gru-performance-courses').length, 'Missing component');
+  assert.ok($component.find('.search-navigation .search-keyword input').length, 'Missing search navigation');
+  assert.ok($component.find('.search-navigation .search-keyword .search-icon').length, 'Missing search navigation icon');
+  assert.equal($component.find('.item').length,3, 'Should have 3 icons');
+  assert.notOk($component.find('.no-content').length, 'No content label should not appear');
+  this.set('courses',[]);
+  assert.ok($component.find('.no-content').length, 'No content label should appear');
+});
+
+test('Search course by name', function(assert) {
+  const courses = [
+    Ember.Object.create({
+      id: '0101',
+      title: 'Course 1'
+    }),
+    Ember.Object.create({
+      id: '0201',
+      title: 'Course 2'
+    }),
+    Ember.Object.create({
+      id: '0301',
+      title: 'Course 3'
+    })
+  ];
+  this.set('courses',courses);
+
+  this.render(hbs`{{student/gru-performance-courses courses=courses}}`);
+
+  const $component = this.$();
+  assert.equal($component.find('.item:visible').length,3, 'Should 3 student visible');
+  const $searchInput = $component.find('.search-navigation .search-keyword input');
+  $searchInput.val('Course 3');
+  $searchInput.first().keyup();
+  return wait().then(function () {
+    assert.equal($component.find('.item:visible').length,1, 'Should have only 1 student visible');
+  });
+});
