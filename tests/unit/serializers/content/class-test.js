9533a2c63b5818b21b6ecd4a03f7f8dd08924c0e
gmmendezp <gmmendezp@gmail.com>
2017-04-06 11:18:01 -0600

    NILE-758. Change default value of class visibility to visible all.

--- tests/unit/serializers/content/class-test.js
+++ tests/unit/serializers/content/class-test.js
@@ -15,7 +15,7 @@ test('serializeCreateClass with default visibility', function(assert) {
   assert.equal(response.title, 'class-title', 'Wrong title');
   assert.equal(response.class_sharing, 'open', 'Wrong class sharing');
   assert.equal(response.min_score, 0, 'Wrong min score');
-  assert.equal(response.content_visibility, 'visible_collections', 'Wrong default visibility');
+  assert.equal(response.content_visibility, ClassModel.VISIBLE_ALL, 'Wrong default visibility');
 });
 
 test('serializeUpdateContentVisibility with visibility true', function(assert) {
@@ -49,7 +49,7 @@ test('serializeCreateClass with custom visibility', function(assert) {
   assert.equal(response.title, 'class-title', 'Wrong title');
   assert.equal(response.class_sharing, 'open', 'Wrong class sharing');
   assert.equal(response.min_score, 0, 'Wrong min score');
-  assert.equal(response.content_visibility, 'visible_all', 'Wrong visibility');
+  assert.equal(response.content_visibility, ClassModel.VISIBLE_ALL, 'Wrong visibility');
 });
 
 test('serializeUpdateClass', function(assert) {
@@ -64,7 +64,7 @@ test('serializeUpdateClass', function(assert) {
   assert.equal(response.title, 'class-title', 'Wrong title');
   assert.equal(response.class_sharing, 'open', 'Wrong class sharing');
   assert.equal(response.min_score, 90, 'Wrong min score');
-  assert.equal(response.content_visibility, undefined, 'Wrong visibility, it should not be present');
+  assert.notOk(response.content_visibility, 'Wrong visibility, it should not be present');
   assert.equal(response.greeting, 'class-greeting', 'Wrong greeting');
 
 });
@@ -72,156 +72,156 @@ test('serializeUpdateClass', function(assert) {
 test('normalizeClasses', function(assert) {
   const serializer = this.subject();
   const classesPayload = {
-    "owner": [
-      "d8b6eb04-5466-4e29-92f5-06584b6b6ef5",
-      "2a0a0d90-faed-4ba4-9bab-04ddc4c18f30"
+    owner: [
+      'd8b6eb04-5466-4e29-92f5-06584b6b6ef5',
+      '2a0a0d90-faed-4ba4-9bab-04ddc4c18f30'
     ],
-    "collaborator": ["63dd64b2-bb30-47a9-8daf-8330f57e16c3"],
-    "member": ["e264cdc8-19d1-4285-88f5-5b359daf33da"],
-    "member_count": {
-      "d8b6eb04-5466-4e29-92f5-06584b6b6ef5": 1
+    collaborator: ['63dd64b2-bb30-47a9-8daf-8330f57e16c3'],
+    member: ['e264cdc8-19d1-4285-88f5-5b359daf33da'],
+    member_count: {
+      'd8b6eb04-5466-4e29-92f5-06584b6b6ef5': 1
     },
-    "classes": [{
-      "id": "d8b6eb04-5466-4e29-92f5-06584b6b6ef5",
-      "creator_id": "2afaf0fd-ed92-48c5-9195-7f4bc1e0064e",
-      "created_at": '2016-01-01',
-      "title": "My class - 1",
-      "description": "This class is intended to make awareness of good habits",
-      "greeting": "Hi! Welcome to my class",
-      "grade": [
+    classes: [{
+      id: 'd8b6eb04-5466-4e29-92f5-06584b6b6ef5',
+      creator_id: '2afaf0fd-ed92-48c5-9195-7f4bc1e0064e',
+      created_at: '2016-01-01',
+      title: 'My class - 1',
+      description: 'This class is intended to make awareness of good habits',
+      greeting: 'Hi! Welcome to my class',
+      grade: [
         4,
         5
       ],
-      "class_sharing": "open",
-      "cover_image": "e264cdc8-19d1-4285-88f5-5b359daf33da.png",
-      "code": "VZFMEWH",
-      "min_score": 75,
-      "end_date": "2016-12-31",
-      "course_id": 'course-id',
-      "collaborator": [
-        "63dd64b2-bb30-47a9-8daf-8330f57e16c3",
-        "E4510f0a-a4a1-46d8-b7c3-fce31754c7d8"
+      class_sharing: 'open',
+      cover_image: 'e264cdc8-19d1-4285-88f5-5b359daf33da.png',
+      code: 'VZFMEWH',
+      min_score: 75,
+      end_date: '2016-12-31',
+      course_id: 'course-id',
+      collaborator: [
+        '63dd64b2-bb30-47a9-8daf-8330f57e16c3',
+        'E4510f0a-a4a1-46d8-b7c3-fce31754c7d8'
       ],
-      "gooru_version": 3,
-      "content_visibility": null,
-      "is_archived": false
+      gooru_version: 3,
+      content_visibility: null,
+      is_archived: false
     }, {
-      "id": "2a0a0d90-faed-4ba4-9bab-04ddc4c18f30",
-      "creator_id": "2afaf0fd-ed92-48c5-9195-7f4bc1e0064e",
-      "created_at": '2016-01-01',
-      "title": "My class - 2",
-      "description": "This class is intended to make awareness of good habits",
-      "greeting": "Hi! Welcome to my class",
-      "grade": [
+      id: '2a0a0d90-faed-4ba4-9bab-04ddc4c18f30',
+      creator_id: '2afaf0fd-ed92-48c5-9195-7f4bc1e0064e',
+      created_at: '2016-01-01',
+      title: 'My class - 2',
+      description: 'This class is intended to make awareness of good habits',
+      greeting: 'Hi! Welcome to my class',
+      grade: [
         4,
         5
       ],
-      "class_sharing": "open",
-      "cover_image": "e264cdc8-19d1-4285-88f5-5b359daf33da.png",
-      "code": "ALU3LCB",
-      "end_date": "2016-12-31",
-      "course_id": 'course-id',
-      "collaborator": [
-        "63dd64b2-bb30-47a9-8daf-8330f57e16c3",
-        "E4510f0a-a4a1-46d8-b7c3-fce31754c7d8"
+      class_sharing: 'open',
+      cover_image: 'e264cdc8-19d1-4285-88f5-5b359daf33da.png',
+      code: 'ALU3LCB',
+      end_date: '2016-12-31',
+      course_id: 'course-id',
+      collaborator: [
+        '63dd64b2-bb30-47a9-8daf-8330f57e16c3',
+        'E4510f0a-a4a1-46d8-b7c3-fce31754c7d8'
       ],
-      "gooru_version": 3,
-      "content_visibility": null,
-      "is_archived": false
+      gooru_version: 3,
+      content_visibility: null,
+      is_archived: false
     }]
   };
 
   const normalizedClasses = serializer.normalizeClasses(classesPayload);
-  assert.equal(normalizedClasses.get("ownerList.length"), 2, 'Wrong ownerList');
-  assert.equal(normalizedClasses.get("collaboratorList.length"), 1, 'Wrong collaboratorList');
-  assert.equal(normalizedClasses.get("memberCount")['d8b6eb04-5466-4e29-92f5-06584b6b6ef5'], 1, 'Wrong memberCount');
-  assert.equal(normalizedClasses.get("classes.length"), 2, 'Wrong classes');
-  assert.equal(normalizedClasses.get("classes")[0].get('minScore'), 75, 'Wrong min score');
-  assert.equal(normalizedClasses.get("classes")[0].get('contentVisibility'), 'visible_none', 'Wrong default visibility when receiving null');
-  assert.equal(normalizedClasses.get("classes")[1].get('minScore'), null, 'Wrong min score');
+  assert.equal(normalizedClasses.get('ownerList.length'), 2, 'Wrong ownerList');
+  assert.equal(normalizedClasses.get('collaboratorList.length'), 1, 'Wrong collaboratorList');
+  assert.equal(normalizedClasses.get('memberCount')['d8b6eb04-5466-4e29-92f5-06584b6b6ef5'], 1, 'Wrong memberCount');
+  assert.equal(normalizedClasses.get('classes.length'), 2, 'Wrong classes');
+  assert.equal(normalizedClasses.get('classes')[0].get('minScore'), 75, 'Wrong min score');
+  assert.equal(normalizedClasses.get('classes')[0].get('contentVisibility'), ClassModel.VISIBLE_NONE, 'Wrong default visibility when receiving null');
+  assert.equal(normalizedClasses.get('classes')[1].get('minScore'), null, 'Wrong min score');
 });
 
 test('normalizeReadClassInfo', function(assert) {
   const serializer = this.subject();
   const classPayload = {
-    "id": "d44d3928-2623-4925-9d38-e933650a7573",
-    "creator_id": "327598a1-109a-4bcc-be9a-357981711381",
-    "created_at": '2016-01-01',
-    "title": "My Class 1",
-    "description": 'Class description',
-    "greeting": 'Greeting message',
-    "grade": null,
-    "class_sharing": "open",
-    "cover_image": 'image-name',
-    "code": "ABC123",
-    "min_score": 0,
-    "end_date": "2016-01-01",
-    "course_id": "d44d3928-2623-4925-9d38-e933650a7573",
-    "course_title": "Course 1",
-    "collaborator": ['1', '2'],
-    "gooru_version": 3,
-    "content_visibility": null,
-    "is_archived": false
+    id: 'd44d3928-2623-4925-9d38-e933650a7573',
+    creator_id: '327598a1-109a-4bcc-be9a-357981711381',
+    created_at: '2016-01-01',
+    title: 'My Class 1',
+    description: 'Class description',
+    greeting: 'Greeting message',
+    grade: null,
+    class_sharing: 'open',
+    cover_image: 'image-name',
+    code: 'ABC123',
+    min_score: 0,
+    end_date: '2016-01-01',
+    course_id: 'd44d3928-2623-4925-9d38-e933650a7573',
+    course_title: 'Course 1',
+    collaborator: ['1', '2'],
+    gooru_version: 3,
+    content_visibility: null,
+    is_archived: false
   };
 
   const normalizedClassInfo = serializer.normalizeReadClassInfo(classPayload);
-  assert.equal(normalizedClassInfo.get("id"), "d44d3928-2623-4925-9d38-e933650a7573", 'Wrong id');
-  assert.equal(normalizedClassInfo.get("creatorId"), "327598a1-109a-4bcc-be9a-357981711381", 'Wrong creator id');
-  assert.equal(normalizedClassInfo.get("owner.id"), "327598a1-109a-4bcc-be9a-357981711381", 'Wrong owner id');
-  assert.equal(normalizedClassInfo.get("code"), "ABC123", 'Wrong code');
-  assert.equal(normalizedClassInfo.get("title"), "My Class 1", 'Wrong title');
-  assert.equal(normalizedClassInfo.get("description"), "Class description", 'Wrong description');
-  assert.equal(normalizedClassInfo.get("courseId"), "d44d3928-2623-4925-9d38-e933650a7573", 'Wrong course id');
-  assert.equal(normalizedClassInfo.get("courseTitle"), "Course 1", 'Wrong course title');
-  assert.equal(normalizedClassInfo.get("greeting"), "Greeting message", 'Wrong greeting');
-  assert.equal(normalizedClassInfo.get("grade.length"), 0, 'Wrong grade length');
-  assert.equal(normalizedClassInfo.get("classSharing"), "open", 'Wrong classSharing');
-  assert.equal(normalizedClassInfo.get("coverImage"), "image-name", 'Wrong coverImage');
-  assert.equal(normalizedClassInfo.get("minScore"), null, 'Wrong minScore');
-  assert.equal(normalizedClassInfo.get("startDate"), '2016-01-01', 'Wrong startDate');
-  assert.equal(normalizedClassInfo.get("endDate"), '2016-01-01', 'Wrong endDate');
-  assert.equal(normalizedClassInfo.get("creatorSystem"), '', 'Wrong creator system');
-  assert.equal(normalizedClassInfo.get("contentVisibility"), 'visible_none', 'Wrong visibility when receiving null');
-  assert.equal(normalizedClassInfo.get("isArchived"), false, 'Wrong creator is archived');
-  assert.equal(normalizedClassInfo.get("collaborators.length"), 2, 'Wrong collaborators');
-  assert.equal(normalizedClassInfo.get("collaborators")[0].get("id"), '1', 'Wrong collaborator id');
+  assert.equal(normalizedClassInfo.get('id'), 'd44d3928-2623-4925-9d38-e933650a7573', 'Wrong id');
+  assert.equal(normalizedClassInfo.get('creatorId'), '327598a1-109a-4bcc-be9a-357981711381', 'Wrong creator id');
+  assert.equal(normalizedClassInfo.get('owner.id'), '327598a1-109a-4bcc-be9a-357981711381', 'Wrong owner id');
+  assert.equal(normalizedClassInfo.get('code'), 'ABC123', 'Wrong code');
+  assert.equal(normalizedClassInfo.get('title'), 'My Class 1', 'Wrong title');
+  assert.equal(normalizedClassInfo.get('description'), 'Class description', 'Wrong description');
+  assert.equal(normalizedClassInfo.get('courseId'), 'd44d3928-2623-4925-9d38-e933650a7573', 'Wrong course id');
+  assert.equal(normalizedClassInfo.get('courseTitle'), 'Course 1', 'Wrong course title');
+  assert.equal(normalizedClassInfo.get('greeting'), 'Greeting message', 'Wrong greeting');
+  assert.equal(normalizedClassInfo.get('grade.length'), 0, 'Wrong grade length');
+  assert.equal(normalizedClassInfo.get('classSharing'), 'open', 'Wrong classSharing');
+  assert.equal(normalizedClassInfo.get('coverImage'), 'image-name', 'Wrong coverImage');
+  assert.equal(normalizedClassInfo.get('minScore'), null, 'Wrong minScore');
+  assert.equal(normalizedClassInfo.get('startDate'), '2016-01-01', 'Wrong startDate');
+  assert.equal(normalizedClassInfo.get('endDate'), '2016-01-01', 'Wrong endDate');
+  assert.equal(normalizedClassInfo.get('creatorSystem'), '', 'Wrong creator system');
+  assert.equal(normalizedClassInfo.get('contentVisibility'), ClassModel.VISIBLE_NONE, 'Wrong visibility when receiving null');
+  assert.equal(normalizedClassInfo.get('isArchived'), false, 'Wrong creator is archived');
+  assert.equal(normalizedClassInfo.get('collaborators.length'), 2, 'Wrong collaborators');
+  assert.equal(normalizedClassInfo.get('collaborators')[0].get('id'), '1', 'Wrong collaborator id');
 
 });
 
 test('normalizeReadClassMembers', function(assert) {
   const serializer = this.subject();
   const classPayload = {
-    "owner": ["08fd3a1a-8118-4b02-ab59-c0b4a5037863"],
-    "collaborator": [],
-    "member": ["e568193d-603a-4c3a-a5d2-320579d32f3f"],
-    "invitees": [],
-    "details": [{
-      "id": "08fd3a1a-8118-4b02-ab59-c0b4a5037863",
-      "first_name": "Jeffrey",
-      "last_name": "Bermudez",
-      "thumbnail": null
+    owner: ['08fd3a1a-8118-4b02-ab59-c0b4a5037863'],
+    collaborator: [],
+    member: ['e568193d-603a-4c3a-a5d2-320579d32f3f'],
+    invitees: [],
+    details: [{
+      id: '08fd3a1a-8118-4b02-ab59-c0b4a5037863',
+      first_name: 'Jeffrey',
+      last_name: 'Bermudez',
+      thumbnail: null
     }, {
-      "id": "e568193d-603a-4c3a-a5d2-320579d32f3f",
-      "first_name": "Jeffrey",
-      "last_name": "Bermudez",
-      "thumbnail": null
+      id: 'e568193d-603a-4c3a-a5d2-320579d32f3f',
+      first_name: 'Jeffrey',
+      last_name: 'Bermudez',
+      thumbnail: null
     }]
   };
   const normalizedClassMembers = serializer.normalizeReadClassMembers(classPayload);
-  assert.equal(normalizedClassMembers.get("owner.id"), '08fd3a1a-8118-4b02-ab59-c0b4a5037863', 'Wrong owner id');
-  assert.equal(normalizedClassMembers.get("collaborators").length, 0, 'Wrong collaborators');
-  assert.equal(normalizedClassMembers.get("members").length, 1, 'Wrong members');
-  assert.equal(normalizedClassMembers.get("members")[0].get("id"), 'e568193d-603a-4c3a-a5d2-320579d32f3f', 'Wrong members');
+  assert.equal(normalizedClassMembers.get('owner.id'), '08fd3a1a-8118-4b02-ab59-c0b4a5037863', 'Wrong owner id');
+  assert.equal(normalizedClassMembers.get('collaborators').length, 0, 'Wrong collaborators');
+  assert.equal(normalizedClassMembers.get('members').length, 1, 'Wrong members');
+  assert.equal(normalizedClassMembers.get('members')[0].get('id'), 'e568193d-603a-4c3a-a5d2-320579d32f3f', 'Wrong members');
 });
 
 test('normalizeReadClassContentVisibility', function(assert) {
   const serializer = this.subject();
   const payload = {
-    "content_visibility": 'visible_none',
-    "course": 'fake-content'
+    content_visibility: 'visible_none',
+    course: 'fake-content'
   };
 
   const classContentVisibility = serializer.normalizeReadClassContentVisibility(payload);
-  assert.equal(classContentVisibility.get("contentVisibility"), 'visible_none', 'Wrong content visibility');
-  assert.equal(classContentVisibility.get("course"), 'fake-content', 'Wrong course');
+  assert.equal(classContentVisibility.get('contentVisibility'), ClassModel.VISIBLE_NONE, 'Wrong content visibility');
+  assert.equal(classContentVisibility.get('course'), 'fake-content', 'Wrong course');
 });
