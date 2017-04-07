e27045123f6a163d3d1ef4cba3cf9039a9d38735
Jeffrey A. Bermúdez P <jeffrey.bermudez@edify.cr>
2017-04-07 12:43:10 -0600

    NILE-821: Fixing issue in My Report that is not showing the Collections data. Adding fix for My Performance report that does not require Class ID (corresponding to NILE-820)

--- app/adapters/performance/collection-performance-summary.js
+++ app/adapters/performance/collection-performance-summary.js
@@ -1,18 +1,19 @@
-import Ember from "ember";
+import Ember from 'ember';
+import ApplicationAdapter from 'gooru-web/adapters/application';
 
 /**
  * Adapter CollectionPerformanceSummary
  *
  * @typedef {Object} CollectionPerformanceSummaryAdapter
  */
-export default Ember.Object.extend({
+export default ApplicationAdapter.extend({
 
   session: Ember.inject.service(),
 
-  namespace: "/api/nucleus-insights/v2",
+  namespace: '/api/nucleus-insights/v2',
 
   /**
-   * Searches student collection performance by course, unit, lesson and type
+   * Searches student collection performance by course, class, unit, lesson and type
    * Criteria values are not required except for courseId
    *
    * @param {string} studentId
@@ -20,14 +21,14 @@ export default Ember.Object.extend({
    * @returns {Promise}
    */
   searchStudentCollectionPerformanceSummary: function (studentId, criteria) {
-    const namespace = this.get("namespace");
-    const collectionType = criteria.collectionType || "assessment";
-    const url = `${namespace}/study/${collectionType}/performance`;
+    let adapter = this;
+    const collectionType = criteria.collectionType || 'assessment';
+    const url = `${adapter.get('namespace')}/study/${collectionType}/performance`;
     const options = {
-      type: "GET",
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      headers: this.defineHeaders(),
+      type: 'GET',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      headers: adapter.get('headers'),
       data: {
         courseId: criteria.courseId,
         classId: criteria.classId,
@@ -44,6 +45,28 @@ export default Ember.Object.extend({
     });
   },
 
+  findMyPerformance: function (userId, courseId, lessonId, unitId, collectionType = 'assessment') {
+    let adapter = this;
+    const url = `${adapter.get('namespace')}/learner/course/${collectionType}s/performance`;
+    const options = {
+      type: 'GET',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      headers: adapter.get('headers'),
+      data: {
+        courseId,
+        lessonId,
+        unitId,
+        userId
+      }
+    };
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      Ember.$.ajax(url, options)
+        .then(function (responseData) {
+          resolve(responseData);
+        }, reject);
+    });
+  },
 
   /**
    * Finds collection performance summary for the ids provided
@@ -54,14 +77,15 @@ export default Ember.Object.extend({
    * @param {string} timePeriod optional time period filter
    * @returns {Ember.RSVP.Promise}
      */
-  findCollectionPerformanceSummaryByIds: function (userId, collectionIds, collectionType, classId = undefined, timePeriod = undefined) {
-    const namespace = this.get("namespace");
-    const url = `${namespace}/${collectionType}/performance`;
+  findCollectionPerformanceSummaryByIds: function (userId, collectionIds, collectionType, classId = undefined,
+                                                   timePeriod = undefined) {
+    let adapter = this;
+    const url = `${adapter.get('namespace')}/${collectionType}/performance`;
     const options = {
-      type: "POST",
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      headers: this.defineHeaders(),
+      type: 'POST',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      headers: adapter.get('headers'),
       data: JSON.stringify({
         userId: userId,
         classId: classId,
@@ -75,12 +99,6 @@ export default Ember.Object.extend({
           resolve(responseData);
         }, reject);
     });
-  },
-
-  defineHeaders: function () {
-    return {
-      "Authorization": "Token " + this.get("session.token-api3")
-    };
   }
 
 });
