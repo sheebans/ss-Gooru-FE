ea57b45af90ef72b47243182cbb843e8bb4f1f5d
Melany Delgado <melany.delgado@edify.cr>
2017-04-12 16:40:30 -0600

    0.6 QA [Student Landing] If student has not started a class, on the class card, shows Performance + Completed 0%

--- tests/acceptance/student-home-test.js
+++ tests/acceptance/student-home-test.js
@@ -104,3 +104,25 @@ test('Go to course map from class card', function(assert) {
     });
   });
 });
+
+test('Valid bubble chart when the class does not has performance', function(assert) {
+  visit('/student-home');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/student-home');
+    let $chart = find('.gru-student-class-card:eq(1) .gru-bubble-chart .bubble-circle');
+    assert.equal($chart.attr('style'),'background-color:#949A9F','Incorrect chart color');
+    assert.equal($chart.find('span').text(),'--','Incorrect score');
+  });
+});
+
+test('Valid bubble chart when the class has performance', function(assert) {
+  visit('/student-home');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/student-home');
+    let $chart = find('.gru-student-class-card:eq(0) .gru-bubble-chart .bubble-circle');
+    assert.equal($chart.attr('style'),'background-color:#F46360','Incorrect chart color');
+    assert.equal($chart.find('span').text(),'0%','Incorrect score');
+  });
+});
