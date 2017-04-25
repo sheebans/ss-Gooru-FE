32510e932e6acfed4a51c31fe81746ebb5b6e209
alerg11 <alerg11@gmail.com>
2017-04-25 12:02:56 -0600

    NILE-896 Added out-of-scope

--- app/components/gru-user-icons.js
+++ app/components/gru-user-icons.js
@@ -17,7 +17,7 @@ export default Ember.Component.extend({
 
   // -------------------------------------------------------------------------
   // Attributes
-  classNames: ['gru-user-icons'],
+  classNames: ['gru-user-icons, out-of-scope'],
 
   // -------------------------------------------------------------------------
   // Actions
