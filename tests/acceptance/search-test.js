ef4ad429849815b793fc913a1c634dfaf2f93676
alerg11 <alerg11@gmail.com>
2017-04-28 12:08:38 -0600

    NILE-920  Eslint fix

--- tests/acceptance/search-test.js
+++ tests/acceptance/search-test.js
@@ -1,7 +1,6 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'gooru-web/tests/helpers/module-for-acceptance';
 import { authenticateSession } from 'gooru-web/tests/helpers/ember-simple-auth';
-import T from 'gooru-web/tests/helpers/assert';
 
 moduleForAcceptance('Acceptance | Search page', {
   beforeEach: function() {
