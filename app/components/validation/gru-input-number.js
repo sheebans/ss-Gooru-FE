d3545049150b5275539e5f80baea1db8794235b4
Melany Delgado <melany.delgado@edify.cr>
2017-05-08 17:00:48 -0600

    Replace double quotes

--- app/components/validation/gru-input-number.js
+++ app/components/validation/gru-input-number.js
@@ -1,5 +1,5 @@
 import GruInput from 'gooru-web/components/validation/gru-input';
-import Ember from "ember";
+import Ember from 'ember';
 
 /**
  * Number field with validation (It only accepts integers)
