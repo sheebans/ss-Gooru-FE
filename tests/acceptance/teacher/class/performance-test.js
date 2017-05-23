862157e1a0690b2325e087493ebbf32fc113761a
Melany Delgado <melany.delgado@edify.cr>
2017-05-23 09:29:37 -0600

    Fix failed tests

--- tests/acceptance/teacher/class/performance-test.js
+++ tests/acceptance/teacher/class/performance-test.js
@@ -23,23 +23,23 @@ test('Layout at course level', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance');
 
-    const $performanceContainer = find(".controller.teacher.class.performance");
-    T.exists(assert, $performanceContainer.find(".gru-metrics-table"), "Missing metrics table component");
+    const $performanceContainer = find('.controller.teacher.class.performance');
+    T.exists(assert, $performanceContainer.find('.gru-metrics-table'), 'Missing metrics table component');
 
-    const $actions = $performanceContainer.find(".actions");
-    assert.equal($actions.find('.radio-button').length, 2, "Incorrect number of radio buttons");
+    const $actions = $performanceContainer.find('.actions');
+    assert.equal($actions.find('.radio-button').length, 2, 'Incorrect number of radio buttons');
     assert.ok($actions.find('.report-btn').length, 'Missing download button');
 
     const $options = $performanceContainer.find('.options');
     const $dataPicker = $options.find('.data-picker .gru-data-picker');
     assert.ok($dataPicker.length, 'Missing data picker');
 
-    const $info = $performanceContainer.find(".info");
-    const $breadcrumb = $info.find(".teacher-breadcrumb");
-    const $breadcrumbItems = $breadcrumb.find("button");
-    assert.equal($breadcrumbItems.length, 1, "Incorrect number of breadcrumb items");
-    assert.equal(T.text($breadcrumb.find("button:last-child")), 'Release time! - Course 1', "Wrong breadcrumb item label");
-    const $legend = $performanceContainer.find(".info .grading-scale-legend");
+    const $info = $performanceContainer.find('.info');
+    const $breadcrumb = $info.find('.teacher-breadcrumb');
+    const $breadcrumbItems = $breadcrumb.find('button');
+    assert.equal($breadcrumbItems.length, 1, 'Incorrect number of breadcrumb items');
+    assert.equal(T.text($breadcrumb.find('button:last-child')), 'Release time! - Course 1', 'Wrong breadcrumb item label');
+    const $legend = $performanceContainer.find('.info .grading-scale-legend');
     assert.ok($legend.length, 'Missing grading scale legend');
 
   });
@@ -52,10 +52,10 @@ test('Navigate to unit', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance');
 
-    const $performanceContainer = find(".controller.teacher.class.performance");
-    const $metricTable = $performanceContainer.find(".gru-metrics-table");
+    const $performanceContainer = find('.controller.teacher.class.performance');
+    const $metricTable = $performanceContainer.find('.gru-metrics-table');
 
-    click($metricTable.find("thead tr:eq(0) th:eq(1)"));
+    click($metricTable.find('thead tr:eq(0) th:eq(1)'));
     andThen(function(){
       assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?unitId=first-unit-id');
     });
@@ -68,33 +68,33 @@ test('Test data picker options selected', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance');
-    const $performanceContainer = find(".controller.teacher.class.performance");
+    const $performanceContainer = find('.controller.teacher.class.performance');
 
     const $options = $performanceContainer.find('.options');
     const $dataPicker = $options.find('.data-picker .gru-data-picker');
-    click($dataPicker.find("ul input:eq(1)")); //click on completion
+    click($dataPicker.find('ul input:eq(1)')); //click on completion
     andThen(function(){
-      const $metricTable = $performanceContainer.find(".gru-metrics-table");
-      const $performanceInformation = $metricTable.find(".gru-metrics-performance-information");
+      const $metricTable = $performanceContainer.find('.gru-metrics-table');
+      const $performanceInformation = $metricTable.find('.gru-metrics-performance-information');
 
       //data picker score item is selected by default
-      T.exists(assert, $performanceInformation.find(".score"), "Missing data picker score information at course level");
-      T.exists(assert, $performanceInformation.find(".completion"), "Missing  data picker completion information at course level");
-      T.notExists(assert, $performanceInformation.find(".study-time"), "Study time item shouldn't be selected at course level");
+      T.exists(assert, $performanceInformation.find('.score'), 'Missing data picker score information at course level');
+      T.exists(assert, $performanceInformation.find('.completion'), 'Missing  data picker completion information at course level');
+      T.notExists(assert, $performanceInformation.find('.study-time'), 'Study time item should not be selected at course level');
 
-      click($metricTable.find("thead tr:eq(0) th:eq(1)"));
+      click($metricTable.find('thead tr:eq(0) th:eq(1)'));
       andThen(function(){
         assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?unitId=first-unit-id');
 
-        const $unitPerformanceContainer = find(".controller.teacher.class.performance");
-        const $unitMetricTable = $unitPerformanceContainer.find(".gru-metrics-table");
-        const $unitPerformanceInformation = $unitMetricTable.find(".gru-metrics-performance-information");
+        const $unitPerformanceContainer = find('.controller.teacher.class.performance');
+        const $unitMetricTable = $unitPerformanceContainer.find('.gru-metrics-table');
+        const $unitPerformanceInformation = $unitMetricTable.find('.gru-metrics-performance-information');
 
         //data picker score item is selected by default
-        T.exists(assert, $unitPerformanceInformation.find(".score"), "Missing  data picker score information at unit level");
+        T.exists(assert, $unitPerformanceInformation.find('.score'), 'Missing  data picker score information at unit level');
 
-        T.exists(assert, $unitPerformanceInformation.find(".completion"), "Missing  data picker completion information at unit level");
-        T.notExists(assert, $unitPerformanceInformation.find(".study-time"), "Study time item shouldn't be selected at unit level");
+        T.exists(assert, $unitPerformanceInformation.find('.completion'), 'Missing  data picker completion information at unit level');
+        T.notExists(assert, $unitPerformanceInformation.find('.study-time'), 'Study time item should not be selected at unit level');
 
       });
     });
@@ -108,8 +108,8 @@ test('Test collections/assessments filter radio button selected', function(asser
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance');
 
-    const $performanceContainer = find(".controller.teacher.class.performance");
-    const $actions = $performanceContainer.find(".actions");
+    const $performanceContainer = find('.controller.teacher.class.performance');
+    const $actions = $performanceContainer.find('.actions');
     const $assessmentRadioButton = $actions.find('.radios-container .assessment');
     const $collectionRadioButton = $actions.find('.radios-container .collection');
 
@@ -132,11 +132,11 @@ test('Layout at unit level', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?unitId=first-unit-id');
 
-    const $performanceContainer = find(".controller.teacher.class.performance");
-    T.exists(assert, $performanceContainer.find(".gru-metrics-table"), "Missing metrics table component");
+    const $performanceContainer = find('.controller.teacher.class.performance');
+    T.exists(assert, $performanceContainer.find('.gru-metrics-table'), 'Missing metrics table component');
 
-    const $actions = $performanceContainer.find(".actions");
-    assert.equal($actions.find('.radio-button').length, 2, "Incorrect number of radio buttons");
+    const $actions = $performanceContainer.find('.actions');
+    assert.equal($actions.find('.radio-button').length, 2, 'Incorrect number of radio buttons');
     assert.ok($actions.find('.report-btn').length, 'Missing report button');
 
     const $options = $performanceContainer.find('.options');
@@ -144,12 +144,12 @@ test('Layout at unit level', function(assert) {
     assert.ok($dataPicker.length, 'Missing data picker');
 
     //assert breadcrumb text
-    const $info = $performanceContainer.find(".info");
-    const $breadcrumb = $info.find(".teacher-breadcrumb");
-    const $breadcrumbItems = $breadcrumb.find("button");
-    assert.equal($breadcrumbItems.length, 2, "Incorrect number of breadcrumb items");
-    assert.equal(T.text($breadcrumb.find("button:last-child")), 'Food', "Wrong breadcrumb item label");
-    const $legend = $performanceContainer.find(".info .grading-scale-legend");
+    const $info = $performanceContainer.find('.info');
+    const $breadcrumb = $info.find('.teacher-breadcrumb');
+    const $breadcrumbItems = $breadcrumb.find('button');
+    assert.equal($breadcrumbItems.length, 2, 'Incorrect number of breadcrumb items');
+    assert.equal(T.text($breadcrumb.find('button:last-child')), 'Food', 'Wrong breadcrumb item label');
+    const $legend = $performanceContainer.find('.info .grading-scale-legend');
     assert.ok($legend.length, 'Missing grading scale legend');
   });
 });
@@ -162,10 +162,10 @@ test('Navigate to lesson', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?unitId=first-unit-id');
 
-    const $performanceContainer = find(".controller.teacher.class.performance");
-    const $metricTable = $performanceContainer.find(".gru-metrics-table");
+    const $performanceContainer = find('.controller.teacher.class.performance');
+    const $metricTable = $performanceContainer.find('.gru-metrics-table');
 
-    click($metricTable.find("thead tr:eq(0) th:eq(3)"));
+    click($metricTable.find('thead tr:eq(0) th:eq(3)'));
     andThen(function(){
       assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?lessonId=first-lesson-id&unitId=first-unit-id');
     });
@@ -178,20 +178,20 @@ test('Layout for lesson', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?lessonId=first-lesson-id&unitId=first-unit-id');
 
-    const $performanceContainer = find(".controller.teacher.class.performance");
-    T.exists(assert, $performanceContainer.find(".gru-metrics-table"), "Missing metrics table component");
+    const $performanceContainer = find('.controller.teacher.class.performance');
+    T.exists(assert, $performanceContainer.find('.gru-metrics-table'), 'Missing metrics table component');
 
     const $options = $performanceContainer.find('.options');
     const $dataPicker = $options.find('.data-picker .gru-data-picker');
     assert.ok($dataPicker.length, 'Missing data picker');
 
     //assert breadcrumb text
-    const $info = $performanceContainer.find(".info");
-    const $breadcrumb = $info.find(".teacher-breadcrumb");
-    const $breadcrumbItems = $breadcrumb.find("button");
-    assert.equal($breadcrumbItems.length, 3, "Incorrect number of breadcrumb items");
-    assert.equal(T.text($breadcrumb.find("button:last-child")), 'Release Day Quiz', "Wrong breadcrumb item label");
-    const $legend = $performanceContainer.find(".info .grading-scale-legend");
+    const $info = $performanceContainer.find('.info');
+    const $breadcrumb = $info.find('.teacher-breadcrumb');
+    const $breadcrumbItems = $breadcrumb.find('button');
+    assert.equal($breadcrumbItems.length, 3, 'Incorrect number of breadcrumb items');
+    assert.equal(T.text($breadcrumb.find('button:last-child')), 'Release Day Quiz', 'Wrong breadcrumb item label');
+    const $legend = $performanceContainer.find('.info .grading-scale-legend');
     assert.ok($legend.length, 'Missing grading scale legend');
   });
 });
@@ -202,10 +202,10 @@ test('Navigate to collection', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?lessonId=first-lesson-id&unitId=first-unit-id');
 
-    const $performanceContainer = find(".controller.teacher.class.performance");
-    const $metricTable = $performanceContainer.find(".gru-metrics-table");
+    const $performanceContainer = find('.controller.teacher.class.performance');
+    const $metricTable = $performanceContainer.find('.gru-metrics-table');
 
-    click($metricTable.find("thead tr:eq(0) th:eq(1)"));
+    click($metricTable.find('thead tr:eq(0) th:eq(1)'));
     andThen(function(){
       assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?collectionId=first-assessment-id&lessonId=first-lesson-id&unitId=first-unit-id');
     });
@@ -218,15 +218,15 @@ test('Navigate to student report and go back to data', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/teacher/class/class-for-pochita-as-teacher/performance?lessonId=first-lesson-id&unitId=first-unit-id');
 
-    const $performanceContainer = find(".controller.teacher.class.performance");
-    const $metricTable = $performanceContainer.find(".gru-metrics-table");
+    const $performanceContainer = find('.controller.teacher.class.performance');
+    const $metricTable = $performanceContainer.find('.gru-metrics-table');
 
-    click($metricTable.find("tbody tr:eq(2) td:eq(1) .score"));
+    click($metricTable.find('tbody tr:eq(2) td:eq(1) .report span'));
     andThen(function(){
-      assert.equal(currentURL(), '/reports/student-collection?classId=462bcc67-1717-4140-bdc0-672e7bf4cdb1&collectionId=first-assessment-id&courseId=course-123&lessonId=first-lesson-id&role=teacher&type=assessment&unitId=first-unit-id&userId=c4176d77-2507-4bd0-b1a1-308ad98526e5');
+      assert.equal(currentURL(), '/reports/student-collection-analytics?classId=462bcc67-1717-4140-bdc0-672e7bf4cdb1&collectionId=first-assessment-id&courseId=course-123&lessonId=first-lesson-id&role=teacher&type=assessment&unitId=first-unit-id&userId=c4176d77-2507-4bd0-b1a1-308ad98526e5');
       //menu is still selected
-      const $container = find(".controller.analytics.collection.student");
-      T.exists(assert, $container, "Missing container");
+      const $container = find('.controller.analytics.collection.student');
+      T.exists(assert, $container, 'Missing container');
     });
   });
 });
