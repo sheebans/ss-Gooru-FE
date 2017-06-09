2a5742e36654fea741e0c2174f98ce1ecbc6aaad
Martin Mendez <gmmendezp@gmail.com>
2017-06-09 11:11:17 -0600

    NILE-968. Fix exit panel not appearing in resource player.

--- app/components/player/gru-player-done.js
+++ app/components/player/gru-player-done.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['player','gru-player-done'],
   // -------------------------------------------------------------------------
   // Properties
 
