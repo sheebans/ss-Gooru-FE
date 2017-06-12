7e06775e85e8d271e180a6cf6e0d35eff3a983c8
Melany Delgado <melany.delgado@edify.cr>
2017-06-12 12:56:05 -0600

    Request report for Archived Classrooms

--- app/controllers/teacher-home.js
+++ app/controllers/teacher-home.js
@@ -21,24 +21,6 @@ export default Ember.Controller.extend(ModalMixin, {
     showClasses: function (type) {
       this.set('showActiveClasses', type === 'active');
       this.set('showArchivedClasses', type === 'archived');
-    },
-    downloadReport: function (aClass) {
-      const classId = aClass.get('id');
-      const courseId = aClass.get('courseId');
-      const basePath = `${window.location.protocol}//${window.location.host}`;
-      const userId = this.get('session.userId');
-      const sessionToken = encodeURIComponent(this.get('session.token-api3'));
-      const url = `${basePath}/api/nucleus-download-reports/v1/class/${classId}/course/${courseId}/download/file?sessionToken=${sessionToken}&userId=${userId}`;
-      Ember.$('#download_iframe').attr('src', url);
-    },
-
-    requestReport: function (aClass) {
-      const classId = aClass.get('id');
-      const courseId = aClass.get('courseId');
-      const userId = this.get('session.userId');
-      this.get('classService').requestClassReport(classId, courseId, userId).then(function(status){
-        aClass.set('reportStatus', status);
-      });
     }
   },
 
