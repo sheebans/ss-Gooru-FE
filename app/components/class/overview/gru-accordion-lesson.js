c61fc45e0522dd65fa030cd25922ec3b306d71d7
Melany Delgado <melany.delgado@edify.cr>
2017-06-15 18:14:33 -0600

    Create a computed property to define when show trophy

--- app/components/class/overview/gru-accordion-lesson.js
+++ app/components/class/overview/gru-accordion-lesson.js
@@ -425,6 +425,10 @@ export default Ember.Component.extend(AccordionMixin, {
             collectionPerformanceData.set('isCompleted', isCompleted);
 
             collection.set('performance', collectionPerformanceData);
+
+            let showTrophy = collection.get('performance.hasTrophy') && component.get('isStudent')  && !collection.get('collectionSubType');
+            collection.set('showTrophy',showTrophy);
+
             const attempts = collectionPerformanceData.get('attempts');
             if(isAssessment) {
               return component.get('assessmentService').readAssessment(collectionId)
@@ -440,6 +444,8 @@ export default Ember.Component.extend(AccordionMixin, {
               return Ember.RSVP.resolve(true);
             }
           });
+
+
           Ember.RSVP.all(promises).then(resolve, reject);
         });
     });
