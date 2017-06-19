3c2af616492e1af0a7b70b3491de4bdd5c7916e0
gmmendezp <gmmendezp@gmail.com>
2017-06-19 10:42:08 -0600

    NILE-1207. Change featured courses layout.

--- app/components/library/gru-browse-library.js
+++ app/components/library/gru-browse-library.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { sortFeaturedCourses,getSubjects } from 'gooru-web/utils/sort-featured-courses';
 
 export default Ember.Component.extend({
   // -------------------------------------------------------------------------
@@ -17,5 +18,18 @@ export default Ember.Component.extend({
   /**
    * @property {Courses[]} courses to show
    */
-  courses: null
+  courses: null,
+
+  /**
+   * @property {Object[]} group courses by subject
+   */
+  formattedContent: Ember.computed('courses', function(){
+    return getSubjects(this.get('courses')).map(
+      (subjectBucket, index) => Ember.Object.create({
+        'category': subjectBucket.subject.slice(subjectBucket.subject.indexOf('.')+1,subjectBucket.subject.lastIndexOf('.')),
+        'subject': subjectBucket.taxonomySubject,
+        'courses': sortFeaturedCourses(this.get('courses'))[index]
+      })
+    );
+  })
 });
