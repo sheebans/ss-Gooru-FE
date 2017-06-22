64cfbb7ba94e80c20d6c281f4c193f420b6f0101
Melany Delgado <melany.delgado@edify.cr>
2017-06-22 15:00:05 -0600

    Change tooltip to collections and assessments

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -73,6 +73,7 @@ export default Object.assign(quizzesTranslations, {
     "add-standards-to-collection": "Add Standards to Collection",
     "add-to": "Add To",
     "add-to-classroom":"Add to Classroom",
+    "add-to-daily-class":"Add to Daily Class Activities",
     "add-to-collection-success": "You've added {{contentTitle}} to {{collectionTitle}}. Do you want to edit that {{collectionType}}?",
     "add-to-lesson-success": "You've added {{collectionTitle}} to {{lessonTitle}}. Do you want to edit that {{collectionType}}?",
     "add-type-question": "What type of question would you like to add?",
