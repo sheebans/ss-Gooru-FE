b166675a54efea7dd635e92253ffee9d9c5c682b
Melany Delgado <melany.delgado@edify.cr>
2017-06-26 12:20:02 -0600

    Add My Report to Independent Learning

--- app/router.js
+++ app/router.js
@@ -116,6 +116,7 @@ Router.map(function() {
     });
     this.route('independent', { path: '/course/:courseId' }, function() {
       this.route('course-map');
+      this.route('performance');
     });
   });
 
@@ -163,7 +164,6 @@ Router.map(function() {
    * IMPORTANT! the profile route should be the last one at this file, so we can handle the app urls
    * and the vanity urls for profiles like www.gooru.org/javier-perez
    */
-
 });
 
 export default Router;
