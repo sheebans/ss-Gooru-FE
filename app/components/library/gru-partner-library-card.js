a08a7e651087dc31359d9868db574bb1007d6682
alerg11 <alerg11@gmail.com>
2017-06-28 17:33:26 -0600

    NILE-1281. General changes

--- app/components/library/gru-partner-library-card.js
+++ app/components/library/gru-partner-library-card.js
@@ -38,37 +38,7 @@ export default Ember.Component.extend({
   /**
    * @property {Library} content
    */
-  content: null,
-
-  /**
-   * @property {boolean} Indicates if library has 1 or more resources
-   */
-  hasResources: Ember.computed.gt('content.resourceCount', 0),
-
-  /**
-   * @property {boolean} Indicates if collection has 1 or more questions
-   */
-  hasCollections: Ember.computed.gt('collectionCount', 0),
-
-  /**
-   * @property {boolean} Indicates if collection has 1 or more resources
-   */
-  hasAssessments: Ember.computed.gt('assessmentCount', 0),
-
-  /**
-   * @property {boolean} Indicates if collection has 1 or more questions
-   */
-  hasQuestions: Ember.computed.gt('content.questionCount', 0),
-
-  /**
-   * @property {boolean}
-   */
-  hasRubrics: Ember.computed.gt('content.rubricCount', 0),
-
-  /**
-   * @property {boolean}
-   */
-  hasCourses: Ember.computed.gt('content.courseCount', 0),
+  content: null
 
 
   // -------------------------------------------------------------------------
--- app/routes/library.js
+++ app/routes/library.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import PrivateRouteMixin from 'gooru-web/mixins/private-route-mixin';
-import LibraryModel from 'gooru-web/models/library/library';
 
 /**
  * Library route
@@ -18,7 +17,7 @@ export default Ember.Route.extend(PrivateRouteMixin, {
   searchService: Ember.inject.service('api-sdk/search'),
 
   /**
-   * @type {searchService} Search service object
+   * @type {libraryService} Library service object
    */
   libraryService: Ember.inject.service('api-sdk/library'),
 
@@ -27,31 +26,13 @@ export default Ember.Route.extend(PrivateRouteMixin, {
 
   model: function() {
 
-    const library = LibraryModel.create({ //for testing, delete it later
-      id: 1,
-      name: "Ale library",
-      thumbnail: "ba956a97-ae15-11e5-a302-f8a963065976.png",
-      tenant: "ba956a97-ae15-11e5-a302-f8a963065976",
-      tenant_root: null,
-      course_count: 5,
-      assessment_count: 12,
-      collection_count: 15,
-      resource_count: 56,
-      question_count: 45,
-      rubric_count: 12,
-      sequence_id: 1,
-      taxonomy: null
-    });
-
     return Ember.RSVP.hash({
       courses: this.get('searchService').searchFeaturedCourses('*'),
-      libraries: this.get('libraryService').fetchLibraries(),
-      library
+      libraries: this.get('libraryService').fetchLibraries()
     });
   },
   setupController: function (controller, model) {
     controller.set('courses', model.courses);
     controller.set('libraries', model.libraries);
-    controller.set('library', model.library);
   }
 });
