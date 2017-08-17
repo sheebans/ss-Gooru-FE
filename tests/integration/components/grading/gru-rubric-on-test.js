990816f690edfe63e2691408e286f7a2dcbcfb94
gmmendezp <gmmendezp@gmail.com>
2017-08-16 19:10:45 -0600

    NILE-1538: Add rubric on grading.

--- tests/integration/components/grading/gru-rubric-on-test.js
+++ tests/integration/components/grading/gru-rubric-on-test.js
@@ -2,8 +2,8 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent(
-  'grading/gru-scoring-on',
-  'Integration | Component | grading/gru scoring on',
+  'grading/gru-rubric-on',
+  'Integration | Component | grading/gru rubric on',
   {
     integration: true
   }
@@ -13,7 +13,7 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{grading/gru-scoring-on}}`);
+  this.render(hbs`{{grading/gru-rubric-on}}`);
 
   assert.notEqual(this.$().text().trim(), '');
 });
