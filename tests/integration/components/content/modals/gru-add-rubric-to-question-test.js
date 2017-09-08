4e7dde4fc799998f9ddf0d900160ce712710d6cc
alerg11 <alerg11@gmail.com>
2017-09-08 00:58:50 -0600

    NILE-1708 Test for add rubric modal when no rubircs

--- tests/integration/components/content/modals/gru-add-rubric-to-question-test.js
+++ tests/integration/components/content/modals/gru-add-rubric-to-question-test.js
@@ -10,7 +10,7 @@ moduleForComponent(
   }
 );
 
-test('Layout', function(assert) {
+test('Layout with rubrics', function(assert) {
   let rubrics = Ember.A([
     Ember.Object.create({
       id: 'rubric-id-1',
@@ -46,6 +46,11 @@ test('Layout', function(assert) {
   );
   assert.ok($container.find('.modal-body .lead').length, 'Missing lead');
   assert.equal(
+    $container.find('.modal-body .lead').text().trim(),
+    'Select a rubric to add to this question.',
+    'Wrong lead'
+  );
+  assert.equal(
     $container.find('.list .panel').length,
     3,
     'Wrong number of rubric cards'
@@ -57,5 +62,50 @@ test('Layout', function(assert) {
   assert.ok(
     $container.find('.modal-footer .add-to').length,
     'Missing add to button'
+  );
+
+  assert.notOk(
+    $container.find('.modal-footer .go-to').length,
+    'Go to My Content button should not be displayed'
+  );
+});
+
+test('Layout without rubrics', function(assert) {
+  let rubrics = Ember.A([]);
+
+  let model = {
+    questionId: 'question-1',
+    rubrics: rubrics
+  };
+
+  this.set('model', model);
+
+  this.render(hbs`{{content/modals/gru-add-rubric-to-question model=model}}`);
+  var $container = this.$('.content.modals.gru-add-rubric-to-question');
+
+  assert.ok($container.length, 'Missing add rubric to question component');
+  assert.ok(
+    $container.find('.modal-header .modal-title').length,
+    'Missing title'
+  );
+  assert.ok($container.find('.modal-body .lead').length, 'Missing lead');
+
+  assert.equal(
+    $container.find('.list .panel').length,
+    0,
+    'No rubrics cards should be displayed'
+  );
+  assert.ok(
+    $container.find('.modal-footer .cancel').length,
+    'Missing cancel button'
+  );
+  assert.notOk(
+    $container.find('.modal-footer .add-to').length,
+    'Add to button should not be displayed'
+  );
+
+  assert.ok(
+    $container.find('.modal-footer .go-to').length,
+    'Missing go to button'
   );
 });
