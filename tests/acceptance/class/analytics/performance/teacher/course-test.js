dc76efedf3150455b8889ee42448b54f5788408c
Sushma <sushma@gooru.org>
2017-09-22 18:26:44 +0530

    Navigate to unit tests removed

--- tests/acceptance/class/analytics/performance/teacher/course-test.js
+++ tests/acceptance/class/analytics/performance/teacher/course-test.js
@@ -1,4 +1,3 @@
-import { test } from 'qunit';
 import moduleForAcceptance from 'gooru-web/tests/helpers/module-for-acceptance';
 import { authenticateSession } from 'gooru-web/tests/helpers/ember-simple-auth';
 
@@ -13,58 +12,4 @@ moduleForAcceptance('Acceptance | class/analytics/performance/teacher/course', {
       }
     });
   }
-});
-test('Test collections/assessments filter button selected', function(assert) {
-  visit(
-    '/class/class-for-pochita-as-teacher/analytics/performance/teacher/course'
-  );
-
-  andThen(function() {
-    assert.equal(
-      currentURL(),
-      '/class/class-for-pochita-as-teacher/analytics/performance/teacher/course'
-    );
-
-    const $actionsBar = find('.controller.class .gru-actions-bar');
-    const $assessmentButton = $actionsBar.find('button.assessment');
-    const $collectionButton = $actionsBar.find('button.collection');
-    assert.ok(
-      $assessmentButton.hasClass('selected'),
-      'assessment button should be selected by default'
-    );
-
-    click($collectionButton); //click on collections filter button
-
-    andThen(function() {
-      assert.ok(
-        $collectionButton.hasClass('selected'),
-        'collection button should be selected'
-      );
-      assert.ok(
-        !$assessmentButton.hasClass('selected'),
-        'assessment button should not be selected'
-      );
-      assert.equal(
-        currentURL(),
-        '/class/class-for-pochita-as-teacher/analytics/performance/teacher/course?filterBy=collection'
-      );
-
-      click($assessmentButton); //click on assessments filter button
-      andThen(function() {
-        assert.ok(
-          !$collectionButton.hasClass('selected'),
-          'collection button should not be selected'
-        );
-        assert.ok(
-          $assessmentButton.hasClass('selected'),
-          'assessment button should be selected'
-        );
-
-        assert.equal(
-          currentURL(),
-          '/class/class-for-pochita-as-teacher/analytics/performance/teacher/course'
-        );
-      });
-    });
-  });
 });
