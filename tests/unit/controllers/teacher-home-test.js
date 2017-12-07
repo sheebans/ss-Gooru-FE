3d8d4cdfdeb866a05e8f9595988663abc9d223dc
ashokkumar <ashok.s@bambeeq.com>
2017-12-07 18:01:29 +0530

    test case issue fixed

--- tests/unit/controllers/teacher-home-test.js
+++ tests/unit/controllers/teacher-home-test.js
@@ -1,10 +1,104 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
+var performanceService = Ember.Object.create({
+  findClassPerformanceSummaryByClassIds: () => {
+    return Ember.RSVP.resolve([]);
+  }
+});
+
+var applicationController = Ember.Object.create({
+  profile: Ember.Object.create({
+    id: 'profile-id'
+  }),
+  myClasses: {
+    classes: [
+      Ember.Object.create({
+        id: 'class1',
+        title: 'Class Archived 1',
+        isArchived: true,
+        startDate: '2017-04-19T21:30:56Z',
+        isTeacher: function() {
+          return true;
+        }
+      }),
+      Ember.Object.create({
+        id: 'class2',
+        title: 'Class Archived 2',
+        isArchived: true,
+        startDate: '2016-04-19T21:30:56Z',
+        isTeacher: function() {
+          return true;
+        }
+      })
+    ]
+  },
+  showArchivedClasses: true,
+
+  getLocalStorage: () => ({
+    getItem: () => null
+  })
+});
 moduleFor('controller:teacher-home', 'Unit | Controller | teacher-home', {});
 test('Sort Archived Classes by Date', function(assert) {
-  assert.expect(0);
+  Ember.run(() => {
+    let controller = this.subject({
+      applicationController,
+      performanceService
+    });
+    controller.set('showArchivedClasses', true);
+    controller.send('filterByDate');
+    assert.equal(
+      controller.get('sortedArchivedClassrooms')[0].get('title'),
+      'Class Archived 2',
+      'Incorrect first class'
+    );
+    assert.equal(
+      controller.get('sortedArchivedClassrooms')[1].get('title'),
+      'Class Archived 1',
+      'Incorrect last class'
+    );
+    controller.send('filterByDate');
+    assert.equal(
+      controller.get('sortedArchivedClassrooms')[0].get('title'),
+      'Class Archived 1',
+      'Incorrect first class'
+    );
+    assert.equal(
+      controller.get('sortedArchivedClassrooms')[1].get('title'),
+      'Class Archived 2',
+      'Incorrect last class'
+    );
+  });
 });
 
 test('Sort Archived Classes By Title', function(assert) {
-  assert.expect(0);
+  Ember.run(() => {
+    let controller = this.subject({
+      applicationController,
+      performanceService
+    });
+    controller.send('filterByTitle');
+    assert.equal(
+      controller.get('sortedArchivedClassrooms')[0].get('title'),
+      'Class Archived 1',
+      'Incorrect first class'
+    );
+    assert.equal(
+      controller.get('sortedArchivedClassrooms')[1].get('title'),
+      'Class Archived 2',
+      'Incorrect last class'
+    );
+    controller.send('filterByTitle');
+    assert.equal(
+      controller.get('sortedArchivedClassrooms')[0].get('title'),
+      'Class Archived 2',
+      'Incorrect first class'
+    );
+    assert.equal(
+      controller.get('sortedArchivedClassrooms')[1].get('title'),
+      'Class Archived 1',
+      'Incorrect last class'
+    );
+  });
 });
