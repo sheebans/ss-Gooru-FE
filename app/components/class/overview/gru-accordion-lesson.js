8835b249f84a8b2a0ffa46c16c8bff6263d39cb2
Venkat Kumar S <venkatkumar.s@bambeeq.com>
2018-06-13 18:43:48 +0530

    Added student view popup close

--- app/components/class/overview/gru-accordion-lesson.js
+++ app/components/class/overview/gru-accordion-lesson.js
@@ -222,6 +222,7 @@ export default Ember.Component.extend(AccordionMixin, ModalMixin, {
       let collectionId = collection.get('id');
       let type = collection.get('format');
       let params = {
+        isStudent: this.get('isStudent'),
         userId: userId,
         classId: classId,
         courseId: courseId,
