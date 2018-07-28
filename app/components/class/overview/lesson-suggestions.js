32d09e3bdeaa65474c568f89ca5bebd9971d1dcd
abhay-gooru <abhay@gooru.org>
2018-07-28 12:30:03 +0530

    tmp staged changes

--- app/components/class/overview/lesson-suggestions.js
+++ app/components/class/overview/lesson-suggestions.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import AccordionMixin from '../../../mixins/gru-accordion';
+import { CONTENT_TYPES } from 'gooru-web/config/config';
 
 export default Ember.Component.extend(AccordionMixin, {
   classNames: [
@@ -83,6 +84,127 @@ export default Ember.Component.extend(AccordionMixin, {
       };
       component.set('studentReportData', params);
       component.set('showReportPullUp', true);
+    },
+
+    setLessonItems() {
+      let collections = Ember.A();
+      const component = this;
+      var options = {
+        userId: component.get('session.userId'),
+        classId: component.get('class').id,
+        courseId: component.get('course').id,
+        unitId: component.get('unit').unitId,
+        lessonId: component.get('lesson').lessonId
+      };
+      let loadDataPromise = component.loadStudentData(options, collections);
+
+      loadDataPromise.then(() => {
+        component.set('items', collections);
+        component.set('loading', false);
+      });
+    },
+
+    loadStudentData: function(options, collections) {
+      let { userId, classId, courseId, unitId, lessonId } = options;
+      const component = this;
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        Ember.RSVP.hash({
+          performanceAssessment: component
+            .get('performanceService')
+            .findStudentPerformanceByLesson(
+              userId,
+              classId,
+              courseId,
+              unitId,
+              lessonId,
+              collections,
+              {
+                collectionType: CONTENT_TYPES.ASSESSMENT
+              }
+            ),
+          performanceCollection: component
+            .get('performanceService')
+            .findStudentPerformanceByLesson(
+              userId,
+              classId,
+              courseId,
+              unitId,
+              lessonId,
+              collections,
+              {
+                collectionType: CONTENT_TYPES.COLLECTION
+              }
+            )
+        }).then(({ performanceAssessment, performanceCollection }) => {
+          let assessments = performanceAssessment.filterBy(
+            'type',
+            'assessment'
+          );
+          let collection = performanceCollection.filterBy('type', 'collection');
+          let performance = assessments.concat(collection);
+          const promises = collections.map(function(collection) {
+            const collectionId = collection.get('id');
+            const isAssessment = collection.get('format') === 'assessment';
+            const isResource =
+              collection.get('format') !== 'assessment' &&
+              collection.get('format') !== 'assessment-external' &&
+              collection.get('format') !== 'collection';
+            collection.set('isResource', isResource);
+
+            const collectionPerformanceData = performance.findBy(
+              'id',
+              collectionId
+            );
+            if (collectionPerformanceData) {
+              const score = collectionPerformanceData.get('score');
+              const timeSpent = collectionPerformanceData.get('timeSpent');
+              const completionDone = collectionPerformanceData.get(
+                'completionDone'
+              );
+              const completionTotal = collectionPerformanceData.get(
+                'completionTotal'
+              );
+
+              const hasStarted = score > 0 || timeSpent > 0;
+              const isCompleted =
+                completionDone > 0 && completionDone >= completionTotal;
+
+              collectionPerformanceData.set('timeSpent', timeSpent);
+              collectionPerformanceData.set('hasStarted', hasStarted);
+              collectionPerformanceData.set('isCompleted', isCompleted);
+              collection.set('performance', collectionPerformanceData);
+
+              const attempts = collectionPerformanceData.get('attempts');
+
+              if (isAssessment) {
+                return component
+                  .get('assessmentService')
+                  .readAssessment(collectionId)
+                  .then(function(assessment) {
+                    const attemptsSettings = assessment.get('attempts');
+                    if (attemptsSettings) {
+                      const noMoreAttempts =
+                        attempts &&
+                        attemptsSettings > 0 &&
+                        attempts >= attemptsSettings;
+                      collectionPerformanceData.set(
+                        'noMoreAttempts',
+                        noMoreAttempts
+                      );
+                      collectionPerformanceData.set(
+                        'isDisabled',
+                        !assessment.get('classroom_play_enabled')
+                      );
+                    }
+                  });
+              } else {
+                return Ember.RSVP.resolve(true);
+              }
+            }
+          });
+          Ember.RSVP.all(promises).then(resolve, reject);
+        });
+      });
     }
   }
 });
